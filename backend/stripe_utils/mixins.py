e84af0fdbcf9263ae6744bdba78a45621c44863d
Patrick Arminio <patrick.arminio@gmail.com>
2018-01-20 16:35:15 +0100

    Remove unused imports

--- backend/stripe_utils/mixins.py
+++ backend/stripe_utils/mixins.py
@@ -1,8 +1,6 @@
 import stripe
 
 from django.conf import settings
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
