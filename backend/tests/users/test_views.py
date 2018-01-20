fdaae0369ef0dd9441e78f06796053b5f40786fa
Simone Basso <thesolitary90@gmail.com>
2018-01-20 14:55:38 +0100

    users: add tests for views

---
+++ backend/tests/users/test_views.py
@@ -0,0 +1,26 @@
+import pytest
+from django.urls import reverse
+from users.models import User
+
+
+def test_post_login_view(client):
+    url = reverse('post-login')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+    assert b'Something went wrong.' in response.content
+
+
+@pytest.mark.django_db
+def test_post_login_view_as_logged(client):
+    user = User.objects.create_user('lennon@thebeatles.com', 'johnpassword')
+
+    client.login(username='lennon@thebeatles.com', password='johnpassword')
+
+    url = reverse('post-login')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+    assert b'lennon@thebeatles.com' in response.content
