5d784217880d4777da1d59821c96138de4533090
Patrick Arminio <patrick.arminio@gmail.com>
2018-02-25 21:49:35 +0000

    Add theme wrapper to storybook

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,11 +1,15 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator as add } from '@storybook/react';
 
 import 'modern-normalize';
+
+import { wrapper } from './wrapper';
 
 function loadStories() {
   require("../src/stories/index.tsx");
   require("../src/stories/navbar/index.tsx");
   require("../src/stories/logo/index.tsx");
 }
+
+add(wrapper());
 
 configure(loadStories, module);
