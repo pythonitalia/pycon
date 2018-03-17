d6547bf3d1a407588458289fff2f8c19c4d95e20
Patrick Arminio <patrick.arminio@gmail.com>
2018-03-17 12:31:25 +0100

    Use box component on the navbar
    Co-authored-by: Giulio Fagioli <fagioli.giulio@gmail.com>
    Co-authored-by: Andrea Ginanni <andrea.ginanni@lotrek.it>

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Menu } from 'react-feather';
+import { Box } from '../box';
 
 export class Navbar extends React.Component<{}, {}> {
   render() {
     return (
-      <div>
+      <Box p={3}>
         <Menu />
-      </div>
+      </Box>
     );
   }
 }
