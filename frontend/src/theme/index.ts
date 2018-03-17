6ecff4f9a986e870c13a045d928f45ad7fc60ab6
Patrick Arminio <patrick.arminio@gmail.com>
2018-03-17 17:39:46 +0100

    Lint theme file

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -43,10 +43,10 @@ const shadows = [`0 1px 2px 0 ${colors.text}`, `0 1px 4px 0 ${colors.text}`];
 const fonts = {
   title: 'Rubik',
   base: 'Roboto Mono',
-  button : 'Rubik',
+  button: 'Rubik'
 };
 
-const timings = [0.15,0.3]
+const timings = [0.15, 0.3];
 
 export const theme = {
   breakpoints,
