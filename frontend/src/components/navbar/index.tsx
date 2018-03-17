290906e3bff289499d51306f3c725425e11de08f
Patrick Arminio <patrick.arminio@gmail.com>
2018-03-17 12:58:05 +0100

    Add logo to navbar
    Co-authored-by: Giulio Fagioli <fagioli.giulio@gmail.com>
    Co-authored-by: Andrea Ginanni <andrea.ginanni@lotrek.it>

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { Menu } from 'react-feather';
 import { Box } from '../box';
+import { Logo } from '../logo';
 
 export class Navbar extends React.Component<{}, {}> {
   render() {
     return (
-      <Box p={3}>
-        <Menu />
+      <Box p={3} flexDirection="row" display="flex" alignItems="center">
+        <Box p={2}>
+          <Menu />
+        </Box>
+
+        <Logo />
       </Box>
     );
   }
