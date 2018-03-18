829d54a0f61dc96b8d59ce4ccdab0048ff23ae8d
Patrick Arminio <patrick.arminio@gmail.com>
2018-03-18 01:15:11 +0100

    Add links to navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -4,25 +4,26 @@ import { Menu } from 'react-feather';
 import { Box } from '../box';
 import { Logo } from '../logo';
 import { ButtonLink } from '../button';
+import { Link } from '../link';
 
 export class Navbar extends React.Component<{}, {}> {
   render() {
     return (
       <Box p={3} flexDirection="row" display="flex" alignItems="center">
-        <Box p={2}>
+        <Box p={2} display={['block', 'none']}>
           <Menu />
         </Box>
 
         <Logo />
 
-        <ButtonLink
-          href="/"
-          style={{
-            marginLeft: 'auto'
-          }}
-        >
-          Tickets
-        </ButtonLink>
+        <Box ml="auto" flexDirection="row" display="flex" alignItems="center">
+          <Box mr={3} display={['none', 'block']}>
+            <Link href="/">Schedule</Link>
+            <Link href="/">Speaker</Link>
+            <Link href="/">FAQ</Link>
+          </Box>
+          <ButtonLink href="/">Tickets</ButtonLink>
+        </Box>
       </Box>
     );
   }
