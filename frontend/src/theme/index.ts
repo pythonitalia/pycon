f0a1844803084e78c54dd9332c55324c2e4594d2
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-03-21 19:10:25 +0100

    Theme, add temporary box shadow for card

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -38,7 +38,11 @@ const radii = [0, 2, 4, 8];
 
 const borders = [0, '1px solid', '2px solid'];
 
-const shadows = [`0 1px 2px 0 ${colors.text}`, `0 1px 4px 0 ${colors.text}`];
+const shadows = [
+  `0 1px 2px 0 ${colors.text}`,
+  `0 1px 4px 0 ${colors.text}`,
+  `0 8px 24px 0 ${colors.text}`
+];
 
 const fonts = {
   title: 'Rubik',
