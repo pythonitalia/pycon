afc0809cd844a69446c0baf678a8ab9c094d830a
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-03-21 19:05:26 +0100

    Card, add getColor method

--- frontend/src/components/card/utils.ts
+++ frontend/src/components/card/utils.ts
@@ -3,3 +3,9 @@ export const getBackgroundColor = (hover: boolean) => {
   hover ? (result = 'white') : (result = 'blue');
   return result;
 };
+
+export const getColor = (hover: boolean) => {
+  let result;
+  hover ? (result = 'white') : (result = 'white');
+  return result;
+};
