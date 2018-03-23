7cc1b132f4ea20792edda965bbf00ca33bb88901
Patrick Arminio <patrick.arminio@gmail.com>
2018-03-23 22:34:24 +0000

    Add some padding to card story

--- frontend/src/stories/card/index.tsx
+++ frontend/src/stories/card/index.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Card } from '../../components/card';
+import { Box } from 'components/box';
 
-storiesOf('Card', module).add('default', () => <Card>Hello world</Card>);
+storiesOf('Card', module).add('default', () => (
+  <Box p={4}>
+    <Card>Hello world</Card>
+  </Box>
+));
