a88b76541e1a71a1165db6ece404e158e7207062
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-21 17:53:15 +0200

    Allow cards to have a background image

--- frontend/src/stories/card/index.tsx
+++ frontend/src/stories/card/index.tsx
@@ -4,8 +4,14 @@ import { storiesOf } from '@storybook/react';
 import { Card } from '../../components/card';
 import { Box } from 'components/box';
 
-storiesOf('Card', module).add('default', () => (
-  <Box p={4}>
-    <Card>Hello world</Card>
-  </Box>
-));
+storiesOf('Card', module)
+  .add('default', () => (
+    <Box p={4}>
+      <Card>Hello world</Card>
+    </Box>
+  ))
+  .add('image', () => (
+    <Box p={4}>
+      <Card src="https://placeimg.com/480/640/people">Hello world</Card>
+    </Box>
+  ));
