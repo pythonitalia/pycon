b5e86a827324ca58d538a7374ea8d9ac08ae57cc
Patrick Arminio <patrick.arminio@gmail.com>
2018-05-26 18:07:53 +0200

    Move logo stories inside logo component folder

--- frontend/src/components/logo/stories.tsx
+++ frontend/src/components/logo/stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Logo } from '../../components/logo';
+import { Logo } from '.';
 
 storiesOf('Logo', module).add('normal', () => (
   <div
