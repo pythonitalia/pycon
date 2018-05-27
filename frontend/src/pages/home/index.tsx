4fae8d4ca5ee2976af38fe6c07c5a9962d5afa90
Patrick Arminio <patrick.arminio@gmail.com>
2018-05-27 13:53:46 +0200

    Make home page a landing page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { Navbar } from 'components/navbar';
 import { Button } from 'components/button';
-import { Grid, Column } from 'components/grid';
-import { Card } from 'components/card';
 import { Hero } from 'components/hero';
 import { Title, Subtitle } from 'components/typography';
 
@@ -13,66 +11,13 @@ export const HomePage = () => (
     <Hero
       renderFooter={() => (
         <React.Fragment>
-          <Button>Get your Ticket</Button>
-          <Button variant="secondary">Propose a talk</Button>
+          <Button variant="secondary">Coming soon</Button>
         </React.Fragment>
       )}
     >
       <Title>PyCon 10</Title>
-      <Subtitle>Florence, XX XXXX 2019</Subtitle>
-      <Subtitle level={2}>Location</Subtitle>
+      <Subtitle>Florence, Italy</Subtitle>
+      <Subtitle level={2}>May 2019</Subtitle>
     </Hero>
-
-    <div className="pitch">
-      <div>
-        <h2>Perché la pycon</h2>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
-          reprehenderit labore, voluptatem officia velit rerum amet excepturi
-          esse, sit ea harum! Aspernatur, earum. Dolor sed commodi non
-          laudantium, ipsam veniam?
-        </p>
-      </div>
-
-      <div>
-        <h2>Di cosa parleremo</h2>
-
-        <h3>Data</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
-          reprehenderit labore, voluptatem officia velit rerum amet excepturi
-          esse, sit ea harum! Aspernatur, earum. Dolor sed commodi non
-          laudantium, ipsam veniam?
-        </p>
-
-        <h3>Web</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt,
-          reprehenderit labore, voluptatem officia velit rerum amet excepturi
-          esse, sit ea harum! Aspernatur, earum. Dolor sed commodi non
-          laudantium, ipsam veniam?
-        </p>
-      </div>
-    </div>
-
-    <div className="keynote-speakers">
-      <Grid>
-        <Column cols={4}>
-          <Card>Example</Card>
-          <Card>Example</Card>
-        </Column>
-        <Column cols={4}>
-          <Card>Example</Card>
-          <Card>Example</Card>
-        </Column>
-        <Column cols={4}>
-          <Card>Example</Card>
-          <Card>Example</Card>
-        </Column>
-      </Grid>
-
-      <Button variant="secondary">See the schedule</Button>
-    </div>
   </div>
 );
