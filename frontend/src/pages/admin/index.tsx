bb49e396a639a19211bcd68a46644096984e9802
Patrick Arminio <patrick.arminio@gmail.com>
2018-09-01 22:31:17 +0100

    Add simple header for the admin

--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { Router, RouteComponentProps } from '@reach/router';
 
+import * as styles from './style.css';
+
 import { Home } from './home';
+import { Logo } from '../../components/logo/index';
 
 export const Admin = (props: RouteComponentProps) => (
-  <Router>
-    <Home path="/" />
-  </Router>
+  <>
+    <header className={styles.header}>
+      <Logo />
+      <h1 className={styles.headerTitle}>Python Italia</h1>
+    </header>
+
+    <Router>
+      <Home path="/" />
+    </Router>
+  </>
 );
