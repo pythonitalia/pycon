bbf5ee0cc2600aac7346fdd315f0d1013e4a4918
Patrick Arminio <patrick.arminio@gmail.com>
2018-09-01 20:03:20 +0100

    Remove frontend

--- frontend/src/components/logo/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Title } from '../typography';
-
-export const Logo = () => <Title level={2}>PyCon 10</Title>;
--- frontend/src/components/navbar/stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import { Navbar } from '.';
-
-storiesOf('Navbar', module).add('normal', () => <Navbar />);
