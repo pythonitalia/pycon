a6206ebf3d7feb6fd2b3a98e96559d749af49623
Patrick Arminio <patrick.arminio@gmail.com>
2018-09-02 17:16:06 +0100

    Add apollo setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router, RouteComponentProps } from '@reach/router';
+import { ApolloProvider } from 'react-apollo';
 
 import { Admin } from './pages/admin';
+
+import { client } from './client';
 
 import './index.css';
 
@@ -10,11 +13,13 @@ const Home = (props: RouteComponentProps) => <div>Home</div>;
 const Dash = (props: RouteComponentProps) => <div>Dash</div>;
 
 const App = () => (
-  <Router>
-    <Home path="/" />
-    <Dash path="dashboard" />
-    <Admin path="/admin/*" />
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <Home path="/" />
+      <Dash path="dashboard" />
+      <Admin path="/admin/*" />
+    </Router>
+  </ApolloProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
