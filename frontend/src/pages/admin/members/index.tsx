6e8d1115a32f6ebc52a48d70d985b34319aa1b85
Patrick Arminio <patrick.arminio@gmail.com>
2018-09-02 14:38:41 +0100

    Add stub table for members

--- frontend/src/pages/admin/members/index.tsx
+++ frontend/src/pages/admin/members/index.tsx
@@ -4,5 +4,26 @@ import { RouteComponentProps } from '@reach/router';
 import { Title } from '../../../components/title/index';
 
 export const Members = (props: RouteComponentProps) => (
-  <Title>Members list</Title>
+  <>
+    <Title>Members list</Title>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Stato</th>
+          <th>Scadenza</th>
+          <th />
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Patrick Arminio</td>
+          <td>Attivo</td>
+          <td>20 aprile 2019</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
 );
