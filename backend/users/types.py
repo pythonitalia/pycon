9bb792c6c054a1cc0c9efd6b82f0710edd1db66f
Marco Acierno <marcoaciernoemail@gmail.com>
2018-09-23 12:46:15 +0200

    Add user's "data_joined" to the allowed GraphQL fields

--- backend/users/types.py
+++ backend/users/types.py
@@ -9,7 +9,7 @@ from .models import User
 class MeUserType(DjangoObjectType):
     class Meta:
         model = User
-        only_fields = ("id", "email")
+        only_fields = ("id", "email", "date_joined")
 
 
 class PaginatedUsersType(ObjectType):
