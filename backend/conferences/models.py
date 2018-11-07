8fadfb0c0aae76df02b11cb61a62ed1f0078ef80
Marco Acierno <marcoaciernoemail@gmail.com>
2018-11-07 23:15:57 +0100

    Make Conference Topic unique

--- backend/conferences/models.py
+++ backend/conferences/models.py
@@ -53,7 +53,7 @@ class Conference(TimeStampedModel, TimeFramedModel):
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
