0fb81ea4bb2fe68d9ff1997604d0c5e8de1bca12
Marco Acierno <marcoaciernoemail@gmail.com>
2018-12-29 13:37:05 +0100

    Extract generic `AuthOnlyDjangoFormMutation`

--- backend/api/mutations.py
+++ backend/api/mutations.py
@@ -1,3 +1,5 @@
+from graphql import GraphQLError
+
 from graphene_django.forms import mutation
 
 
@@ -10,3 +12,15 @@ class ContextAwareDjangoModelFormMutation(mutation.DjangoModelFormMutation):
         kwargs = super().get_form_kwargs(root, info, **input)
         kwargs['context'] = info.context
         return kwargs
+
+
+class AuthOnlyDjangoFormMutation(ContextAwareDjangoModelFormMutation):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        if not info.context.user.is_authenticated:
+            raise GraphQLError('User not logged in')
+
+        return super().mutate_and_get_payload(root, info, **input)
--- backend/submissions/mutations.py
+++ backend/submissions/mutations.py
@@ -1,22 +1,13 @@
 import graphene
 
-from graphql import GraphQLError
+from api.mutations import AuthOnlyDjangoFormMutation
 
-from api.mutations import ContextAwareDjangoModelFormMutation
-
-from .types import SubmissionType, SubmissionTypeType  # noqa
+from .types import SubmissionType, SubmissionTypeType
 
 from .forms import SendSubmissionForm
 
 
-class SendSubmission(ContextAwareDjangoModelFormMutation):
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        if not info.context.user.is_authenticated:
-            raise GraphQLError('User not logged in')
-
-        return super().mutate_and_get_payload(root, info, **input)
-
+class SendSubmission(AuthOnlyDjangoFormMutation):
     class Meta:
         form_class = SendSubmissionForm
 
