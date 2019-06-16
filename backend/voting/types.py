5a751b9c59d92a2136edd064903b9a5b4d731025
Etty <beltrami.ester@gmail.com>
2019-06-16 19:21:52 +0200

    Add voting types

---
+++ backend/voting/types.py
@@ -0,0 +1,24 @@
+from graphene_django import DjangoObjectType
+from voting.models import VoteRange, Vote
+
+
+class VoteRangeType(DjangoObjectType):
+    class Meta:
+        model = VoteRange
+        only_fields = (
+            'name',
+            'first',
+            'last',
+            'step',
+        )
+
+
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+        only_fields = (
+            'range',
+            'value',
+            'user',
+            'submission'
+        )
