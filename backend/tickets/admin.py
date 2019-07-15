e8665384ff9510dceb1ee5a3c78ca3db853e0e99
greenkey <greenkey@loman.it>
2019-07-15 19:13:41 +0200

    create answer model and show answers in the ticket admin view

--- backend/tickets/admin.py
+++ backend/tickets/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
-from tickets.models import Ticket, TicketQuestion, TicketQuestionChoices
+from tickets.models import Ticket, TicketQuestion, TicketQuestionChoices, UserAnswer
+
+
+class UserAnswersInline(admin.TabularInline):
+    model = UserAnswer
+    fields = ("question", "answer_choice")
+    readonly_fields = ("question", "answer_choice")
+
+    def question(self, instance):
+        return instance.answer.question
+
+    question.short_description = "Question"
+
+    def answer_choice(self, instance):
+        return instance.answer.choice
+
+    answer_choice.short_description = "Answer"
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ("ticket_fare_name", "user_email")
     search_filters = ("user__email", "conference__code")
+    inlines = [UserAnswersInline]
 
     def ticket_fare_name(self, obj):
         return obj.ticket_fare.name
