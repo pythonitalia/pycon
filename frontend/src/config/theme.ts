a386845a9dffab2eae9efa2e2786f72b2525c018
lucafedri <luca.fedrizzi@belka.us>
2019-07-15 12:00:02 +0200

    Add fannypack

---
+++ frontend/src/config/theme.ts
@@ -0,0 +1,15 @@
+import { css } from "fannypack";
+
+export const theme = {
+  Button: {
+    base: css`
+      border-radius: 100px;
+    `,
+    disabled: css`
+      opacity: 0.2;
+    `,
+  },
+  palette: {
+    primary: "#0C67FF",
+  },
+};
