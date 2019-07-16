a340b27445779b01e9977c81c22e06d12614147a
Patrick Arminio <patrick.arminio@gmail.com>
2019-07-17 00:07:46 +0200

    Add hero component (#639)
    * Add hero component
    
    * Update props

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,22 @@
 import * as React from "react";
 
 import styled from "styled-components";
+import { Hero } from "../components/hero";
 import { HomeLayout } from "../layouts/home";
-
-
-const Title = styled.h1`
-  color: blue;
-`;
 
 export default () => (
   <HomeLayout>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
-    <Title>Yay!!1</Title>
+    <Hero
+      title="Hello world"
+      backgroundImage={
+        "https://live.staticflickr.com/65535/33985680028_8eb0b570f9_h.jpg"
+      }
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et omnis
+        hic veniam nisi architecto reprehenderit voluptate magnam sed commodi
+        vel quidem ea, blanditiis quos harum non ipsam, soluta saepe.
+      </p>
+    </Hero>
   </HomeLayout>
 );
