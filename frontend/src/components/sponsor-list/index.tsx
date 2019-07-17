254659ba700478a0039099471c2d98287a90dc62
Patrick Arminio <patrick.arminio@gmail.com>
2019-07-17 13:21:41 +0200

    WIP

---
+++ frontend/src/components/sponsor-list/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const SponsorList: React.SFC<{}> = props => (
+  <div>
+    <h1>Diversity</h1>
+
+    <ul>
+      <li>AAAAAAAAAAAAA</li>
+    </ul>
+  </div>
+);
