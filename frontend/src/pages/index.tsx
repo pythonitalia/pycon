d7ef8e706fa87ebf59ab01141bb1dd15ca83fc4c
Patrick Arminio <patrick.arminio@gmail.com>
2019-07-17 12:22:28 +0200

    Use responsive images in hero (#648)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 
+import { graphql } from "gatsby";
 import { Hero } from "../components/hero";
 import { TwoColumnsText } from "../components/two-columns-text";
 import { HomeLayout } from "../layouts/home";
 
-export default () => (
+type HomeProps = {
+  data: any;
+};
+
+export default ({ data }: HomeProps) => (
   <HomeLayout>
-    <Hero title="Hello world" backgroundImage={"https://placebear.com/1300/400"}>
+    <Hero title="Hello world" backgroundImage={data.heroImage.childImageSharp}>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et omnis
         hic veniam nisi architecto reprehenderit voluptate magnam sed commodi
@@ -16,3 +21,15 @@ export default () => (
     <TwoColumnsText />
   </HomeLayout>
 );
+
+export const query = graphql`
+  query {
+    heroImage: file(relativePath: { eq: "images/hero.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
