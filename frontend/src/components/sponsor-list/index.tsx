bd129184a4c5f59a473aca8684bda2db9dcede0c
Marco Bianchi <mbianchi.akita8@gmail.com>
2019-07-19 00:03:08 +0200

    add sponsor-list component

--- frontend/src/components/sponsor-list/index.tsx
+++ frontend/src/components/sponsor-list/index.tsx
@@ -1,11 +1,85 @@
 import React from "react";
 
-export const SponsorList: React.SFC<{}> = props => (
-  <div>
-    <h1>Diversity</h1>
+import { Heading, List } from "fannypack";
+import Img, { GatsbyImageProps } from "gatsby-image";
+import styled from "styled-components";
+import { STANDARD_CUSTOM_COLUMNS_PADDING } from "../../config/spacing";
+import { CustomColumn } from "../column";
+import { CustomColumns } from "../columns";
+import { SectionTitle } from "../section-title";
 
-    <ul>
-      <li>AAAAAAAAAAAAA</li>
-    </ul>
-  </div>
-);
+const Wrapper = styled.div`
+li {
+  width: 100%;
+  max-width: 250px;
+}
+`;
+
+type Sponsor = Array<{name: string, logo: GatsbyImageProps, category: string, link: string}>;
+
+type SponsorListProps = {
+  sponsors: Sponsor;
+};
+
+export const SponsorList: React.SFC<SponsorListProps> = props => {
+
+  const grouppedSponsors = new Map<string, Sponsor>();
+
+  for (const sponsor of props.sponsors) {
+    if (grouppedSponsors.has(sponsor.category)) {
+      const categorizedSponsors = grouppedSponsors.get(sponsor.category);
+      if (!categorizedSponsors) {
+        continue;
+      }
+      categorizedSponsors.push(sponsor);
+    } else {
+      grouppedSponsors.set(sponsor.category, [sponsor]);
+    }
+  }
+
+  const sponsorsItems = Array.from(grouppedSponsors.entries()).map((value, categoryIndex) => {
+    const category = value[0];
+    const sponsors = value[1];
+    const listItems = sponsors.map((sponsor, sponsorIndex) => (
+      <List.Item key={sponsorIndex}>
+        <Img {...sponsor.logo} alt={sponsor.name}/>
+      </List.Item>
+      )
+    );
+    return (
+      <div key={categoryIndex}>
+        <CustomColumns
+          marginTop={categoryIndex === 0 ? { desktop: -4, tablet: -4, mobile: -1 } : {desktop: 0, tablet: 0, mobile: 0}}
+          paddingLeft={STANDARD_CUSTOM_COLUMNS_PADDING}
+          paddingRight={STANDARD_CUSTOM_COLUMNS_PADDING}
+        >
+          <CustomColumn
+            paddingRight={{ desktop: 3, tablet: 2, mobile: 0 }}
+            spreadMobile={12}
+            spread={6}
+            spreadDesktop={12}
+          >
+            <Heading use="h5">{category}</Heading>
+            <List isHorizontal={true}>
+              {listItems}
+            </List>
+          </CustomColumn>
+        </CustomColumns>
+      </div>
+      );
+    }
+  );
+  return (
+    <Wrapper>
+      <CustomColumns
+        paddingLeft={STANDARD_CUSTOM_COLUMNS_PADDING}
+        paddingRight={STANDARD_CUSTOM_COLUMNS_PADDING}
+      >
+        <CustomColumn>
+          <SectionTitle>Sponsors</SectionTitle>
+          </CustomColumn>
+      </CustomColumns>
+      { sponsorsItems }
+    </Wrapper>
+  );
+};
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { Hero } from "../components/hero";
 import { Faq } from "../components/home-faq";
-import { SectionTitle } from "../components/section-title";
+import { SponsorList } from "../components/sponsor-list";
 import { TwoColumnsText } from "../components/two-columns-text";
 import { HomeLayout } from "../layouts/home";
 
@@ -11,32 +11,46 @@ type HomeProps = {
   data: any;
 };
 
-export default ({ data }: HomeProps) => (
-  <HomeLayout>
-    <Hero title="Pycon XI" backgroundImage={data.heroImage.childImageSharp}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et omnis
-        hic veniam nisi architecto reprehenderit voluptate magnam sed commodi
-        vel quidem ea, blanditiis quos harum non ipsam, soluta saepe.
-      </p>
-    </Hero>
+export default ({ data }: HomeProps) => {
+  const mockSponsors = [
+    {name: "python", logo: data.logoImage.childImageSharp, category: "diversity", link: "https://www.python.org/"},
+    {name: "python2", logo: data.logoImage.childImageSharp, category: "diversity", link: "https://www.python.org/"},
+    {name: "python3", logo: data.logoImage.childImageSharp, category: "beginner", link: "https://www.python.org/"}
+  ];
+  return (
+    <HomeLayout>
+      <Hero title="Pycon XI" backgroundImage={data.heroImage.childImageSharp}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias et omnis
+          hic veniam nisi architecto reprehenderit voluptate magnam sed commodi
+          vel quidem ea, blanditiis quos harum non ipsam, soluta saepe.
+        </p>
+      </Hero>
 
-    <TwoColumnsText />
+      <TwoColumnsText />
 
-    <section>
-      <SectionTitle>Sponsors</SectionTitle>
-    </section>
-    <section>
-      <Faq />
-    </section>
-  </HomeLayout>
-);
+      <section>
+        <SponsorList sponsors={mockSponsors} />
+      </section>
+      <section>
+        <Faq />
+      </section>
+    </HomeLayout>
+  );
+};
 
 export const query = graphql`
   query {
     heroImage: file(relativePath: { eq: "images/hero.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    logoImage: file(relativePath: { eq: "images/python-logo.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1200) {
           ...GatsbyImageSharpFluid
         }
       }
