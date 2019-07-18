bb2ce1d746ce8b1b2ba8c273dd51ea2426ae3631
Etty <beltrami.ester@gmail.com>
2019-07-18 19:59:59 +0200

    Improve ticket graph styling

--- backend/tickets/graphs.py
+++ backend/tickets/graphs.py
@@ -1,9 +1,12 @@
-import pandas as pd
+import itertools
+
 from bokeh.embed import components
-from bokeh.models.tickers import FixedTicker
+from bokeh.models import BasicTicker, FixedTicker, NumeralTickFormatter
+from bokeh.palettes import Dark2_5 as palette
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from django.db.models import Count
+from django.db.models import F, ExpressionWrapper, fields
 from django.db.models.functions import TruncDate
 
 from conferences.models import Conference
@@ -11,11 +14,15 @@ from tickets.models import Ticket
 
 
 def get_conference_tickets_sold_by_date(conference):
+    duration = ExpressionWrapper(TruncDate(F('order__created')) - TruncDate(
+        F('ticket_fare__conference__start')),
+                                 output_field=fields.DurationField())
+
     sold_tickets = Ticket.objects.filter(
         ticket_fare__conference_id=conference.id) \
-        .annotate(sold_date=TruncDate('order__created')) \
-        .order_by('sold_date') \
-        .values('sold_date') \
+        .annotate(days_to_start=duration) \
+        .order_by('days_to_start') \
+        .values('days_to_start') \
         .annotate(**{'count': Count('id')})
 
     return sold_tickets
@@ -24,7 +31,7 @@ def get_conference_tickets_sold_by_date(conference):
 def get_plot_data():
     conferences = []
 
-    for conference in [Conference.objects.last()]:
+    for conference in Conference.objects.all():
         sold_tickets = get_conference_tickets_sold_by_date(conference)
 
         incremental_count = 0
@@ -32,27 +39,37 @@ def get_plot_data():
             incremental_count += ticket['count']
             ticket['total'] = incremental_count
 
-        conferences.append(sold_tickets)
+        data = {'conference': conference, 'sold_tickets': sold_tickets}
+        conferences.append(data)
     return conferences
 
 
 def make_sold_tickets_plot(conferences_data):
-    p = figure(plot_width=800, plot_height=400, x_axis_type="datetime",
-               y_axis_location='right')
+    p = figure(plot_width=800, plot_height=400,
+               x_axis_type="datetime",
+               y_axis_location='right',
+               title="Pycon Tickets",
+               title_location="above"
+               )
 
-    for index, sold_tickets in enumerate(conferences_data):
-        x = [t['sold_date'] for t in sold_tickets]
+    colors = itertools.cycle(palette)
+
+    for index, data in enumerate(conferences_data):
+        sold_tickets = data['sold_tickets']
+        conference = data['conference']
+
+        x = [int(t['days_to_start'].days) for t in sold_tickets]
         y = [t['total'] for t in sold_tickets]
 
-        p.varea(x=x, y1=y, y2=[1 for i in range(len(x))])
+        p.varea(x=x, y1=y, y2=[1 for i in range(len(x))],
+                fill_color=colors.__next__(),
+                fill_alpha=0.3,
+                legend=conference.code)
 
-        tick_vals = []
-        for index, x in enumerate(sorted(x, reverse=True)):
-            if (index % 20 == 0):
-                tick_vals.extend(pd.to_datetime([x]).astype(int) / 10 ** 6)
+        p.xaxis.axis_label = "Days to start conference"
+        p.xaxis[0].formatter = NumeralTickFormatter(format="0")
 
-        p.xaxis.ticker = FixedTicker(ticks=tick_vals)
-
+    p.legend.location = "top_left"
     return p
 
 
