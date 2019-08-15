6ce3df2ed5af77aec7ab2314554de4051c923c40
Etty <beltrami.ester@gmail.com>
2019-08-15 11:25:34 +0200

    Add input field to FE

--- frontend/src/pages/profile-edit.tsx
+++ frontend/src/pages/profile-edit.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 
-import { Card, Heading, InputField, LayoutSet } from "fannypack";
+import {
+  Button,
+  Card,
+  Heading,
+  InputField,
+  LayoutSet,
+  RadioGroup,
+  RadioGroupField,
+  SelectField,
+} from "fannypack";
 import { Column, Container, Row } from "grigliata";
 import styled from "styled-components";
 import { STANDARD_ROW_PADDING } from "../config/spacing";
@@ -29,6 +38,22 @@ const ROW_PADDING = {
   desktop: 1,
 };
 
+const BUTTON_PADDING = {
+  mobile: 1,
+  tabletPortrait: 1,
+  tabletLandscape: 1,
+  desktop: 2,
+};
+
+const COUNTRY_OPTIONS = [
+  { label: "Select", value: "" },
+  { label: "Italy", value: "IT" },
+  { label: "United Kingdom", value: "GB" },
+  { label: "France", value: "FR" },
+  { label: "Germany", value: "DE" },
+  { label: "United States of America", value: "US" },
+];
+
 const InputWrapper: React.FC = props => (
   <Row paddingBottom={ROW_PADDING}>
     <Column
@@ -54,6 +79,10 @@ const EditPage: React.FC = me => {
       ...user,
       [target.id]: target.value,
     }));
+  };
+
+  const handleUserSubmit = event => {
+    console.log(user);
   };
 
   return (
@@ -82,8 +111,127 @@ const EditPage: React.FC = me => {
               validationText={errors.lastName}
             />
           </InputWrapper>
+
+          <InputWrapper>
+            <RadioGroupField
+              isHorizontal={true}
+              label="Gender"
+              a11yId="gender"
+              name="gender"
+              value={user.gender}
+              onChange={hangleUserChange}
+              validationText={errors.gender}
+              options={[
+                { label: "Male", value: "male" },
+                { label: "Female", value: "female" },
+              ]}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="dateBirth"
+              label="Birth Date"
+              value={user.dateBirth}
+              onChange={hangleUserChange}
+              validationText={errors.dateBirth}
+            />
+          </InputWrapper>
+        </Card>
+
+        <Card title="Address">
+          <InputWrapper>
+            <InputField
+              a11yId="address"
+              label="Address"
+              value={user.address}
+              onChange={hangleUserChange}
+              validationText={errors.address}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <SelectField
+              a11yId="country"
+              label="Country"
+              value={user.country}
+              onChange={hangleUserChange}
+              options={COUNTRY_OPTIONS}
+              isRequired={true}
+              validationText={errors.country}
+            />
+          </InputWrapper>
+        </Card>
+
+        <Card title="Company Info">
+          <InputWrapper>
+            <InputField
+              a11yId="businessName"
+              label="Business Name"
+              value={user.businessName}
+              onChange={hangleUserChange}
+              validationText={errors.businessName}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="fiscalCode"
+              maxLength={16}
+              label="Fiscal Code"
+              value={user.fiscalCode}
+              onChange={hangleUserChange}
+              validationText={errors.fiscalCode}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="vatNumber"
+              label="VAT Number"
+              value={user.vatNumber}
+              onChange={hangleUserChange}
+              validationText={errors.vatNumber}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="phoneNumber"
+              maxLength={16}
+              label="Phone Number"
+              value={user.phoneNumber}
+              onChange={hangleUserChange}
+              validationText={errors.phoneNumber}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="recipientCode"
+              maxLength={7}
+              label="Recipient Code *(Italian Company Only)"
+              value={user.recipientCode}
+              onChange={hangleUserChange}
+              validationText={errors.recipientCode}
+            />
+          </InputWrapper>
+
+          <InputWrapper>
+            <InputField
+              a11yId="pecAddress"
+              maxLength={7}
+              label="PEC Address *(Italian Company Only)"
+              value={user.pecAddress}
+              onChange={hangleUserChange}
+              validationText={errors.pecAddress}
+            />
+          </InputWrapper>
         </Card>
       </LayoutSet>
+      <Row paddingBottom={ROW_PADDING} paddingTop={BUTTON_PADDING}>
+        <Button onClick={handleUserSubmit}>Send!</Button>
+      </Row>
     </Wrapper>
   );
 };
@@ -103,7 +251,14 @@ export default ({ data }: UserProps) => (
             desktop: 12,
           }}
         >
-          <EditPage {...{ firstName: "Ester", lastName: "Beltrami" }} />
+          <EditPage
+            {...{
+              firstName: "John Winston",
+              lastName: "Lennon",
+              gender: "male",
+              dateBirth: "09/10/1940",
+            }}
+          />
         </Column>
       </Row>
     </Container>
