2ec8dddeebef229b46fee2257183c405969f11d6
Patrick Arminio <patrick.arminio@gmail.com>
2019-08-18 21:00:27 +0100

    Remove jsx files

--- frontend/src/config/spacing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STANDARD_ROW_PADDING = {
-    mobile: 0.5,
-    tabletPortrait: 0.5,
-    tabletLandscape: 0.5,
-    desktop: 0.5,
-};
--- frontend/src/helpers/use-toggle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-exports.useToggle = (defaultValue) => {
-    const [value, setValue] = react_1.useState(defaultValue), toggle = () => {
-        setValue(!value);
-    };
-    return [value, toggle];
-};
