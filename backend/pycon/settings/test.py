56b35a7331f560562d2269936d5c26e64087fb07
Patrick Arminio <patrick.arminio@gmail.com>
2019-08-18 18:56:59 +0100

    Backend test action

--- backend/pycon/settings/test.py
+++ backend/pycon/settings/test.py
@@ -6,6 +6,7 @@ from .base import STRIPE_SECRET_KEY
 SECRET_KEY = "this-key-should-only-be-used-for-tests"
 SLACK_INCOMING_WEBHOOK_URL = ""
 USE_SCHEDULER = False
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 if not STRIPE_SECRET_KEY.startswith("sk_test_"):
     raise ImproperlyConfigured("Stripe key does not seem a test key")
