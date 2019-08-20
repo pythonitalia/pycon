37cd909ae83fda8d44d5b3d3a09e5f86e0f2728a
Etty <beltrami.ester@gmail.com>
2019-08-20 21:25:04 +0200

    Refactor User mutation, use inheritance

--- backend/api/users/mutations.py
+++ backend/api/users/mutations.py
@@ -4,79 +4,50 @@ from .forms import LoginForm, RegisterForm, UpdateUserForm
 from .types import MeUser
 
 
-class Login(FormMutation):
+def get_me_user_type(result):
+    return MeUser(
+        id=result.id,
+        email=result.email,
+        first_name=result.first_name,
+        last_name=result.last_name,
+        gender=result.gender,
+        open_to_recruiting=result.open_to_recruiting,
+        date_birth=result.date_birth,
+        business_name=result.business_name,
+        fiscal_code=result.fiscal_code,
+        vat_number=result.vat_number,
+        recipient_code=result.recipient_code,
+        pec_address=result.pec_address,
+        address=result.address,
+        country=result.country,
+        phone_number=result.phone_number,
+    )
+
+
+class BaseUserMutation(FormMutation):
     @classmethod
     def transform(cls, result):
-        return MeUser(
-            id=result.id,
-            email=result.email,
-            first_name=result.first_name,
-            last_name=result.last_name,
-            gender=result.gender,
-            open_to_recruiting=result.open_to_recruiting,
-            date_birth=result.date_birth,
-            business_name=result.business_name,
-            fiscal_code=result.fiscal_code,
-            vat_number=result.vat_number,
-            recipient_code=result.recipient_code,
-            pec_address=result.pec_address,
-            address=result.address,
-            country=result.country,
-            phone_number=result.phone_number,
-        )
+        return get_me_user_type(result)
 
+    class Meta:
+
+        form_class = RegisterForm
+        output_types = (MeUser,)
+
+
+class Login(BaseUserMutation):
     class Meta:
         form_class = LoginForm
         output_types = (MeUser,)
 
 
-class Register(FormMutation):
-    @classmethod
-    def transform(cls, result):
-        return MeUser(
-            id=result.id,
-            email=result.email,
-            first_name=result.first_name,
-            last_name=result.last_name,
-            gender=result.gender,
-            open_to_recruiting=result.open_to_recruiting,
-            date_birth=result.date_birth,
-            business_name=result.business_name,
-            fiscal_code=result.fiscal_code,
-            vat_number=result.vat_number,
-            recipient_code=result.recipient_code,
-            pec_address=result.pec_address,
-            address=result.address,
-            country=result.country,
-            phone_number=result.phone_number,
-        )
-
+class Register(BaseUserMutation):
     class Meta:
         form_class = RegisterForm
         output_types = (MeUser,)
 
 
-class Update(FormMutation):
-    @classmethod
-    def transform(cls, result):
-        return MeUser(
-            id=result.id,
-            email=result.email,
-            first_name=result.first_name,
-            last_name=result.last_name,
-            gender=result.gender,
-            open_to_recruiting=result.open_to_recruiting,
-            date_birth=result.date_birth,
-            business_name=result.business_name,
-            fiscal_code=result.fiscal_code,
-            vat_number=result.vat_number,
-            recipient_code=result.recipient_code,
-            pec_address=result.pec_address,
-            address=result.address,
-            country=result.country,
-            phone_number=result.phone_number,
-        )
-
+class Update(BaseUserMutation):
     class Meta:
         form_class = UpdateUserForm
         output_types = (MeUser,)
