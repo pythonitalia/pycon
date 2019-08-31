4e463fbdf318b63795f3824dc9e581600e4c0bc1
Patrick Arminio <patrick.arminio@gmail.com>
2019-08-31 18:11:41 +0100

    Use s3 storage on production

--- backend/pycon/settings/prod.py
+++ backend/pycon/settings/prod.py
@@ -7,3 +7,8 @@ USE_SCHEDULER = False
 
 # if FRONTEND_URL == "http://testfrontend.it/":
 #     raise ImproperlyConfigured("Please configure FRONTEND_URL for production")
+
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_STORAGE_BUCKET_NAME = env("AWS_MEDIA_BUCKET")
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
