35fb664f53c806c9b9fae12606c6210cd6b4fad7
Patrick Arminio <patrick.arminio@gmail.com>
2019-10-03 23:28:41 +0200

    Initial menu model and api

--- backend/api/cms/types.py
+++ backend/api/cms/types.py
@@ -1,3 +1,5 @@
+import typing
+
 import strawberry
 
 from ..helpers.i18n import make_localized_resolver
@@ -7,3 +9,17 @@ from ..helpers.i18n import make_localized_resolver
 class FAQ:
     question: str = strawberry.field(resolver=make_localized_resolver("question"))
     answer: str = strawberry.field(resolver=make_localized_resolver("answer"))
+
+
+@strawberry.type
+class MenuLink:
+    href: str
+    title: str
+    target: typing.Optional[str]
+
+
+@strawberry.type
+class Menu:
+    @strawberry.field
+    def links(self, info) -> typing.List[MenuLink]:
+        return self.links.all()
--- backend/cms/models.py
+++ backend/cms/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from i18n.fields import I18nTextField
 from model_utils.models import TimeStampedModel
+from ordered_model.models import OrderedModel
 
 
 class GenericCopy(TimeStampedModel):
@@ -40,3 +41,38 @@ class FAQ(TimeStampedModel):
         unique_together = ["question", "conference"]
         verbose_name = _("FAQ")
         verbose_name_plural = _("FAQs")
+
+
+class Menu(TimeStampedModel):
+    identifier = models.SlugField(_("identifier"))
+    conference = models.ForeignKey(
+        "conferences.Conference",
+        on_delete=models.CASCADE,
+        verbose_name=_("conference"),
+        related_name="menus",
+    )
+
+    def __str__(self):
+        return f"{self.identifier} ({self.conference.name})"
+
+    class Meta:
+        unique_together = ["identifier", "conference"]
+
+
+class MenuLink(OrderedModel, TimeStampedModel):
+    menu = models.ForeignKey(
+        Menu, on_delete=models.CASCADE, verbose_name=_("menu"), related_name="links"
+    )
+    title = I18nTextField(_("title"), blank=False)
+    href = I18nTextField(_("Link url"), blank=True)
+    page = models.ForeignKey(
+        "pages.Page",
+        on_delete=models.CASCADE,
+        verbose_name=_("page"),
+        related_name="links",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.menu})"
