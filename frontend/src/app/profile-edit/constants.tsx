4ccec511ab4d5e73205fae0ea8ed28ff81ada1ac
Etty <beltrami.ester@gmail.com>
2019-10-31 22:00:21 +0100

    Remove unused constant

--- frontend/src/app/profile-edit/constants.tsx
+++ frontend/src/app/profile-edit/constants.tsx
@@ -1,4 +1,3 @@
-
 export const ROW_PADDING = {
   mobile: 0.5,
   tabletPortrait: 0.5,
@@ -11,11 +10,4 @@ export const BUTTON_PADDING = {
   tabletPortrait: 1,
   tabletLandscape: 1,
   desktop: 2,
-};
-
-export const COLUMN_WIDTH = {
-  mobile: 12,
-  tabletPortrait: 12,
-  tabletLandscape: 12,
-  desktop: 12,
 };
