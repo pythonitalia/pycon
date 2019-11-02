9b52bb12f5e45c1853ddc0d2a40a824043564df1
Marco Acierno <marcoaciernoemail@gmail.com>
2019-11-02 15:42:12 +0100

    WIP New topbar

--- frontend/src/components/topbar/index.tsx
+++ frontend/src/components/topbar/index.tsx
@@ -1,113 +1,23 @@
-import { Link } from "fannypack";
+import { Box, Button, Flex } from "@theme-ui/components";
 import { graphql, useStaticQuery } from "gatsby";
-import { Column, Container, Row } from "grigliata";
 import React from "react";
-import styled, { css } from "styled-components";
 
-import { STANDARD_ROW_PADDING } from "../../config/spacing";
-import { theme } from "../../config/theme";
-import { HeaderNavQuery } from "../../generated/graphql";
+import { NavBarQuery } from "../../generated/graphql";
 import { useToggle } from "../../helpers/use-toggle";
-import { Button } from "../button";
-import { MaxWidthWrapper } from "../max-width-wrapper";
-import { ExpandedMenu } from "./expanded-menu";
-import { Hamburger } from "./hamburger";
-import { ExpandableProps } from "./types";
-
-const LinkContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  height: 100%;
-  display: none;
-
-  @media (min-width: 992px) {
-    display: flex;
-  }
-
-  a {
-    margin-right: 2rem;
-    text-decoration: none;
-    &:last-child {
-      margin-right: 0;
-    }
-  }
-`;
-
-const LogoContainer = styled.div<ExpandableProps>`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  display: flex;
-  height: 100%;
-  align-items: center;
-  font-family: Rubik;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 24px;
-  justify-content: center;
-  color: inherit;
-`;
-
-const MenuContainer = styled.div<ExpandableProps>`
-  display: flex;
-  align-items: center;
-  height: 100%;
-
-  a {
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    outline: none;
-    transition: 0.25s ease-in-out;
-
-    .label {
-      display: none;
-      @media (min-width: 992px) {
-        display: inline-block;
-      }
-    }
-
-    &:hover,
-    &:focus {
-      color: inherit;
-    }
-  }
-`;
-
-const Wrapper = styled.div<ExpandableProps>`
-  height: 80px;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  z-index: 10;
-  background-color: ${theme.palette.white};
-  color: ${theme.palette.primary};
-
-  ${props =>
-    props.open &&
-    css`
-      background-color: ${theme.palette.primary};
-      color: ${theme.palette.white};
-    `}
-
-  > div,
-  > div > div {
-    height: 100%;
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-`;
 
 export const Topbar = () => {
   const [isMenuOpen, toggleMenu] = useToggle(false);
   const {
+    logo,
     backend: {
       conference: { menu },
     },
-  } = useStaticQuery<HeaderNavQuery>(graphql`
-    query HeaderNav {
+  } = useStaticQuery<NavBarQuery>(graphql`
+    query NavBar {
+      logo: file(name: { eq: "logo" }) {
+        publicURL
+      }
+
       backend {
         conference {
           menu(identifier: "header-nav") {
@@ -125,67 +35,20 @@ export const Topbar = () => {
   const { links } = menu!;
 
   return (
-    <Wrapper open={isMenuOpen}>
-      <Container>
-        <LogoContainer open={isMenuOpen}>PyCon Italia</LogoContainer>
+    <Flex
+      sx={{
+        justifyContent: "space-between",
+        alignItems: "center",
+        maxWidth: "largeContainer",
 
-        <Row
-          paddingLeft={STANDARD_ROW_PADDING}
-          paddingRight={STANDARD_ROW_PADDING}
-        >
-          <Column
-            columnWidth={{
-              mobile: 3,
-              tabletPortrait: 6,
-              tabletLandscape: 6,
-              desktop: 6,
-            }}
-          >
-            <MenuContainer open={isMenuOpen}>
-              <Link
-                href="#"
-                onClick={e => {
-                  e.preventDefault();
-                  toggleMenu();
-                }}
-              >
-                <Hamburger open={isMenuOpen} />{" "}
-                <span className="label">Menu</span>
-              </Link>
-            </MenuContainer>
-          </Column>
-          <Column
-            columnWidth={{
-              mobile: 0,
-              tabletPortrait: 6,
-              tabletLandscape: 6,
-              desktop: 6,
-            }}
-          >
-            <LinkContainer>
-              {links.map((link, i) => {
-                if (link.isPrimary) {
-                  return (
-                    <Button key={i} palette={isMenuOpen ? "white" : "primary"}>
-                      {link.title}
-                    </Button>
-                  );
-                }
-
-                // TODO: don't use button for the primary link, rather use a custom
-                // link that looks like the button
-
-                return (
-                  <Link key={i} href={link.href}>
-                    {link.title}
-                  </Link>
-                );
-              })}
-            </LinkContainer>
-          </Column>
-        </Row>
-      </Container>
-      {isMenuOpen && <ExpandedMenu />}
-    </Wrapper>
+        mx: "auto",
+      }}
+    >
+      <img src={logo!.publicURL!} />
+      <Box>
+        <Button>Login</Button>
+        <Button>Menu</Button>
+      </Box>
+    </Flex>
   );
 };
