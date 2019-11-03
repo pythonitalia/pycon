74250ffed276e612bb001ec45d67248b1014ea75
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-03 16:35:50 +0000

    Update login page

--- frontend/src/templates/app.tsx
+++ frontend/src/templates/app.tsx
@@ -1,37 +1,15 @@
 import { Router } from "@reach/router";
-import { Column, Row } from "grigliata";
-import * as React from "react";
+import React from "react";
 
 import { PrivateRoute } from "../app/private-route/private-route";
 import { ProfileApp } from "../app/profile";
 import { LoginForm } from "../components/login-form";
-import { MaxWidthWrapper } from "../components/max-width-wrapper";
 import { SignupForm } from "../components/signup-form";
-import { STANDARD_ROW_PADDING } from "../config/spacing";
-import { MainLayout } from "../layouts/main";
 
-export default ({ pageContext }: { pageContext: { language: string } }) => (
-  <MainLayout language={pageContext.language}>
-    <MaxWidthWrapper>
-      <Row
-        paddingLeft={STANDARD_ROW_PADDING}
-        paddingRight={STANDARD_ROW_PADDING}
-      >
-        <Column
-          columnWidth={{
-            mobile: 12,
-            tabletPortrait: 6,
-            tabletLandscape: 6,
-            desktop: 6,
-          }}
-        >
-          <Router>
-            <LoginForm path="/:lang/login" />
-            <SignupForm path="/:lang/signup" />
-            <PrivateRoute path="/:lang/profile" component={ProfileApp} />
-          </Router>
-        </Column>
-      </Row>
-    </MaxWidthWrapper>
-  </MainLayout>
+export default () => (
+  <Router>
+    <PrivateRoute path="/:lang/profile" component={ProfileApp} />
+    <LoginForm path="/:lang/login" />
+    <SignupForm path="/:lang/signup" />
+  </Router>
 );
