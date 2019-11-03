2c95d7f4d88b8e71a14b96636391dea25ffb3b20
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-03 13:13:58 +0000

    Run prettier everywhere

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,101 +1,101 @@
 const proxy = require("http-proxy-middleware");
 require("dotenv").config({
-    path: `.env`,
+  path: `.env`,
 });
 
 const API_URL = process.env.API_URL || "http://127.0.0.1:8000/graphql";
 const CONFERENCE_CODE = process.env.CONFERENCE_CODE || "pycon-demo";
 
 module.exports = {
-    developMiddleware: app => {
-        app.use(
-            "/graphql",
-            proxy({
-                target: API_URL.replace("/graphql", ""),
-            }),
-        );
+  developMiddleware: app => {
+    app.use(
+      "/graphql",
+      proxy({
+        target: API_URL.replace("/graphql", ""),
+      }),
+    );
+  },
+  plugins: [
+    `gatsby-plugin-netlify`,
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-source-pycon",
+      options: {
+        url: API_URL,
+        conferenceCode: CONFERENCE_CODE,
+        typeName: "BACKEND",
+        fieldName: "backend",
+        refetchInterval: 60,
+      },
     },
-    plugins: [
-        `gatsby-plugin-netlify`,
-        'gatsby-plugin-theme-ui',
-        {
-            resolve: "gatsby-source-pycon",
+    {
+      resolve: "gatsby-plugin-extract-schema",
+      options: {
+        dest: `${__dirname}/_schema.json`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                url: API_URL,
-                conferenceCode: CONFERENCE_CODE,
-                typeName: "BACKEND",
-                fieldName: "backend",
-                refetchInterval: 60,
+              maxWidth: 500,
             },
-        },
-        {
-            resolve: "gatsby-plugin-extract-schema",
+          },
+          {
+            resolve: `gatsby-remark-external-links`,
             options: {
-                dest: `${__dirname}/_schema.json`,
+              target: `_blank`,
+              rel: `nofollow noopener noreferrer`,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 500,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-external-links`,
-                        options: {
-                            target: `_blank`,
-                            rel: `nofollow noopener noreferrer`,
-                        },
-                    },
-                    `gatsby-remark-responsive-iframe`,
-                ],
-            },
-        },
-        `gatsby-plugin-typescript`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-styled-components`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-favicon`,
-            options: {
-                logo: "./static/logo.png",
-                appName: "PyCon Italia",
-                developerName: null,
-                developerURL: null,
-                dir: "auto",
-                lang: "en-GB",
-                background: "#fff",
-                theme_color: "#fff",
-                display: "standalone",
-                orientation: "any",
-                start_url: "/?homescreen=1",
-                version: "1.0",
+          },
+          `gatsby-remark-responsive-iframe`,
+        ],
+      },
+    },
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./static/logo.png",
+        appName: "PyCon Italia",
+        developerName: null,
+        developerURL: null,
+        dir: "auto",
+        lang: "en-GB",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        start_url: "/?homescreen=1",
+        version: "1.0",
 
-                icons: {
-                    android: true,
-                    appleIcon: true,
-                    appleStartup: true,
-                    coast: false,
-                    favicons: true,
-                    firefox: true,
-                    opengraph: false,
-                    twitter: false,
-                    yandex: false,
-                    windows: false,
-                },
-            },
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
         },
-    ],
+      },
+    },
+  ],
 };
--- frontend/plugins/gatsby-source-pycon/src/gatsby-node.js
+++ frontend/plugins/gatsby-source-pycon/src/gatsby-node.js
@@ -1,129 +1,129 @@
 const uuidv4 = require(`uuid/v4`);
 const { buildSchema, printSchema } = require(`gatsby/graphql`);
 const {
-    makeRemoteExecutableSchema,
-    transformSchema,
-    introspectSchema,
-    RenameTypes,
+  makeRemoteExecutableSchema,
+  transformSchema,
+  introspectSchema,
+  RenameTypes,
 } = require(`graphql-tools`);
 const { createHttpLink } = require(`apollo-link-http`);
 const fetch = require(`node-fetch`);
 const invariant = require(`invariant`);
 
 const {
-    NamespaceUnderFieldTransform,
-    StripNonQueryTransform,
-    RemoveConferenceArgument,
+  NamespaceUnderFieldTransform,
+  StripNonQueryTransform,
+  RemoveConferenceArgument,
 } = require(`./transforms`);
 
 exports.sourceNodes = async (
-    { actions, createNodeId, cache, createContentDigest },
-    options,
+  { actions, createNodeId, cache, createContentDigest },
+  options,
 ) => {
-    const { addThirdPartySchema, createNode } = actions;
-    const {
-        url,
-        typeName,
-        fieldName,
-        conferenceCode,
-        headers = {},
-        fetchOptions = {},
-        createSchema,
-        refetchInterval,
-    } = options;
+  const { addThirdPartySchema, createNode } = actions;
+  const {
+    url,
+    typeName,
+    fieldName,
+    conferenceCode,
+    headers = {},
+    fetchOptions = {},
+    createSchema,
+    refetchInterval,
+  } = options;
 
-    const link = createHttpLink({
-        uri: url,
-        fetch,
-        headers,
-        fetchOptions,
-    });
+  const link = createHttpLink({
+    uri: url,
+    fetch,
+    headers,
+    fetchOptions,
+  });
 
-    let introspectionSchema;
+  let introspectionSchema;
 
-    if (createSchema) {
-        introspectionSchema = await createSchema(options);
+  if (createSchema) {
+    introspectionSchema = await createSchema(options);
+  } else {
+    const cacheKey = `gatsby-source-graphql-schema-${typeName}-${fieldName}`;
+    let sdl = await cache.get(cacheKey);
+
+    if (!sdl) {
+      introspectionSchema = await introspectSchema(link);
+      sdl = printSchema(introspectionSchema);
     } else {
-        const cacheKey = `gatsby-source-graphql-schema-${typeName}-${fieldName}`;
-        let sdl = await cache.get(cacheKey);
-
-        if (!sdl) {
-            introspectionSchema = await introspectSchema(link);
-            sdl = printSchema(introspectionSchema);
-        } else {
-            introspectionSchema = buildSchema(sdl);
-        }
-
-        await cache.set(cacheKey, sdl);
+      introspectionSchema = buildSchema(sdl);
     }
 
-    const remoteSchema = makeRemoteExecutableSchema({
-        schema: introspectionSchema,
-        link,
+    await cache.set(cacheKey, sdl);
+  }
+
+  const remoteSchema = makeRemoteExecutableSchema({
+    schema: introspectionSchema,
+    link,
+  });
+
+  const nodeId = createNodeId(`gatsby-source-graphql-${typeName}`);
+  const node = createSchemaNode({
+    id: nodeId,
+    typeName,
+    fieldName,
+    createContentDigest,
+  });
+  createNode(node);
+
+  const resolver = (parent, args, context) => {
+    context.nodeModel.createPageDependency({
+      path: context.path,
+      nodeId: nodeId,
     });
+    return {};
+  };
 
-    const nodeId = createNodeId(`gatsby-source-graphql-${typeName}`);
-    const node = createSchemaNode({
-        id: nodeId,
-        typeName,
-        fieldName,
-        createContentDigest,
-    });
-    createNode(node);
+  const schema = transformSchema(remoteSchema, [
+    new StripNonQueryTransform(),
+    new RemoveConferenceArgument(conferenceCode),
+    new RenameTypes(name => `${typeName}_${name}`),
+    new NamespaceUnderFieldTransform({
+      typeName,
+      fieldName,
+      resolver,
+    }),
+  ]);
 
-    const resolver = (parent, args, context) => {
-        context.nodeModel.createPageDependency({
-            path: context.path,
-            nodeId: nodeId,
-        });
-        return {};
-    };
+  addThirdPartySchema({ schema });
 
-    const schema = transformSchema(remoteSchema, [
-        new StripNonQueryTransform(),
-        new RemoveConferenceArgument(conferenceCode),
-        new RenameTypes(name => `${typeName}_${name}`),
-        new NamespaceUnderFieldTransform({
+  if (process.env.NODE_ENV !== `production`) {
+    if (refetchInterval) {
+      const msRefetchInterval = refetchInterval * 1000;
+      const refetcher = () => {
+        createNode(
+          createSchemaNode({
+            id: nodeId,
             typeName,
             fieldName,
-            resolver,
-        }),
-    ]);
-
-    addThirdPartySchema({ schema });
-
-    if (process.env.NODE_ENV !== `production`) {
-        if (refetchInterval) {
-            const msRefetchInterval = refetchInterval * 1000;
-            const refetcher = () => {
-                createNode(
-                    createSchemaNode({
-                        id: nodeId,
-                        typeName,
-                        fieldName,
-                        createContentDigest,
-                    }),
-                );
-                setTimeout(refetcher, msRefetchInterval);
-            };
-            setTimeout(refetcher, msRefetchInterval);
-        }
+            createContentDigest,
+          }),
+        );
+        setTimeout(refetcher, msRefetchInterval);
+      };
+      setTimeout(refetcher, msRefetchInterval);
     }
+  }
 };
 
 function createSchemaNode({ id, typeName, fieldName, createContentDigest }) {
-    const nodeContent = uuidv4();
-    const nodeContentDigest = createContentDigest(nodeContent);
-    return {
-        id,
-        typeName: typeName,
-        fieldName: fieldName,
-        parent: null,
-        children: [],
-        internal: {
-            type: `GraphQLSource`,
-            contentDigest: nodeContentDigest,
-            ignoreType: true,
-        },
-    };
+  const nodeContent = uuidv4();
+  const nodeContentDigest = createContentDigest(nodeContent);
+  return {
+    id,
+    typeName: typeName,
+    fieldName: fieldName,
+    parent: null,
+    children: [],
+    internal: {
+      type: `GraphQLSource`,
+      contentDigest: nodeContentDigest,
+      ignoreType: true,
+    },
+  };
 }
