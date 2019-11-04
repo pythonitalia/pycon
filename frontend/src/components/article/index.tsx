e00182e14603a02f6b471eb8a7deafb8e973483b
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-04 13:03:52 +0000

    Update page and blog post

--- frontend/src/components/article/index.tsx
+++ frontend/src/components/article/index.tsx
@@ -1,43 +1,5 @@
 import { GatsbyImageProps } from "gatsby-image";
-import { Row } from "grigliata";
 import React from "react";
-import styled from "styled-components";
-
-import { STANDARD_ROW_PADDING } from "../../config/spacing";
-import { ArticleTitle } from "./title";
-
-const Wrapper = styled.div`
-  .content {
-    position: relative;
-    z-index: 1;
-  }
-
-  img {
-    width: 100%;
-    height: auto;
-  }
-
-  header {
-    position: relative;
-  }
-
-  ${ArticleTitle} {
-    margin-bottom: 0.3em;
-
-    @media (min-width: 1024px) {
-      font-size: 42px;
-      line-height: 32px;
-    }
-    @media (min-width: 1366px) {
-      font-size: 90px;
-      line-height: 72px;
-    }
-  }
-
-  p {
-    margin-top: 0;
-  }
-`;
 
 type ArticleProps = {
   hero: GatsbyImageProps | null;
@@ -46,18 +8,5 @@ type ArticleProps = {
 };
 
 export const Article: React.SFC<ArticleProps> = props => (
-  <Wrapper>
-    <Row
-      marginTop={{
-        mobile: 2,
-        tabletPortrait: 2,
-        tabletLandscape: 2,
-        desktop: 2,
-      }}
-      paddingLeft={STANDARD_ROW_PADDING}
-      paddingRight={STANDARD_ROW_PADDING}
-    >
-      <div className="content">{props.children}</div>
-    </Row>
-  </Wrapper>
+  <div className="content">{props.children}</div>
 );
--- frontend/src/templates/blog-post.tsx
+++ frontend/src/templates/blog-post.tsx
@@ -1,48 +1,25 @@
 import { graphql } from "gatsby";
-import { Column, Container, Row } from "grigliata";
 import marksy from "marksy";
 import React, { createElement } from "react";
 
 import { Article } from "../components/article";
 import { PostQuery } from "../generated/graphql";
-import { MainLayout } from "../layouts/main";
 
 const compile = marksy({
   createElement,
 });
 
-export default ({
-  data,
-  pageContext,
-}: {
-  data: PostQuery;
-  pageContext: { language: string };
-}) => {
+export default ({ data }: { data: PostQuery }) => {
   const post = data.backend.blogPost!;
 
   return (
-    <MainLayout language={pageContext.language}>
-      <Container>
-        <Row>
-          <Column
-            columnWidth={{
-              mobile: 12,
-              tabletPortrait: 12,
-              tabletLandscape: 12,
-              desktop: 12,
-            }}
-          >
-            <Article
-              hero={post.imageFile && { ...post.imageFile.childImageSharp! }}
-              title={post.title}
-              description={post.excerpt || ""}
-            >
-              {compile(post.content).tree}
-            </Article>
-          </Column>
-        </Row>
-      </Container>
-    </MainLayout>
+    <Article
+      hero={post.imageFile && { ...post.imageFile.childImageSharp! }}
+      title={post.title}
+      description={post.excerpt || ""}
+    >
+      {compile(post.content).tree}
+    </Article>
   );
 };
 
