4b554cfdfb66eb07227b8477eb10c6052d3ec3ad
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-18 21:35:34 +0100

    Make CFP private

--- frontend/src/templates/app.tsx
+++ frontend/src/templates/app.tsx
@@ -4,6 +4,7 @@ import { jsx } from "theme-ui";
 
 import { PrivateRoute } from "../app/private-route/private-route";
 import { ProfileApp } from "../app/profile";
+import { CFPPage } from "../components/cpf-page";
 import { LoginForm } from "../components/login-form";
 import { SignupForm } from "../components/signup-form";
 import { SubmissionPage } from "../components/submission-page";
@@ -12,6 +13,7 @@ export default () => (
   <Router>
     <PrivateRoute path="/:lang/profile" component={ProfileApp} />
     <PrivateRoute path="/:lang/submission/:id" component={SubmissionPage} />
+    <PrivateRoute path="/:lang/cfp" component={CFPPage} />
     <LoginForm path="/:lang/login" />
     <SignupForm path="/:lang/signup" />
   </Router>
