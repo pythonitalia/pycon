727fa707c1d8fa6cbcc609576384f02208cb2222
Patrick Arminio <patrick.arminio@gmail.com>
2019-11-18 20:20:22 +0100

    Fix image path

--- frontend/plugins/gatsby-plugin-social-images/gatsby-node.js
+++ frontend/plugins/gatsby-plugin-social-images/gatsby-node.js
@@ -23,6 +23,22 @@ exports.onPostBuild = async (args, pluginOptions) => {
     const page = await browser.newPage();
     await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 2 });
     await page.goto(`file://${pagePath}`);
+    await page.evaluate(
+      (cwd, root) => {
+        const images = Array.from(
+          document.querySelectorAll('img[src^="/static"]'),
+        );
+
+        images.forEach(x => {
+          const src = x.getAttribute("src");
+          const newSrc = `${cwd}/${root}${src}`;
+
+          x.setAttribute("src", `file://${newSrc}`);
+        });
+      },
+      process.cwd(),
+      rootDir,
+    );
     await page.screenshot({ path: screenshotPath });
   });
 
