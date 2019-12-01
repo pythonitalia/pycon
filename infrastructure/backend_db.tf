9112088f320e12cf5c69e35b2e698e5c837dc2eb
Patrick Arminio <patrick.arminio@gmail.com>
2019-12-01 20:02:01 +0000

    Update postgres

--- infrastructure/backend_db.tf
+++ infrastructure/backend_db.tf
@@ -23,7 +23,7 @@ resource "aws_db_instance" "backend" {
   allocated_storage   = 10
   storage_type        = "gp2"
   engine              = "postgres"
-  engine_version      = "9.6.11"
+  engine_version      = "9.6.15"
   instance_class      = "db.t2.micro"
   name                = "${terraform.workspace}backend"
   username            = "root"
