cc44de2f924150f3974c6554d2e787dfc1e6d018
Etty <beltrami.ester@gmail.com>
2019-12-05 16:40:30 +0000

    Fix admin newsletter

--- backend/newsletters/models.py
+++ backend/newsletters/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class Subscription(models.Model):
     email = models.EmailField(_("email address"), unique=True)
     date_subscribed = models.DateTimeField(_("date subscribed"), auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email} subscribed on {self.date_subscribed}"
