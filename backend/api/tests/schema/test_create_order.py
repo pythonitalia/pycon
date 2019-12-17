ec6bc754cfeeb6553b8a58f4b2c7a4b450d8f2e8
Patrick Arminio <patrick.arminio@gmail.com>
2019-12-17 14:50:32 +0100

    Update tests

--- backend/api/tests/schema/test_create_order.py
+++ backend/api/tests/schema/test_create_order.py
@@ -1,6 +1,7 @@
 def test_calls_create_order(graphql_client, user, conference, mocker):
     create_order_mock = mocker.patch("api.orders.mutations.create_order")
     create_order_mock.return_value.payment_url = "https://example.com"
+    create_order_mock.return_value.code = "123"
 
     response = graphql_client.query(
         """mutation CreateOrder($code: String!, $input: CreateOrderInput!) {
@@ -22,6 +23,9 @@ def test_calls_create_order(graphql_client, user, conference, mocker):
     )
 
     assert not response.get("errors")
-    assert response["data"]["createOrder"]["paymentUrl"] == "https://example.com"
+    assert response["data"]["createOrder"]["paymentUrl"] == (
+        "https://example.com?return_url=http://localhost:4000/en/orders"
+        "/123/confirmation"
+    )
 
     create_order_mock.assert_called_once()
