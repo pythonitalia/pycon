bbad239238f8bdb91bf5c01aae652e6e8a4d4386
Patrick Arminio <patrick.arminio@gmail.com>
2019-12-19 23:19:27 +0100

    Update strawberry

--- backend/pycon/urls.py
+++ backend/pycon/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
-from strawberry.contrib.django.views import GraphQLView
+from strawberry.django.views import GraphQLView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
