5c4b477d0181be8e6c676d57263820c18dd99acc
Etty <beltrami.ester@gmail.com>
2019-12-20 13:56:23 +0000

    WIP Log GraphQL errors
    Add CustomGraphQLView to log errors when the request returns 400

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,19 @@
 import json
 import logging
 
+import sentry_sdk
 from strawberry.contrib.django.views import GraphQLView
 
 
 class CustomGraphQLView(GraphQLView):
     def dispatch(self, request, *args, **kwargs):
         resp = super(CustomGraphQLView, self).dispatch(request, *args, **kwargs)
-        if resp.status_code == 400:
-            logging.error(json.loads(resp.getvalue())["errors"], exc_info=True)
-            return resp
+        errors = json.loads(resp.getvalue()).get("errors", [])
+        if errors:
+            self._capture_sentry_exceptions(errors)
         return resp
+
+    def _capture_sentry_exceptions(self, errors):
+        for error in errors:
+            logging.error(error, exc_info=True)
+            sentry_sdk.capture_exception(error["message"])
