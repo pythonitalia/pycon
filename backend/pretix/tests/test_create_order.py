ce63d61d444beef8037a9248bb3143a1a1d25ae7
Patrick Arminio <patrick.arminio@gmail.com>
2019-12-23 18:11:28 +0100

    Update tests

--- backend/pretix/tests/test_create_order.py
+++ backend/pretix/tests/test_create_order.py
@@ -4,14 +4,30 @@ from pretix import (
     CreateOrderInput,
     CreateOrderTicket,
     CreateOrderTicketAnswer,
+    InvoiceInformation,
     create_order,
 )
 from pretix.exceptions import PretixError
 
 
+@pytest.fixture
+def invoice_information():
+    return InvoiceInformation(
+        is_business=False,
+        company="ABC",
+        name="ABC",
+        street="ABC",
+        zipcode="ABC",
+        city="ABC",
+        country="ABC",
+        vat_id="ABC",
+        fiscal_code="ABC",
+    )
+
+
 @override_settings(PRETIX_API="https://pretix/api/")
 @pytest.mark.django_db
-def test_creates_order(conference, requests_mock):
+def test_creates_order(conference, requests_mock, invoice_information):
     requests_mock.post(
         f"https://pretix/api/organizers/events/orders/",
         json={"payments": [{"payment_url": "http://example.com"}], "code": 123},
@@ -25,11 +41,16 @@ def test_creates_order(conference, requests_mock):
             ]
         },
     )
+    requests_mock.get(
+        f"https://pretix/api/organizers/events/items",
+        json={"results": [{"id": "123", "admission": True}]},
+    )
 
     order_data = CreateOrderInput(
         email="my@email.com",
         locale="en",
         payment_provider="stripe",
+        invoice_information=invoice_information,
         tickets=[
             CreateOrderTicket(
                 ticket_id="123",
@@ -51,18 +72,23 @@ def test_creates_order(conference, requests_mock):
 
 @override_settings(PRETIX_API="https://pretix/api/")
 @pytest.mark.django_db
-def test_raises_when_response_is_400(conference, requests_mock):
+def test_raises_when_response_is_400(conference, requests_mock, invoice_information):
     requests_mock.post(
         f"https://pretix/api/organizers/events/orders/", status_code=400, json={}
     )
     requests_mock.get(
         f"https://pretix/api/organizers/events/questions", json={"results": []}
     )
+    requests_mock.get(
+        f"https://pretix/api/organizers/events/items",
+        json={"results": [{"id": "123", "admission": False}]},
+    )
 
     order_data = CreateOrderInput(
         email="my@email.com",
         locale="en",
         payment_provider="stripe",
+        invoice_information=invoice_information,
         tickets=[
             CreateOrderTicket(
                 ticket_id="123",
@@ -80,7 +106,9 @@ def test_raises_when_response_is_400(conference, requests_mock):
 
 @override_settings(PRETIX_API="https://pretix/api/")
 @pytest.mark.django_db
-def test_raises_value_error_if_answer_value_is_wrong(conference, requests_mock):
+def test_raises_value_error_if_answer_value_is_wrong(
+    conference, requests_mock, invoice_information
+):
     requests_mock.post(
         f"https://pretix/api/organizers/events/orders/",
         json={"payments": [{"payment_url": "http://example.com"}], "code": 123},
@@ -94,11 +122,16 @@ def test_raises_value_error_if_answer_value_is_wrong(conference, requests_mock):
             ]
         },
     )
+    requests_mock.get(
+        f"https://pretix/api/organizers/events/items",
+        json={"results": [{"id": "123", "admission": True}]},
+    )
 
     order_data = CreateOrderInput(
         email="my@email.com",
         locale="en",
         payment_provider="stripe",
+        invoice_information=invoice_information,
         tickets=[
             CreateOrderTicket(
                 ticket_id="123",
