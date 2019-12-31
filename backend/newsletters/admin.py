892de8e13800cc266a8a4bec49c1eea096340e70
Etty <beltrami.ester@gmail.com>
2019-12-31 16:04:07 +0100

    Remove Form

--- backend/newsletters/admin.py
+++ backend/newsletters/admin.py
@@ -1,42 +1,11 @@
 from django.contrib import admin
-from newsletters.forms import SendEmailForm
-from notifications.emails import send_mail
 
 from .models import Email, Subscription
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    def changelist_view(self, request, extra_context=None):
-        extra_context = extra_context or {}
-        submitted = False
-
-        if request.method == "POST":
-            form = SendEmailForm(request.POST)
-            if form.is_valid():
-                submitted = self.send_emails(**form.cleaned_data)
-        else:
-            form = SendEmailForm()
-
-        extra_context.update({"form": form, "submitted": submitted})
-        return super(SubscriptionAdmin, self).changelist_view(
-            request, extra_context=extra_context
-        )
-
-    def send_emails(self, **kwargs):
-        subject = kwargs.pop("subject")
-        recipients = kwargs.pop("recipients")
-
-        return (
-            send_mail(
-                subject,
-                recipients,
-                "newsletter",
-                context=kwargs,
-                path="emails/newsletter/",
-            )
-            == 1
-        )
+    list_display = ("email", "date_subscribed")
 
 
 @admin.register(Email)
