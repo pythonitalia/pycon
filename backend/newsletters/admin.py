7ab865eea0fd194992a3fdbd74a70a38e77243c8
Etty <beltrami.ester@gmail.com>
2020-01-05 18:16:39 +0000

    Add Button "Send Now" for each mail

--- backend/newsletters/admin.py
+++ backend/newsletters/admin.py
@@ -1,4 +1,8 @@
+from django.conf.urls import url
 from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Email, Subscription
 
@@ -10,18 +14,46 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
 @admin.register(Email)
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ("subject", "recipients_type", "scheduled_date")
-    readonly_fields = ("recipients",)
+    list_display = ("subject", "recipients_type", "scheduled_date", "email_actions")
+    readonly_fields = ("recipients", "email_actions")
     actions = ["send_emails"]
 
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                r"^(?P<email_id>.+)/send_email/$",
+                self.admin_site.admin_view(self.send_email),
+                name="send_email",
+            )
+        ]
+        return custom_urls + urls
+
+    def email_actions(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Send Now!!</a>&nbsp;',
+            reverse("admin:send_email", args=[obj.pk]),
+        )
+
+    email_actions.short_description = "Email Actions"
+    email_actions.allow_tags = True
+
+    def send_email(self, request, email_id):
+        self._send_emails(request, [Email.objects.get(pk=email_id)])
+        return HttpResponseRedirect(reverse("admin:newsletters_email_changelist"))
+
     def send_emails(self, request, queryset):
+        self._send_emails(request, queryset)
+
+    def _send_emails(self, request, emails):
 
         results = []
-        for email in queryset:
+        for email in emails:
             results.append(email.send_email())
 
         if all(results):
-            self.message_user(request, f"Successfully sent mails.")
+            subjects = "', '".join([e.subject for e in emails])
+            self.message_user(request, f"Successfully sent '{subjects}' Email(s).")
         else:
             self.message_user(
                 request, "Mmmm... Something went wrong :(", level=messages.ERROR
--- backend/newsletters/tests/test_admin.py
+++ backend/newsletters/tests/test_admin.py
@@ -1,5 +1,8 @@
 import pytest
+from django.contrib.admin.sites import AdminSite
 from django.urls import reverse
+from newsletters.admin import EmailAdmin
+from newsletters.models import Email
 
 NEWSLETTER_EMAIL_URL = "admin:newsletters_email_changelist"
 
@@ -10,6 +13,17 @@ def post_changelist(admin_client, admin_user):
 
     def wrapper(**kwargs):
         resp = admin_client.post(reverse(NEWSLETTER_EMAIL_URL), kwargs)
+        return resp
+
+    return wrapper
+
+
+@pytest.fixture
+def post_send_email(admin_client, admin_user):
+    admin_client.force_login(admin_user)
+
+    def wrapper(pk, **kwargs):
+        resp = admin_client.post(reverse("admin:send_email", args=[pk]), kwargs)
         return resp
 
     return wrapper
@@ -31,3 +45,21 @@ def test_send_email_failed(email_factory, mocker, post_changelist):
 
     resp = post_changelist(action="send_emails", _selected_action=[email.pk])
     assert resp.status_code == 302
+
+
+@pytest.mark.django_db
+def test_send_single(email_factory, post_send_email):
+    email = email_factory()
+    resp = post_send_email(email.pk)
+    assert resp.status_code == 302
+
+
+@pytest.mark.django_db
+def test_actions(email_factory):
+    admin = EmailAdmin(Email, AdminSite())
+
+    email = email_factory()
+    assert (
+        admin.email_actions(email)
+        == f"""<a class="button" href="/admin/newsletters/email/{email.pk}/send_email/">Send Now!!</a>&nbsp;"""  # noqa
+    )
