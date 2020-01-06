75930d0dab23e377755b768cb6e1674b7ad8a369
Etty <beltrami.ester@gmail.com>
2020-01-06 23:33:38 +0000

    Log errors to Sentry

--- frontend/src/components/error-boundary/index.tsx
+++ frontend/src/components/error-boundary/index.tsx
@@ -3,21 +3,28 @@ import { isRedirect } from "@reach/router";
 import { Box } from "@theme-ui/components";
 import { Component } from "react";
 import { jsx } from "theme-ui";
+import * as Sentry from "@sentry/browser";
 
 export class ErrorBoundary extends Component<
   {},
   {
     errorInfo: any | null;
     error: string | null;
+    eventId: any | null;
   }
 > {
   constructor(props: Readonly<{}>) {
     super(props);
-
-    this.state = { error: null, errorInfo: null };
+    this.state = { error: null, errorInfo: null, eventId: null };
   }
 
   componentDidCatch(error: any, errorInfo: any) {
+    Sentry.withScope(scope => {
+      scope.setExtras(errorInfo);
+      const eventId = Sentry.captureException(error);
+      this.setState({ eventId });
+    });
+
     if (isRedirect(error)) {
       throw error;
     } else {
