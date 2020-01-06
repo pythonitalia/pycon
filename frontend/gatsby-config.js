9a9641f3e2a078d5de9190296afcd62827b61870
Etty <beltrami.ester@gmail.com>
2020-01-06 23:16:06 +0000

    Add Sentry Configuration

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,3 +1,5 @@
+const Sentry = require("@sentry/node");
+
 const proxy = require("http-proxy-middleware");
 require("dotenv").config({
   path: `.env`,
@@ -6,6 +8,12 @@ require("dotenv").config({
 const API_URL = process.env.API_URL || "http://127.0.0.1:8000/graphql";
 const API_TOKEN = process.env.API_TOKEN || "example-token";
 const CONFERENCE_CODE = process.env.CONFERENCE_CODE || "pycon-demo";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN || "",
+  maxBreadcrumbs: 50,
+  debug: true,
+});
 
 module.exports = {
   siteMetadata: {
