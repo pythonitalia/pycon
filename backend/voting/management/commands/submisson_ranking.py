548d78ebd628b44b0ba71b34d304fe86073a8415
Etty <beltrami.ester@gmail.com>
2020-01-15 05:03:26 +0000

    Count votes

--- backend/voting/management/commands/submisson_ranking.py
+++ backend/voting/management/commands/submisson_ranking.py
@@ -1,5 +1,6 @@
 from conferences.models import Conference
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Sum
 from submissions.models import Submission
 from voting.models import Vote
 
@@ -20,8 +21,56 @@ class Command(BaseCommand):
             f"{submissions.count()} talks | {users} users | {votes} votes"
         )
 
-    def ranking_submissions(self, submissions):
-        pass
+    def rank_submissions(self, submissions):
+        """Decide here how to rank submission (e,g. vote-engine)
+        for "now" simply Sums votes.
+
+        :param submissions: Submission queryset
+        :return: submissions sorted by ranking in descending order
+        :rtype: list of dictionary
+
+        Example of return:
+            [
+                {
+                    "submission_id": <pk>,
+                    "votes": N,
+                    "submission": Submission object
+                 },
+                 ...
+             ]
+        """
+
+        queryset = (
+            Vote.objects.filter(submission__in=submissions)
+            .values("submission_id")
+            .annotate(votes=Sum("value"))
+            .order_by("-votes")
+        )
+        # list(submissions)[0].votes.all().aggregate(total=Sum("value"))
+        return [
+            {"submission": submissions.get(pk=i["submission_id"]), **i}
+            for i in queryset
+        ]
+
+    def print_ranking(self, ranking):
+        for rank in ranking:
+            submission = rank["submission"]
+            votes = rank["votes"]
+            langs = [l.code for l in submission.languages.all()]
+            msg = " | ".join(
+                [
+                    str(submission.pk),
+                    str(votes),
+                    submission.type.name,
+                    str(submission.duration.duration),
+                    submission.title,
+                    submission.topic.name,
+                    " ".join(langs),
+                    submission.speaker.full_name,
+                    submission.speaker.gender,
+                ]
+            )
+            self.stdout.write(msg)
 
     def handle(self, *args, **options):
         self.stdout.write("Starting...")
@@ -31,7 +80,8 @@ class Command(BaseCommand):
         except Conference.DoesNotExist:
             raise CommandError(f'Conference "{options["conference"]}" does not exist')
 
-        submissions = Submission.objects.filter(conference=conference)
+        submissions = Submission.objects.filter(conference_id=conference.id)
 
         self.print_counts(submissions)
-        self.ranking_submissions(submissions)
+        ranking = self.rank_submissions(submissions)
+        self.print_ranking(ranking)
--- backend/voting/tests/test_submission_ranking.py
+++ backend/voting/tests/test_submission_ranking.py
@@ -3,6 +3,8 @@ import random
 
 import pytest
 from django.core.management import CommandError, call_command
+from submissions.models import Submission
+from voting.management.commands.submisson_ranking import Command
 from voting.models import Vote
 
 
@@ -11,26 +13,46 @@ def random_vote():
 
 
 @pytest.fixture
-def setup_case(conference_factory, submission_factory, user_factory, vote_factory):
+def _setup(conference_factory, submission_factory, user_factory, vote_factory):
     conference = conference_factory()
-    submissions = submission_factory.create_batch(5, conference=conference)
-    users = user_factory.create_batch(5)
 
+    SUBMISSION_NUMBER = random.randint(1, 10)
+    USERS_NUMBER = random.randint(5, 20)
+    submissions = submission_factory.create_batch(
+        SUBMISSION_NUMBER, conference=conference
+    )
+    users = user_factory.create_batch(USERS_NUMBER)
+
+    counts_votes = {}
     for submission in submissions:
-
+        counts_votes[submission.pk] = 0
         for user in users:
             # make more realistic: skip some voting...
             if bool(random.getrandbits(1)):
                 continue
+
             value = random_vote()
             vote_factory(user=user, value=value, submission=submission)
+            counts_votes[submission.pk] += value
 
-    return conference
+    return conference, counts_votes
 
 
 @pytest.mark.django_db
-def test_submission_ranking(setup_case):
-    conference = setup_case
+def test_votes_counts(_setup):
+    conference, votes_counts = _setup
+
+    submissions = Submission.objects.filter(conference_id=conference.id)
+    cmd = Command()
+    ranked_submissions = cmd.rank_submissions(submissions)
+    assert ranked_submissions
+    for rank in ranked_submissions:
+        assert votes_counts[rank["submission_id"]] == rank["votes"]
+
+
+@pytest.mark.django_db
+def test_submission_ranking(_setup):
+    conference, _ = _setup
 
     call_command("submisson_ranking", conference.code)
 
