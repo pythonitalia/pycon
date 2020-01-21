2364224bb0046f850d98212cd17af99eb8d01d4c
Patrick Arminio <patrick.arminio@gmail.com>
2020-01-21 14:50:49 +0000

    Update URLs

--- backend/pycon/urls.py
+++ backend/pycon/urls.py
@@ -9,16 +9,20 @@ from submissions.views import SubmissionAutocomplete
 from users.views import UserAutocomplete
 
 urlpatterns = [
+    path(
+        "admin/_/user-autocomplete",
+        UserAutocomplete.as_view(),
+        name="user-autocomplete",
+    ),
+    path(
+        "admin/_/submission-autocomplete",
+        SubmissionAutocomplete.as_view(),
+        name="submission-autocomplete",
+    ),
     path("admin/", admin.site.urls),
     path(
         "graphql", csrf_exempt(CustomGraphQLView.as_view(schema=schema)), name="graphql"
     ),
     path("user/", include("users.urls")),
     path("", include("social_django.urls", namespace="social")),
-    path("_/user-autocomplete", UserAutocomplete.as_view(), name="user-autocomplete"),
-    path(
-        "_/submission-autocomplete",
-        SubmissionAutocomplete.as_view(),
-        name="submission-autocomplete",
-    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
