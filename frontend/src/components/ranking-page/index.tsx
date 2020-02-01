05f485771ae352d649955620a4bbb53378d57f55
Etty <beltrami.ester@gmail.com>
2020-02-01 17:58:09 +0000

    Improve layout

--- frontend/src/components/ranking-page/index.tsx
+++ frontend/src/components/ranking-page/index.tsx
@@ -11,6 +11,7 @@ import {
   RankingSubmissionQueryVariables,
 } from "../../generated/graphql-backend";
 import { Alert } from "../alert";
+import { Link } from "../link";
 import { MetaTags } from "../meta-tags";
 import { SubmissionAccordion } from "../voting-page/submission-accordion";
 import RANKING_SUBMISSION from "./ranking-submissions.graphql";
@@ -83,13 +84,85 @@ export const RankingPage: React.SFC<RouteComponentProps> = ({ location }) => {
           }}
         >
           {data?.conference.ranking.map((submission, index) => (
-            <Text
+            <Box
+              as="li"
               sx={{
-                py: 3,
+                background: "lightBlue",
+                overflow: "hidden",
               }}
             >
-              {submission.submission.title}
-            </Text>
+              <Box
+                sx={{
+                  borderTop: "primary",
+                }}
+              >
+                <Grid
+                  sx={{
+                    maxWidth: "container",
+                    mx: "auto",
+                    px: 3,
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    cursor: "pointer",
+                    gridTemplateColumns: [`40px 1fr 150px 200px`],
+                    "svg + svg": {
+                      marginLeft: [0, 1],
+                      marginTop: [1, 0],
+                    },
+                  }}
+                >
+                  <Box>
+                    <Text
+                      variant="label"
+                      sx={{
+                        fontWeight: "bold",
+                        py: 3,
+                        visibility: ["hidden", "visible"],
+                      }}
+                    >
+                      {submission.absoluteRank}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Text
+                      sx={{
+                        py: 3,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Link
+                        variant="heading"
+                        href={`/:language/submission/${submission.submission.id}`}
+                      >
+                        {submission.submission.title}
+                      </Link>
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Text
+                      sx={{
+                        py: 3,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {submission.submission.topic?.name}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Text
+                      sx={{
+                        py: 3,
+                        fontWeight: "bold",
+                        color: "violet",
+                        textTransform: "uppercase",
+                      }}
+                    >
+                      {submission.submission.speaker.fullName}
+                    </Text>
+                  </Box>
+                </Grid>
+              </Box>
+            </Box>
           ))}
         </Box>
       )}
