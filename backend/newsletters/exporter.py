8405d7e3fc079e4a615bc6aad51d9638b50c1142
Patrick Arminio <patrick.arminio@gmail.com>
2020-02-21 23:25:59 +0000

    Add more export functionalities

--- backend/newsletters/exporter.py
+++ backend/newsletters/exporter.py
@@ -1,6 +1,9 @@
 import dataclasses
 import typing
 
+from django.db.models import Q
+from schedule.models import ScheduleItem
+from submissions.models import Submission
 from users.models import User
 
 
@@ -17,13 +20,20 @@ class Endpoint:
 
 
 def convert_user_to_endpoint(user: User) -> Endpoint:
+    has_sent_submission_to = Submission.objects.filter(speaker=user).values_list(
+        "conference__code", flat=True
+    )
+    has_item_in_schedule = ScheduleItem.objects.filter(
+        Q(submission__speaker=user) | Q(additional_speakers=user)
+    ).values_list("conference__code", flat=True)
+
     return Endpoint(
         id=str(user.id),
         name=user.name,
         full_name=user.full_name,
         email=user.email,
         is_staff=user.is_staff,
-        has_sent_submission_to=[],
-        has_item_in_schedule=[],
+        has_sent_submission_to=list(has_sent_submission_to),
+        has_item_in_schedule=list(has_item_in_schedule),
         has_cancelled_talks=[],
     )
