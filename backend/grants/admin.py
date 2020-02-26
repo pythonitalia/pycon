35e662e8fdf65a1ef79ece9c3f6f01a823c1058f
Patrick Arminio <patrick.arminio@gmail.com>
2020-02-26 14:11:42 +0000

    Better grants admin

--- backend/grants/admin.py
+++ backend/grants/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import Grant
 
-admin.site.register(Grant)
+
+@admin.register(Grant)
+class GrantAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "full_name",
+        "travelling_from",
+        "age",
+        "gender",
+        "occupation",
+        "grant_type",
+        "python_usage",
+        "been_to_other_events",
+        "interested_in_volunteering",
+        "needs_funds_for_travel",
+        "why",
+        "notes",
+    )
