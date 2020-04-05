fd5a2e0c546c3f724888485012d0b1dc9afa3234
Etty <beltrami.ester@gmail.com>
2020-04-05 18:27:21 +0100

    Add room filter to slots

--- backend/api/tests/schema/conference/test_days.py
+++ backend/api/tests/schema/conference/test_days.py
@@ -177,3 +177,88 @@ def test_get_days_items(
 
     assert slots[0]["items"][0]["image"]
     assert slots[1]["items"][0]["image"] is None
+
+
+@mark.django_db
+def test_filter_days_by_room(
+    conference_factory,
+    day_factory,
+    slot_factory,
+    graphql_client,
+    schedule_item_factory,
+    room_factory,
+):
+
+    conference = conference_factory(start=date(2020, 4, 2), end=date(2020, 4, 2))
+
+    day = day_factory(conference=conference, day=date(2020, 4, 2))
+
+    slot = slot_factory(day=day, hour=time(8, 45), duration=60)
+    slot_2 = slot_factory(day=day, hour=time(9, 45), duration=60)
+
+    room = room_factory(name="Papa John's", conference=conference)
+    room_2 = room_factory(name="Sushi", conference=conference)
+
+    schedule_item_factory(slot=slot, rooms=[room])
+    item_2 = schedule_item_factory(slot=slot_2, image=None, rooms=[room, room_2])
+
+    resp = graphql_client.query(
+        """
+        query($code: String!, $room: ID) {
+            conference(code: $code) {
+                days {
+                    slots (room: $room) {
+                        items {
+                            id
+                        }
+                    }
+                }
+            }
+        }
+        """,
+        variables={"code": conference.code, "room": room_2.id},
+    )
+
+    assert "errors" not in resp
+    items = resp["data"]["conference"]["days"][0]["slots"][0]["items"]
+
+    assert len(items) == 1
+    assert items[0]["id"] == str(item_2.id)
+
+
+@mark.django_db
+def test_filter_days_by_room_not_found(
+    conference_factory,
+    day_factory,
+    slot_factory,
+    graphql_client,
+    schedule_item_factory,
+    room_factory,
+):
+
+    conference = conference_factory(start=date(2020, 4, 2), end=date(2020, 4, 2))
+
+    day = day_factory(conference=conference, day=date(2020, 4, 2))
+
+    slot_factory(day=day, hour=time(8, 45), duration=60)
+    slot_factory(day=day, hour=time(9, 45), duration=60)
+
+    resp = graphql_client.query(
+        """
+        query($code: String!) {
+            conference(code: $code) {
+                days {
+                    slots (room: 1) {
+                        items {
+                            id
+                        }
+                    }
+                }
+            }
+        }
+        """,
+        variables={"code": conference.code},
+    )
+
+    assert "errors" not in resp
+    assert len(resp["data"]["conference"]["days"][0]["slots"]) == 0
