3a69af769e6c7616078d06c8dd1f17b434a4fb6d
Marco Acierno <marcoaciernoemail@gmail.com>
2020-12-20 18:23:55 +0000

    Reduuce traces_sample_rate to 0.2

--- backend/pycon/settings/prod.py
+++ backend/pycon/settings/prod.py
@@ -17,7 +17,7 @@ if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.2,
         send_default_pii=True,
     )
 
