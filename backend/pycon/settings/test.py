32d229c79af4bfecf8d72387d019b20959efcc22
Marco Acierno <marcoaciernoemail@gmail.com>
2020-12-23 19:27:43 +0000

    Remove legacy Stripe code (#1959)
    We do not use Stripe directly anymore, all tickets
    are sold via Pretix, so it doesn't make
    much sense to keep the old code and
    dependency here

--- backend/pycon/settings/test.py
+++ backend/pycon/settings/test.py
@@ -1,7 +1,4 @@
-from django.core.exceptions import ImproperlyConfigured
-
 from .base import *  # noqa
-from .base import STRIPE_SECRET_KEY
 
 SECRET_KEY = "this-key-should-only-be-used-for-tests"
 SLACK_INCOMING_WEBHOOK_URL = ""
@@ -9,6 +6,3 @@ USE_SCHEDULER = False
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-
-if not STRIPE_SECRET_KEY.startswith("sk_test_"):
-    raise ImproperlyConfigured("Stripe key does not seem a test key")
