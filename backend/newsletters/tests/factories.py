09cf477576bd714dae411d7d7c94a890ba45c456
Marco Acierno <marcoaciernoemail@gmail.com>
2020-12-24 10:27:21 +0000

    Remove unused Email model
    1. We do not use this model
    2. Has an hard-dependency on postgres, which we would like to remove
    
    Closes #1953

--- backend/newsletters/tests/factories.py
+++ backend/newsletters/tests/factories.py
@@ -1,7 +1,6 @@
 import factory
-import pytz
 from factory.django import DjangoModelFactory
-from newsletters.models import Email, Subscription
+from newsletters.models import Subscription
 from pytest_factoryboy import register
 
 
@@ -12,26 +11,3 @@ class SubscriptionFactory(DjangoModelFactory):
         django_get_or_create = ("email",)
 
     email = factory.Faker("email")
-
-
-@register
-class EmailFactory(DjangoModelFactory):
-    class Meta:
-        model = Email
-
-    subject = factory.Faker("sentence")
-    heading = factory.Faker("sentence", nb_words=3)
-    body = factory.Faker("text")
-    cta_label = factory.Faker("word")
-    cta_link = factory.Faker("url")
-    recipients_type = "newsletters"
-    scheduled_date = factory.Faker("future_datetime", tzinfo=pytz.UTC)
-
-    @factory.post_generation
-    def recipients(self, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if extracted:
-            for email in extracted:
-                self.recipients.append(email)
