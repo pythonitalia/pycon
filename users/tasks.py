3c53c6c2c50f91973b96e2b85b901a8e540ffd0a
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-28 03:09:03 +0100

    Keep colors when running pyinvoke

--- users/tasks.py
+++ users/tasks.py
@@ -3,14 +3,14 @@ from invoke import task
 
 @task
 def makemigrations(c, name):
-    c.run(f'alembic revision --autogenerate -m "{name}"')
+    c.run(f'alembic revision --autogenerate -m "{name}"', pty=True)
 
 
 @task
 def migrate(c):
-    c.run("alembic upgrade head")
+    c.run("alembic upgrade head", pty=True)
 
 
 @task
 def server(c):
-    c.run("uvicorn main:app --reload")
+    c.run("uvicorn main:app --reload", pty=True)
