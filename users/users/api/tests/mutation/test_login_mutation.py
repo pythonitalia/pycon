2049129ca8d2446e48a5bfb9fe018d3611d3bca6
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-31 20:00:25 +0100

    Login errors

--- users/users/api/tests/mutation/test_login_mutation.py
+++ users/users/api/tests/mutation/test_login_mutation.py
@@ -1,11 +1,12 @@
+from ward import test
+
 from users.api.tests.graphql_client import graphql_client
 from users.tests.factories import user_factory
 from users.tests.session import db
-from ward import test
 
 
 @test("cannot login with non existent user")
-async def _(graphql_client=graphql_client):
+async def _(graphql_client=graphql_client, db=db):
     query = """
     mutation($input: LoginInput!) {
         login(input: $input) {
@@ -19,6 +20,87 @@ async def _(graphql_client=graphql_client):
 
     assert not response.errors
     assert response.data["login"]["__typename"] == "UsernameAndPasswordCombinationWrong"
+
+
+@test("cannot login with empty password")
+async def _(graphql_client=graphql_client, db=db):
+    query = """
+    mutation($input: LoginInput!) {
+        login(input: $input) {
+            __typename
+            ... on LoginValidationErrorValidationError {
+                errors {
+                    password {
+                        message
+                        type
+                    }
+                    email {
+                        message
+                        type
+                    }
+                }
+            }
+        }
+    }
+    """
+    response = await graphql_client.query(
+        query, variables={"input": {"email": "hah@pycon.it", "password": ""}}
+    )
+
+    assert not response.errors, response.errors
+    assert response.data["login"] == {
+        "__typename": "LoginValidationErrorValidationError",
+        "errors": {
+            "password": [
+                {
+                    "message": "ensure this value has at least 1 characters",
+                    "type": "value_error.any_str.min_length",
+                }
+            ],
+            "email": None,
+        },
+    }
+
+
+@test("cannot login with empty email")
+async def _(graphql_client=graphql_client, db=db):
+    query = """
+    mutation($input: LoginInput!) {
+        login(input: $input) {
+            __typename
+            ... on LoginValidationErrorValidationError {
+                errors {
+                    password {
+                        message
+                        type
+                    }
+                    email {
+                        message
+                        type
+                    }
+                }
+            }
+        }
+    }
+    """
+    response = await graphql_client.query(
+        query, variables={"input": {"email": "", "password": "password"}}
+    )
+
+    assert not response.errors, response.errors
+    assert not response.errors, response.errors
+    assert response.data["login"] == {
+        "__typename": "LoginValidationErrorValidationError",
+        "errors": {
+            "email": [
+                {
+                    "message": "value is not a valid email address",
+                    "type": "value_error.email",
+                }
+            ],
+            "password": None,
+        },
+    }
 
 
 @test("cannot login with wrong password")
