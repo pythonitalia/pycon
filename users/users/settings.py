db2acc3088d7b9815d58f0e2f2eb0449cfe5a441
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-31 16:47:44 +0100

    Unit tests and DB setup

--- users/users/settings.py
+++ users/users/settings.py
@@ -1,3 +1,4 @@
+from sqlalchemy.engine.url import URL, make_url
 from starlette.config import Config
 from starlette.datastructures import Secret
 
@@ -20,4 +21,16 @@ PASSWORD_HASHERS = [
 RUNNING_TESTS = config("RUNNING_TESTS", cast=bool, default=False)
 
 if RUNNING_TESTS:
+    original_url = make_url(DATABASE_URL)
+    test_db_url = URL.create(
+        drivername=original_url.drivername,
+        username=original_url.username,
+        password=original_url.password,
+        host=original_url.host,
+        port=original_url.port,
+        database=f"TEST_{original_url.database}",
+        query=original_url.query,
+    )
+
+    DATABASE_URL = test_db_url
     PASSWORD_HASHERS = ["users.starlette_password.plain_hasher.PlainPasswordHasher"]
