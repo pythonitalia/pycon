99a723f558b52272ca02d85efeb7697b1792f266
Ester Beltrami <ester@esters-mbp.lan>
2021-02-06 16:21:02 +0000

    WIP setup frontend association

---
+++ association-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./pages/**/*.ts", "./components/**/*.ts"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
