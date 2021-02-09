ecf108b250917f9f6611f762967969648c6754d2
Marco Acierno <marcoaciernoemail@gmail.com>
2021-02-10 00:40:20 +0100

    lato font

--- association-admin/src/pages/_document.tsx
+++ association-admin/src/pages/_document.tsx
@@ -10,7 +10,13 @@ export default class MyDocument extends Document<{
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- association-admin/tailwind.config.js
+++ association-admin/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["Helvetica"],
-      serif: ["Helvetica"],
+      sans: ["Lato"],
+      serif: ["Lato"],
       mono: ["Menlo, monospace"],
     },
     extend: {},
