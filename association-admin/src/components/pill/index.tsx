6c370076426cc8a230a0d586bf90dff645e509e6
Marco Acierno <marcoaciernoemail@gmail.com>
2021-02-10 02:08:16 +0100

    Disable select text on the pill

--- association-admin/src/components/pill/index.tsx
+++ association-admin/src/components/pill/index.tsx
@@ -12,7 +12,7 @@ export const Pill: React.FC<Props> = ({
 }) => (
   <span
     className={classnames(
-      "ml-2 px-2 inline-flex text-xs leading-5 font-semibold rounded-full",
+      "ml-2 px-2 inline-flex text-xs leading-5 font-semibold rounded-full select-none",
       {
         "bg-green-100 text-green-800": variant === "success",
         "bg-red-100 text-red-800": variant === "warning",
