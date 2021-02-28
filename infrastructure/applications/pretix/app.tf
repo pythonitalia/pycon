7bd47c038f3ed6120808bfca97e3251203116cbf
Marco Acierno <marcoaciernoemail@gmail.com>
2021-02-28 01:24:34 +0100

    Refactor pretix module

--- infrastructure/applications/pretix/app.tf
+++ infrastructure/applications/pretix/app.tf
@@ -1,35 +1,10 @@
+data "aws_db_instance" "database" {
+  db_instance_identifier = "terraform-20190815202105324300000001"
+}
+
 resource "aws_elastic_beanstalk_application" "app" {
   name        = "pretix"
   description = "pretix"
-}
-
-data "aws_vpc" "default" {
-  filter {
-    name   = "tag:Name"
-    values = ["pythonit-vpc"]
-  }
-}
-
-data "aws_security_group" "rds" {
-  name = "pythonit-rds-security-group"
-}
-
-data "aws_subnet" "public" {
-  vpc_id = data.aws_vpc.default.id
-
-  filter {
-    name   = "tag:Type"
-    values = ["public"]
-  }
-
-  filter {
-    name   = "tag:AZ"
-    values = ["eu-central-1a"]
-  }
-}
-
-data "aws_db_instance" "database" {
-  db_instance_identifier = "terraform-20190815202105324300000001"
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
