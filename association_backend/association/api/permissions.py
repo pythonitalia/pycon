b6d9719d33b29e723258a312e274763280378bb2
madeng84 <gaetanodonghia@gmail.com>
2021-03-13 18:16:04 +0100

    IsJWTAvailable removed

--- association_backend/association/api/permissions.py
+++ association_backend/association/api/permissions.py
@@ -1,10 +1,1 @@
-from association.api.context import Info
-from association.settings import JWT_USERS_COOKIE_NAME
-from strawberry.permission import BasePermission
-
-
-class IsJWTAvailable(BasePermission):
-    message = "JWT Not Available"
-
-    def has_permission(self, source, info: Info, **kwargs):
-        return info.context.request.cookies.get(JWT_USERS_COOKIE_NAME, None) is not None
+# put here your permissions
