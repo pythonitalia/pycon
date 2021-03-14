9e3723095530746f7dbb85dfcf98f6878463e04d
madeng84 <gaetanodonghia@gmail.com>
2021-03-14 19:07:43 +0100

    removed unuseful PASTAPORTO_ACTION_xxx vars

--- association_backend/association/api/context.py
+++ association_backend/association/api/context.py
@@ -1,7 +1,6 @@
 import typing
 from dataclasses import dataclass
 
-from pythonit_toolkit.pastaporto.actions import PastaportoAction
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette.websockets import WebSocket
@@ -13,7 +12,6 @@ from association.domain.repositories.association_repository import AssociationRe
 class Context:
     request: typing.Union[Request, WebSocket]
     session: AsyncSession
-    pastaporto_action: typing.Optional[PastaportoAction] = None
 
     @property
     def association_repository(self) -> AssociationRepository:
