7f588b49e17fb354f7e0a984c3efacab9762de74
Ester Beltrami <beltrami.ester@gmail.com>
2021-03-20 13:35:31 +0000

    Fix css class

--- association-frontend/components/hero/hero.tsx
+++ association-frontend/components/hero/hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-cover bg-center bg-local bg-pycon-group bg-opacity-80	bg-no-repeat pb-96"></div>
+    <div className="bg-cover bg-center bg-local bg-pycon-group bg-no-repeat pb-96 "></div>
   );
 };
 export default Hero;
