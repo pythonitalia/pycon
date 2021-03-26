837f55f479f0bb1ff4495a798f3fe83a776d1ec8
Ester Beltrami <beltrami.ester@gmail.com>
2021-03-26 19:05:37 +0000

    Get GraphQL url from .env file

--- association-frontend/next.config.js
+++ association-frontend/next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   async rewrites() {
+    const API_URL = process.env.API_URL
+      ? process.env.API_URL
+      : "http://localhost:4001/graphql";
+
     return [
       {
         source: "/graphql",
-        destination: `http://localhost:4000/graphql`,
+        destination: GATEWAY_URL,
       },
     ];
   },
