e5d8979ee1be19aef926307fa13b5c1d7712ed04
Marco Acierno <marcoaciernoemail@gmail.com>
2021-03-28 12:18:44 +0100

    Move get email backend logic into toolkit

---
+++ toolkit/pythonit_toolkit/emails/__init__.py
@@ -0,0 +1,3 @@
+from .utils import get_email_backend
+
+__all__ = ["get_email_backend"]
--- users-backend/users/emails.py
+++ users-backend/users/emails.py
@@ -1,24 +1,8 @@
-import importlib
 from typing import Optional
 
-from pythonit_toolkit.emails.backends.base import EmailBackend
+from pythonit_toolkit.emails import get_email_backend
 from pythonit_toolkit.emails.templates import EmailTemplate
-
 from users.settings import DEFAULT_EMAIL_FROM, EMAIL_BACKEND, ENVIRONMENT
-
-EMAIL_BACKEND_INSTANCE: Optional[EmailBackend] = None
-
-
-def get_email_backend() -> EmailBackend:
-    global EMAIL_BACKEND_INSTANCE
-
-    if not EMAIL_BACKEND_INSTANCE:
-        module_path, class_name = EMAIL_BACKEND.rsplit(".", 1)
-        module = importlib.import_module(module_path)
-        class_impl = getattr(module, class_name)
-        EMAIL_BACKEND_INSTANCE = class_impl(environment=ENVIRONMENT)
-
-    return EMAIL_BACKEND_INSTANCE
 
 
 def send_email(
@@ -30,7 +14,7 @@ def send_email(
     variables: Optional[dict[str, str]] = None
 ):
     from_ = from_ or DEFAULT_EMAIL_FROM
-    backend = get_email_backend()
+    backend = get_email_backend(EMAIL_BACKEND, environment=ENVIRONMENT)
     backend.send_email(
         template=template, from_=from_, to=to, subject=subject, variables=variables
     )
