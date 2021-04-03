78e621aca1b235e505978e591de1d7de0bf7aa67
Marco Acierno <marcoaciernoemail@gmail.com>
2021-04-03 15:19:42 +0100

    Remove comments

--- users-backend/users/domain/services/__init__.py
+++ users-backend/users/domain/services/__init__.py
@@ -5,19 +5,14 @@ from .reset_password import ResetPasswordInput, reset_password
 from .social_login import SocialAccount, SocialLoginInput, social_login
 
 __all__ = [
-    # login
     "login",
     "LoginInputModel",
-    # register
     "register",
     "RegisterInputModel",
-    # social account
     "social_login",
     "SocialLoginInput",
     "SocialAccount",
-    # reset_password
     "reset_password",
     "ResetPasswordInput",
-    # request_reset_password
     "request_reset_password",
 ]
