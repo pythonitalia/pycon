288dd0ac9344a2e183a467313f52141e08d22e76
Patrick Arminio <patrick.arminio@gmail.com>
2021-04-05 21:39:48 +0100

    Another Marquee fix

--- styleguide/src/marquee/marquee.tsx
+++ styleguide/src/marquee/marquee.tsx
@@ -7,10 +7,13 @@ type Props = {
 export const Marquee = ({ children }: Props) => (
   <div className="overflow-hidden w-full">
     <div className="motion-safe:animate-marquee p-8 text-3xl whitespace-nowrap">
-      <div className="inline-block">{children}</div>
+      <div className="inline-block whitespace-nowrap">{children}</div>
 
       {new Array(20).fill(null).map((_, index) => (
-        <div key={index} className="inline-block ml-4 not-sr-only">
+        <div
+          key={index}
+          className="inline-block ml-4 not-sr-only whitespace-nowrap"
+        >
           /{" "}
           {React.Children.map(children, (child) => {
             if (React.isValidElement(child)) {
