c0db97c6fe6dddc26093ee9bc711d4c051db1d3d
Marco Acierno <marcoaciernoemail@gmail.com>
2021-04-18 14:58:05 +0100

    Move unused code

--- association-frontend/src/components/sections/sections.tsx
+++ association-frontend/src/components/sections/sections.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import SectionEvents from "~/components/section-events/section-events";
+
 import SectionJoin from "~/components/section-join/section-join";
 import SectionPythonIta from "~/components/section-python-ita/section-python-ita";
-import SectionSocialMedia from "~/components/section-social-media/section-social-media";
-import Modal from "../modal/modal";
 
 import SectionPyConIta from "../section-pycon-ita/section-pycon-ita";
 
