37016eaa850f7436a0f02f4dfcf26d7443ffdb74
Marco Acierno <marcoaciernoemail@gmail.com>
2021-04-18 02:16:31 +0100

    Fix CI

--- association-backend/scripts/prepare_db.py
+++ association-backend/scripts/prepare_db.py
@@ -1,24 +1,26 @@
 # isort: off
-# flake8: noqa
 import asyncio
 import os
-import sys  # noqa
+import sys
+from sqlalchemy.engine import create_engine
+
+from sqlalchemy.engine.url import make_url  # noqa
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 
-import sqlalchemy
-from db_utils import create_database, database_exists  # noqa
+from db_utils import create_database, database_exists
 
 from database.db import metadata
 from association.settings import DATABASE_URL
 
 
 async def run():
-    engine = sqlalchemy.create_engine(DATABASE_URL)
+    db_name = make_url(DATABASE_URL).database
+    sync_database_url = DATABASE_URL.replace("asyncpg", "psycopg2").replace("TEST_", "")
+    engine = create_engine(sync_database_url)
 
-    if not database_exists(DATABASE_URL):
-        create_database(DATABASE_URL)
-
+    if not database_exists(sync_database_url, db_name):
+        create_database(sync_database_url, db_name)
         metadata.create_all(engine)
     else:
         metadata.drop_all(engine)
