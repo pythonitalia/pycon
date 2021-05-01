f604dbc1a9c22476fd78441ed81bdae2a69a1ca2
Patrick Arminio <patrick.arminio@gmail.com>
2021-05-01 15:47:52 +0100

    Fix header

--- styleguide/src/header/header.tsx
+++ styleguide/src/header/header.tsx
@@ -4,7 +4,7 @@ import { MenuButton } from "../menu-button/menu-button";
 
 export const Header = () => (
   <header className="p-8 flex justify-between max-w-7xl mx-auto">
-    <Logo />
+    <Logo className="w-40" />
     <MenuButton />
   </header>
 );
