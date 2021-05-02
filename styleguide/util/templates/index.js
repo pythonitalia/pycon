5bb31b2f3aac19d490c65249a51381fa6ed3ef04
Patrick Arminio <patrick.arminio@gmail.com>
2021-05-02 16:49:34 +0100

    Update readme and remove command to generate components

--- styleguide/util/templates/index.js
+++
@@ -1,5 +0,0 @@
-const component = require("./component");
-const componentStories = require("./component.stories");
-const componentTests = require("./component.test");
-
-module.exports = [component, componentStories, componentTests];
