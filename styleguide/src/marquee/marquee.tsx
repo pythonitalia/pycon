ebc56400d44f47fdda78f628b1cba8556fde0172
Patrick Arminio <patrick.arminio@gmail.com>
2021-05-09 21:29:51 +0100

    Improve marquee and split section

--- styleguide/src/marquee/marquee.tsx
+++ styleguide/src/marquee/marquee.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  separator?: string | React.ReactNode;
 };
 
-export const Marquee = ({ children }: Props) => (
+export const Marquee = ({ children, separator = "/" }: Props) => (
   <div className="overflow-hidden w-full">
     <div className="motion-safe:animate-marquee p-8 text-3xl whitespace-nowrap">
       <div className="inline-block whitespace-nowrap">{children}</div>
@@ -14,7 +15,7 @@ export const Marquee = ({ children }: Props) => (
           key={index}
           className="inline-block ml-4 not-sr-only whitespace-nowrap"
         >
-          /{" "}
+          <span className="inline-block mr-4">{separator}</span>
           {React.Children.map(children, (child) => {
             if (React.isValidElement(child)) {
               return React.cloneElement(child);
