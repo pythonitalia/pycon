d01f8c767d0d016270f40bd927342588a86e55f5
Marco Acierno <marcoaciernoemail@gmail.com>
2021-05-15 16:19:20 +0200

    Only show admin users in pycon admin

--- backend/pycon/urls.py
+++ backend/pycon/urls.py
@@ -1,19 +1,14 @@
-from api.schema import schema
-from api.views import GraphQLView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
+
+from api.schema import schema
+from api.views import GraphQLView
 from submissions.views import SubmissionAutocomplete
-from users.views import UserAutocomplete
 
 urlpatterns = [
-    path(
-        "admin/_/user-autocomplete",
-        UserAutocomplete.as_view(),
-        name="user-autocomplete",
-    ),
     path(
         "admin/_/submission-autocomplete",
         SubmissionAutocomplete.as_view(),
