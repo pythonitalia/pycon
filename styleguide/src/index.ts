81147c9c8e17a755a825e4402e17c02751bee643
Patrick Arminio <patrick.arminio@gmail.com>
2021-05-15 19:06:26 +0100

    Update package

--- styleguide/src/index.ts
+++ styleguide/src/index.ts
@@ -6,4 +6,5 @@ export { Marquee } from "./marquee/marquee";
 export { Carousel } from "./carousel/carousel";
 export { SplitSection } from "./split-section/split-section";
 export { Schedule } from "./schedule/schedule";
+export { ScheduleProgram } from "./schedule/types";
 export { Colors } from "./colors/colors";
