bb38c916a09b5dc57c5e1b72d68b2ba867837db4
Marco Acierno <marcoaciernoemail@gmail.com>
2021-05-29 18:10:42 +0200

    Fix tsc on gateway

--- gateway/config.ts
+++ gateway/config.ts
@@ -11,6 +11,8 @@ export const {
 
   // Services
   USERS_SERVICE,
+  ASSOCIATION_BACKEND_SERVICE,
+  PYCON_BACKEND_SERVICE,
 
   // Other configuration
   IS_DEV = process.env.NODE_ENV === "development",
