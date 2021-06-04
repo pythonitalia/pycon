5d7ca9272d69739f8a2eb16b208f10c7ea70c22c
Patrick Arminio <patrick.arminio@gmail.com>
2021-06-04 17:52:59 +0100

    Support colors in paragraphs

--- styleguide/src/paragraph/paragraph.stories.tsx
+++ styleguide/src/paragraph/paragraph.stories.tsx
@@ -19,5 +19,11 @@ export const Primary = () => (
       saepe adipisci rerum, itaque minima voluptas quasi porro eius accusamus
       quo aspernatur laborum nam enim. Iusto iure doloribus molestias et.
     </Paragraph>
+
+    <Paragraph bold color="purple">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus vero
+      saepe adipisci rerum, itaque minima voluptas quasi porro eius accusamus
+      quo aspernatur laborum nam enim. Iusto iure doloribus molestias et.
+    </Paragraph>
   </div>
 );
--- styleguide/src/paragraph/paragraph.tsx
+++ styleguide/src/paragraph/paragraph.tsx
@@ -4,13 +4,31 @@ import React, { ReactNode } from "react";
 export const Paragraph = ({
   children,
   bold = false,
+  color = "black",
 }: {
   children: ReactNode;
   bold?: boolean;
+  color?:
+    | "aquamarine"
+    | "casablanca"
+    | "black"
+    | "orange"
+    | "keppel"
+    | "pink"
+    | "purple"
+    | "white";
 }) => (
   <p
     className={clsx("mb-8", {
       "font-bold": bold,
+      "text-aquamarine": color === "aquamarine",
+      "text-casablanca": color === "casablanca",
+      "text-black": color === "black",
+      "text-orange": color === "orange",
+      "text-keppel": color === "casablanca",
+      "text-pink": color === "pink",
+      "text-purple": color === "purple",
+      "text-white": color === "white",
     })}
   >
     {children}
