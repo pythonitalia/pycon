65661cf6f7c79a0f7d3b9acb972e3a1ab02fdda2
Marco Acierno <marcoaciernoemail@gmail.com>
2021-06-07 21:26:35 +0200

    Fix TS, add nodejs version, update version for pre-release

--- styleguide/src/ticket/ticket.tsx
+++ styleguide/src/ticket/ticket.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { TicketWrapper } from "./wrapper";
+
 import { Snape } from "./snape";
-import { Logo } from "../logo/logo";
 
 type TicketProps = {
   name: string;
@@ -10,31 +9,31 @@ type TicketProps = {
 export const Ticket = ({ name }: TicketProps) => {
   return (
     <div>
-      <div className=" max-w-sm  ">
+      <div className="max-w-sm ">
         <div className="flex max-w-sm">
-          <div className="flex border-solid border-4 border-black max-w-sm m-4 w-full">
-            <div className="w-24 min-w-min bg-purple border-solid border-r-2 border-black">
+          <div className="flex w-full max-w-sm m-4 border-4 border-black border-solid">
+            <div className="w-24 border-r-2 border-black border-solid min-w-min bg-purple">
               <Snape />
             </div>
-            <div className="flex flex-grow items-center justify-center">
-              <div className=" font-bold text-5xl">PyFest</div>
+            <div className="flex items-center justify-center flex-grow">
+              <div className="text-5xl font-bold ">PyFest</div>
             </div>
           </div>
         </div>
 
-        <div className="flex border-solid border-t-4 border-b-4 border-black bg-purple">
+        <div className="flex border-t-4 border-b-4 border-black border-solid bg-purple">
           <div className="ml-4">
-            <div className="font-bold text-5xl my-3">Ester Beltrami</div>
-            <div className="my-2 text-xl  text-white ">Made.com</div>
+            <div className="my-3 text-5xl font-bold">Ester Beltrami</div>
+            <div className="my-2 text-xl text-white ">Made.com</div>
             <div className="my-2 ">@etty</div>
           </div>
         </div>
 
-        <div className="flex  border-solid border-b-4 border-black">
+        <div className="flex border-b-4 border-black border-solid">
           <div className="w-1/3 bg-green ">
             <Snape />
           </div>
-          <div className="w-1/3 bg-aquamarine border-solid border-l-4 border-r-4 border-black ">
+          <div className="w-1/3 border-l-4 border-r-4 border-black border-solid bg-aquamarine ">
             <Snape />
           </div>
           <div className="w-1/3 bg-casablanca ">
