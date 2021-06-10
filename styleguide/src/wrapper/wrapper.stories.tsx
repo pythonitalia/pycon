798dce08168240a7f0d1c8d5b04ef5193ce27d44
Marco Acierno <marcoaciernoemail@gmail.com>
2021-06-10 23:40:15 +0200

    Remove centered wrapper

--- styleguide/src/wrapper/wrapper.stories.tsx
+++ styleguide/src/wrapper/wrapper.stories.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "../button/button";
 import { Paragraph } from "../paragraph/paragraph";
 import { Title } from "../title";
 import { Wrapper } from "./wrapper";
@@ -29,21 +28,5 @@ export const Primary = () => (
       saepe adipisci rerum, itaque minima voluptas quasi porro eius accusamus
       quo aspernatur laborum nam enim. Iusto iure doloribus molestias et.
     </Paragraph>
-  </Wrapper>
-);
-
-export const CenteredContent = () => (
-  <Wrapper center>
-    <div>
-      <Button onClick={() => {}} color="pink">
-        Button
-      </Button>
-      <Button onClick={() => {}} color="orange">
-        Button 2
-      </Button>
-      <Button onClick={() => {}} color="purple">
-        Button 3
-      </Button>
-    </div>
   </Wrapper>
 );
--- styleguide/src/wrapper/wrapper.tsx
+++ styleguide/src/wrapper/wrapper.tsx
@@ -1,17 +1,9 @@
-import clsx from "clsx";
 import React, { ReactNode } from "react";
 
 type Props = {
-  center?: boolean;
   children: ReactNode;
 };
 
-export const Wrapper = ({ children, center = false }: Props) => (
-  <div
-    className={clsx("max-w-7xl mx-auto px-8 py-8", {
-      "flex items-center justify-center": center,
-    })}
-  >
-    {children}
-  </div>
+export const Wrapper = ({ children }: Props) => (
+  <div className="px-8 py-8 mx-auto max-w-7xl">{children}</div>
 );
