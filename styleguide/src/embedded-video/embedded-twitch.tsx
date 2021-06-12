2e06ff52f0e1e5926155b5491bcedfdeddca2aa3
Etty <beltrami.ester@gmail.com>
2021-06-12 15:46:20 +0100

    Use tailwind aspect ratio plugin

--- styleguide/src/embedded-video/embedded-twitch.tsx
+++ styleguide/src/embedded-video/embedded-twitch.tsx
@@ -14,21 +14,17 @@ export const EmbeddedTwitch = ({
   parent = "localhost",
 }: EmbeddedTwitchProps) => {
   return (
-    <div
-      className={`flex items-center justify-center w-full h-full bg-${backgroundColor}`}
-    >
-      <div className="relative w-2/3 h-full pt-xl ">
+  <div className="aspect-w-16 aspect-h-9">
         <iframe
-          className="absolute top-0 w-full h-full"
+          // className="absolute top-0 w-full h-full"
           width="100%"
           height="100%"
           src={`https://player.twitch.tv/?channel=${channel}&parent=${parent}&autoplay=${autoplay}`}
-          title="YouTube video player"
+          title="Twitch video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen={true}
         ></iframe>
       </div>
-    </div>
   );
 };
