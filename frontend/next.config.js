467ef7a91d66d1edfcf963cf17754c806fe3dc29
Marco Acierno <marcoaciernoemail@gmail.com>
2021-06-25 22:33:18 +0200

    Sentry vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,10 +3,21 @@ const path = require("path");
 // const withSourceMaps = require("@zeit/next-source-maps");
 const { withSentryConfig } = require("@sentry/nextjs");
 
-const { CONFERENCE_CODE, API_URL, API_TOKEN } = process.env;
+const {
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  VERCEL_GITHUB_COMMIT_SHA,
+  CONFERENCE_CODE,
+  API_URL,
+  API_TOKEN,
+} = process.env;
 
 const SentryWebpackPluginOptions = {
   silent: true,
+  org: "python-italia",
+  project: SENTRY_PROJECT,
+  authToken: SENTRY_AUTH_TOKEN,
+  release: VERCEL_GITHUB_COMMIT_SHA,
 };
 
 module.exports = withSentryConfig(
