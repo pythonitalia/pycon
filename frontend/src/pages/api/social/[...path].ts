4b9afa80202f53814c698839da10edcec08b5598
Marco Acierno <marcoaciernoemail@gmail.com>
2021-06-26 19:56:47 +0200

    test

--- frontend/src/pages/api/social/[...path].ts
+++ frontend/src/pages/api/social/[...path].ts
@@ -1,78 +1,78 @@
-import { NowRequest, NowResponse } from "@now/node";
-import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
+// import { NowRequest, NowResponse } from "@now/node";
+// import chromium from "chrome-aws-lambda";
+// import puppeteer from "puppeteer-core";
 
-// import { CardType, getSize } from "~/helpers/social-card";
-export type CardType = "social" | "social-square" | "social-twitter";
+// // import { CardType, getSize } from "~/helpers/social-card";
+// export type CardType = "social" | "social-square" | "social-twitter";
 
-export const getSize = (cardType: CardType) => {
-  switch (cardType) {
-    case "social":
-      return { width: 1200, height: 630 };
-    case "social-twitter":
-      return { width: 1200, height: 600 };
-    case "social-square":
-      return { width: 1200, height: 1200 };
-  }
-};
+// export const getSize = (cardType: CardType) => {
+//   switch (cardType) {
+//     case "social":
+//       return { width: 1200, height: 630 };
+//     case "social-twitter":
+//       return { width: 1200, height: 600 };
+//     case "social-square":
+//       return { width: 1200, height: 1200 };
+//   }
+// };
 
-export default async (req: NowRequest, res: NowResponse) => {
-  const type = "png";
-  const userAgent = req.headers["user-agent"];
-  const path = (req.query.path as string[]).join("/");
-  const sizeName = getSizeNameForScraper(userAgent);
-  const url = `https://${req.headers.host}/${path}/social?card-type=${sizeName}`;
+// export default async (req: NowRequest, res: NowResponse) => {
+//   const type = "png";
+//   const userAgent = req.headers["user-agent"];
+//   const path = (req.query.path as string[]).join("/");
+//   const sizeName = getSizeNameForScraper(userAgent);
+//   const url = `https://${req.headers.host}/${path}/social?card-type=${sizeName}`;
 
-  console.log("Taking screenshot of", url);
+//   console.log("Taking screenshot of", url);
 
-  try {
-    const size = getSize(sizeName);
+//   try {
+//     const size = getSize(sizeName);
 
-    const browser = await puppeteer.launch({
-      // Required
-      executablePath: await chromium.executablePath,
+//     const browser = await puppeteer.launch({
+//       // Required
+//       executablePath: await chromium.executablePath,
 
-      // Optional
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      headless: true,
-    });
+//       // Optional
+//       args: chromium.args,
+//       defaultViewport: chromium.defaultViewport,
+//       headless: true,
+//     });
 
-    const page = await browser.newPage();
-    await page.setViewport({ ...size, deviceScaleFactor: 2 });
-    await page.goto(url);
-    await page.waitForSelector("#social-card");
+//     const page = await browser.newPage();
+//     await page.setViewport({ ...size, deviceScaleFactor: 2 });
+//     await page.goto(url);
+//     await page.waitForSelector("#social-card");
 
-    const file = await page.screenshot();
+//     const file = await page.screenshot();
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", `image/${type}`);
-    res.end(file);
-  } catch (e) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Server Error</h1><p>Sorry, there was a problem</p>");
-    console.error(e.message);
-  }
-};
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", `image/${type}`);
+//     res.end(file);
+//   } catch (e) {
+//     res.statusCode = 500;
+//     res.setHeader("Content-Type", "text/html");
+//     res.end("<h1>Server Error</h1><p>Sorry, there was a problem</p>");
+//     console.error(e.message);
+//   }
+// };
 
-export type Scraper = "twitter" | "generic";
+// export type Scraper = "twitter" | "generic";
 
-const getSizeNameForScraper = (userAgent: string): CardType => {
-  const scraper = getScraperName(userAgent);
+// const getSizeNameForScraper = (userAgent: string): CardType => {
+//   const scraper = getScraperName(userAgent);
 
-  switch (scraper) {
-    case "twitter":
-      return "social-twitter";
-    default:
-      return "social";
-  }
-};
+//   switch (scraper) {
+//     case "twitter":
+//       return "social-twitter";
+//     default:
+//       return "social";
+//   }
+// };
 
-const getScraperName = (userAgent: string): Scraper => {
-  if (userAgent.indexOf("Twitterbot/") !== -1) {
-    return "twitter";
-  }
+// const getScraperName = (userAgent: string): Scraper => {
+//   if (userAgent.indexOf("Twitterbot/") !== -1) {
+//     return "twitter";
+//   }
 
-  return "generic";
-};
+//   return "generic";
+// };
