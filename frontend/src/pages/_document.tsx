b5f6491a689e6fbe1513451b3a9f4eb4b59550e0
Marco Acierno <marcoaciernoemail@gmail.com>
2021-07-01 01:59:10 +0200

    Data fetching setup

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -13,27 +13,12 @@ export default class MyDocument extends Document<{
   css: string;
   url: string;
 }> {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    const host = ctx.req
-      ? ((ctx.req as any).protocol || "https") + "://" + ctx.req.headers.host
-      : null;
-
-    const url = host + ctx.req.url;
-
-    return { ...initialProps, url };
-  }
-
   render() {
     return (
       <Html>
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/mbr7dqb.css" />
           <link rel="shortcut icon" href="/favicon.png" />
-
-          <meta property="og:url" content={this.props.url} />
-          <meta property="twitter:url" content={this.props.url} />
 
           <script
             async={true}
