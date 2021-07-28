84813b7c3c8ae4ffaf947cfc52804f904f3054da
Marco Acierno <marcoaciernoemail@gmail.com>
2021-07-28 22:44:29 +0200

    Move pretix role in pretix module, remove unused code

---
+++ infrastructure/applications/pretix/role.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_role" "pretix" {
+  name = "pretix-${terraform.workspace}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "pretix" {
+  name = "pretix-${terraform.workspace}"
+  role = aws_iam_role.pretix.name
+}
+
+resource "aws_iam_role_policy" "pretix" {
+  name = "pretix-policy-${terraform.worksapce}"
+  role = aws_iam_role.pretix.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "cloudwatch:PutMetricData",
+        "ds:CreateComputer",
+        "ds:DescribeDirectories",
+        "ec2:DescribeInstanceStatus",
+        "ec2:CreateNetworkInterface",
+        "logs:*",
+        "ssm:*",
+        "ec2messages:*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage",
+        "s3:*",
+        "ses:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
