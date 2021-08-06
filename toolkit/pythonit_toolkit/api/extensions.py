484d9046c84db8e23638e0e0e4f115fa5338437c
Etty <beltrami.ester@gmail.com>
2021-08-06 09:10:08 +0100

    Revert "Fix SentryExtension"
    This reverts commit cb2acaf16391a42da2feb3d5a698823b2cf6bde2.

--- toolkit/pythonit_toolkit/api/extensions.py
+++ toolkit/pythonit_toolkit/api/extensions.py
@@ -1,14 +1,15 @@
 from sentry_sdk import add_breadcrumb
 from strawberry.extensions import Extension
+from strawberry.types.execution import ExecutionContext
 
 
 class SentryExtension(Extension):
-    def on_request_start(self):
+    def on_request_start(self, execution_context: ExecutionContext):
         add_breadcrumb(
             category="graphql",
             message=f"""
-{self.execution_context.operation_name or 'No operation name'}
-{self.execution_context.query}
+{execution_context.operation_name or 'No operation name'}
+{execution_context.query}
             """.strip(),
             level="info",
         )
