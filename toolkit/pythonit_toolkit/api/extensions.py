39344403099cc56e8a5b26322c93bc4f6f7459f9
Etty <beltrami.ester@gmail.com>
2021-08-06 14:05:58 +0100

    Fix SentryExtension

--- toolkit/pythonit_toolkit/api/extensions.py
+++ toolkit/pythonit_toolkit/api/extensions.py
@@ -1,15 +1,14 @@
 from sentry_sdk import add_breadcrumb
 from strawberry.extensions import Extension
-from strawberry.types.execution import ExecutionContext
 
 
 class SentryExtension(Extension):
-    def on_request_start(self, execution_context: ExecutionContext):
+    def on_request_start(self):
         add_breadcrumb(
             category="graphql",
             message=f"""
-{execution_context.operation_name or 'No operation name'}
-{execution_context.query}
+{self.execution_context.operation_name or 'No operation name'}
+{self.execution_context.query}
             """.strip(),
             level="info",
         )
