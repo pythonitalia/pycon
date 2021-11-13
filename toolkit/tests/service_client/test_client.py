192efc32d15d54735b9f9f445c5f766a86ec2653
Etty <beltrami.ester@gmail.com>
2021-11-13 18:31:06 +0000

    Split tests

--- toolkit/tests/service_client/test_client.py
+++ toolkit/tests/service_client/test_client.py
@@ -1,7 +1,7 @@
 from unittest.mock import AsyncMock, patch
 
 from pythonit_toolkit.service_client import ServiceClient
-from ward import each, raises, test
+from ward import raises, test
 
 
 @test("execute a query")
@@ -50,46 +50,53 @@ async def _():
         assert response.errors == ["Something went wrong"]
 
 
-@test("raise ValueError if arguments are empty")
-async def _(
-    url=each(
-        "",
-        "http://localhost:8050",
-        "http://localhost:8050",
-        "http://localhost:8050",
-    ),
-    issuer=each(
-        "pycon",
-        "",
-        "pycon",
-        "pycon",
-    ),
-    audience=each(
-        "users-service",
-        "users-service",
-        "",
-        "users-service",
-    ),
-    jwt_secret=each(
-        "mysecret",
-        "mysecret",
-        "mysecret",
-        "",
-    ),
-    missing_argument=each(
-        "url",
-        "issuer",
-        "audience",
-        "jwt_secret",
-    ),
-):
-
+@test("raise ValueError if url is empty")
+def _():
     with raises(ValueError) as exc:
         ServiceClient(
-            url=url,
-            issuer=issuer,
-            audience=audience,
-            jwt_secret=jwt_secret,
+            url="",
+            issuer="pycon",
+            audience="users-service",
+            jwt_secret="mysecret",
         )
 
-    assert str(exc.raised) == f"Argument '{missing_argument}' can't be empty"
+    assert str(exc.raised) == "Argument 'url' can't be empty"
+
+
+@test("raise ValueError if issuer is empty")
+def _():
+    with raises(ValueError) as exc:
+        ServiceClient(
+            url="http://localhost:8050",
+            issuer="",
+            audience="users-service",
+            jwt_secret="mysecret",
+        )
+
+    assert str(exc.raised) == "Argument 'issuer' can't be empty"
+
+
+@test("raise ValueError if audience is empty")
+def _():
+    with raises(ValueError) as exc:
+        ServiceClient(
+            url="http://localhost:8050",
+            issuer="pycon",
+            audience="",
+            jwt_secret="mysecret",
+        )
+
+    assert str(exc.raised) == "Argument 'audience' can't be empty"
+
+
+@test("raise ValueError if jwt_secret is empty")
+def _():
+    with raises(ValueError) as exc:
+        ServiceClient(
+            url="http://localhost:8050",
+            issuer="pycon",
+            audience="users-service",
+            jwt_secret="",
+        )
+
+    assert str(exc.raised) == "Argument 'jwt_secret' can't be empty"
