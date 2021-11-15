cfe8dd940353862f3fc3edfc0f67ddd2bb7ab84d
Etty <beltrami.ester@gmail.com>
2021-11-15 21:17:07 +0000

    Error handling

--- backend/integrations/mailchimp.py
+++ backend/integrations/mailchimp.py
@@ -5,12 +5,19 @@ from django.conf import settings
 
 
 @dataclass
-class MailchimpSubscription:
+class MailchimpResponse:
     id: str
     email: str
 
 
-def subscribe(email: str):
+class MailchimpError(Exception):
+    def __init__(self, title, detail="", status=""):
+        message = f"{status} {title}: {detail}"
+
+        return super().__init__(message)
+
+
+def subscribe(email: str) -> MailchimpResponse:
     if not settings.MAILCHIMP_SECRET_KEY:
         raise ValueError("Mailchimp integration is not configured")
 
@@ -19,10 +26,20 @@ def subscribe(email: str):
         "Accept": "application/json",
         "Authorization": f"Bearer {settings.MAILCHIMP_SECRET_KEY}",
     }
+
     response = requests.post(
         url, json={"email_address": email, "status": "subscribed"}, headers=headers
     )
 
     data = response.json()
 
-    return MailchimpSubscription(id=data["id"], email=data["email_address"])
+    if data["status"] == "subscribed":
+        return MailchimpResponse(id=data["id"], email=data["email_address"])
+    elif data["title"] == "Member Exists":
+        return MailchimpResponse(id=data["instance"], email=email)
+    else:
+        raise MailchimpError(
+            status=data.get("status"),
+            title=data.get("title"),
+            detail=data.get("detail"),
+        )
--- backend/integrations/tests/test_mailchimp.py
+++ backend/integrations/tests/test_mailchimp.py
@@ -1,7 +1,7 @@
 import pytest
 from django.test.utils import override_settings
 
-from integrations.mailchimp import subscribe
+from integrations.mailchimp import MailchimpError, subscribe
 
 
 @override_settings(MAILCHIMP_SECRET_KEY="")
@@ -10,6 +10,56 @@ def test_mailchimp_not_configure():
         subscribe("me@pycon.it")
 
 
-@pytest.mark.xfail(reason="TODO")
-def test_call_mailchimp():
-    assert False
+@override_settings(MAILCHIMP_LIST_ID="1234")
+def test_subscribe_mailchimp_success_response(requests_mock):
+    requests_mock.post(
+        "https://us5.api.mailchimp.com/3.0/lists/1234/members",
+        json={
+            "email_address": "sushi@pycon.it",
+            "status": "subscribed",
+            "id": "63b71566-4658-11ec-81d3-0242ac130003",
+        },
+    )
+
+    resp = subscribe("sushi@pycon.it")
+
+    assert resp.id == "63b71566-4658-11ec-81d3-0242ac130003"
+    assert resp.email == "sushi@pycon.it"
+
+
+@override_settings(MAILCHIMP_LIST_ID="1234")
+def test_subscribe_mailchimp_already_exists(requests_mock):
+    requests_mock.post(
+        "https://us5.api.mailchimp.com/3.0/lists/1234/members",
+        json={
+            "title": "Member Exists",
+            "status": 400,
+            "detail": "pizza@pycon.it is already a list member. Use PUT to insert or update list members.",
+            "instance": "59588eec-4658-11ec-81d3-0242ac130003",
+        },
+    )
+
+    resp = subscribe("pizza@pycon.it")
+
+    assert resp.id == "59588eec-4658-11ec-81d3-0242ac130003"
+    assert resp.email == "pizza@pycon.it"
+
+
+@override_settings(MAILCHIMP_LIST_ID="1234")
+def test_subscribe_mailchimp_error_response(requests_mock):
+    requests_mock.post(
+        "https://us5.api.mailchimp.com/3.0/lists/1234/members",
+        json={
+            "type": "https://mailchimp.com/developer/marketing/docs/errors/",
+            "title": "Resource Not Found",
+            "status": 404,
+            "detail": "The requested resource could not be found.",
+            "instance": "b49b2842-f1fc-25c6-135b-d8204fe18b10",
+        },
+    )
+
+    with pytest.raises(
+        MailchimpError,
+        match="404 Resource Not Found: The requested resource could not be found.",
+    ):
+        subscribe("pizza@pycon.it")
