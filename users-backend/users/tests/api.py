3d726b6fea546ccf4a5efe8822f21b452ece7dc6
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-16 00:44:42 +0100

    Update aud references, fixes for latest httpx

--- users-backend/users/tests/api.py
+++ users-backend/users/tests/api.py
@@ -1,38 +1,41 @@
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
-from main import app
 from pythonit_toolkit.api.graphql_test_client import GraphQLClient
+from ward import Scope, fixture
+
+from main import app
 from users.settings import PASTAPORTO_SECRET, SERVICE_TO_SERVICE_SECRET
-from ward import fixture
+
+
+@fixture(scope=Scope.Global)
+async def client():
+    async with AsyncClient(app=app, base_url="http://testserver") as async_client:
+        yield async_client
 
 
 @fixture
-async def testclient():
+async def testclient(client=client):
     async with LifespanManager(app):
-        async with AsyncClient(app=app, base_url="http://testserver") as client:
-            yield client
+        yield client
 
 
 @fixture()
 async def graphql_client(testclient=testclient):
-    async with testclient:
-        yield GraphQLClient(testclient, pastaporto_secret=PASTAPORTO_SECRET)
+    yield GraphQLClient(testclient, pastaporto_secret=PASTAPORTO_SECRET)
 
 
 @fixture()
 async def admin_graphql_client(testclient=testclient):
-    async with testclient:
-        yield GraphQLClient(
-            testclient, admin_endpoint=True, pastaporto_secret=PASTAPORTO_SECRET
-        )
+    yield GraphQLClient(
+        testclient, admin_endpoint=True, pastaporto_secret=PASTAPORTO_SECRET
+    )
 
 
 @fixture()
 async def internalapi_graphql_client(testclient=testclient):
-    async with testclient:
-        yield GraphQLClient(
-            testclient,
-            internal_api_endpoint=True,
-            pastaporto_secret=PASTAPORTO_SECRET,
-            service_to_service_secret=SERVICE_TO_SERVICE_SECRET,
-        )
+    yield GraphQLClient(
+        testclient,
+        internal_api_endpoint=True,
+        pastaporto_secret=PASTAPORTO_SECRET,
+        service_to_service_secret=SERVICE_TO_SERVICE_SECRET,
+    )
