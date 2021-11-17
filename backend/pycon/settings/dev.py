f2b220055ae4558ddf2870203d67f0eaa36491f2
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-17 04:51:13 +0100

    Change env settings

--- backend/pycon/settings/dev.py
+++ backend/pycon/settings/dev.py
@@ -7,3 +7,6 @@ USE_SCHEDULER = False
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 PASTAPORTO_SECRET = env("PASTAPORTO_SECRET", default="pastaporto_xxxxxxxx")
+
+USERS_SERVICE = env("USERS_SERVICE")
+SERVICE_TO_SERVICE_SECRET = env("SERVICE_TO_SERVICE_SECRET")
