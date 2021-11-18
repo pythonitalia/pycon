573d30721f4ee3e32ae10cbd26430177a99c7722
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-19 00:49:50 +0100

    Show again buy tickets CTA

--- frontend/src/pages/[lang]/index.tsx
+++ frontend/src/pages/[lang]/index.tsx
@@ -43,7 +43,7 @@ export const HomePage = () => {
       <FormattedMessage id="home.title">
         {(text) => <MetaTags title={text} />}
       </FormattedMessage>
-      <HomepageHero hideBuyTickets={true} />
+      <HomepageHero hideBuyTickets={false} />
 
       <Marquee message={conference.marquee!} />
 
