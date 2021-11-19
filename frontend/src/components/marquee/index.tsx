2dab6fb1568fa1d8d5510882ef02b23851e7a573
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-19 03:12:38 +0100

    Fix marquee on the homepage not looping correctly

--- frontend/src/components/marquee/index.tsx
+++ frontend/src/components/marquee/index.tsx
@@ -2,6 +2,7 @@
 
 /** @jsx jsx */
 import { keyframes } from "@emotion/core";
+import { useEffect, useState } from "react";
 import { Flex, jsx, Text } from "theme-ui";
 
 type MarqueeProps = {
@@ -9,33 +10,61 @@ type MarqueeProps = {
 };
 
 const animation = keyframes`
-  0% { transform: translate(0, 0); }
-  100% { transform: translate(-100%, 0); }
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
 `;
 
-export const Marquee: React.SFC<MarqueeProps> = ({ message }) => (
-  <Flex
-    sx={{
-      overflow: "hidden",
-      alignItems: "center",
-      height: 125,
-      borderTop: "primary",
-      borderBottom: "primary",
-    }}
-  >
-    <Text
-      variant="marquee"
+export const Marquee = ({ message }: MarqueeProps) => {
+  const messageWithSeparator = `${message} /`;
+  const ch = `${messageWithSeparator.length}ch`;
+  // Huge number so that the SSR version doesn't show blank space at first render
+  const [numOfShadows, setNumOfShadows] = useState(50);
+  useEffect(() => {
+    const listener = () => {
+      setNumOfShadows(
+        Math.ceil(
+          window.innerWidth /
+            (22.399993896484375 * messageWithSeparator.length),
+        ),
+      );
+    };
+    window.addEventListener("resize", listener);
+    listener();
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
+
+  return (
+    <Flex
       sx={{
-        willChange: "transform",
-        animation: `${animation} 20s linear infinite`,
+        height: 125,
+        alignItems: "center",
+        borderTop: "primary",
+        borderBottom: "primary",
       }}
-      css={`
-        @media (prefers-reduced-motion: reduce) {
-          animation: none !important;
-        }
-      `}
     >
-      {new Array(10).fill(message).join(" / ")}
-    </Text>
-  </Flex>
-);
+      <Text
+        as="span"
+        variant="marquee"
+        sx={{
+          display: "inline-block",
+          whiteSpace: "nowrap",
+          willChange: "transform",
+          animation: `${animation} 2.5s linear infinite`,
+          width: `${ch}`,
+          textShadow: new Array(numOfShadows)
+            .fill(0)
+            .map((_, i) => `calc(${ch} * ${i + 1}) 0 currentColor`)
+            .join(","),
+        }}
+      >
+        {messageWithSeparator}
+      </Text>
+    </Flex>
+  );
+};
