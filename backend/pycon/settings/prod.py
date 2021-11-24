ace163816a25d90de9ff9c8dd73c33a586c7ad5b
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-24 00:17:37 +0000

    WEB-157: Remove celery from our codebase (#2486)

--- backend/pycon/settings/prod.py
+++ backend/pycon/settings/prod.py
@@ -5,8 +5,6 @@ from .base import *  # noqa
 from .base import DATABASES, MIDDLEWARE, env
 
 SECRET_KEY = env("SECRET_KEY")
-# CELERY_BROKER_URL = env("CELERY_BROKER_URL")
-USE_SCHEDULER = False
 
 # if FRONTEND_URL == "http://testfrontend.it/":
 #     raise ImproperlyConfigured("Please configure FRONTEND_URL for production")
--- backend/pycon/settings/test.py
+++ backend/pycon/settings/test.py
@@ -2,7 +2,6 @@ from .base import *  # noqa
 
 SECRET_KEY = "this-key-should-only-be-used-for-tests"
 SLACK_INCOMING_WEBHOOK_URL = ""
-USE_SCHEDULER = False
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
