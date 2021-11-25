73bff5e845b1b0c711e08b04272edf41ca9a75c5
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-25 02:42:10 +0100

    fixes

--- .github/workflows/scripts/apply-terraform.py
+++ .github/workflows/scripts/apply-terraform.py
@@ -1,19 +1,27 @@
+import json
 import os
 import subprocess
-import sys
 import time
 
 import boto3
 
+services_to_deploy: list[str] = json.loads(os.environ["SERVICES"])
+targets = []
+
+for service in services_to_deploy:
+    targets.append(f'-target module.{service.replace("-", "_")}')
+
 result = subprocess.run(
-    ["terraform", "apply", "-no-color", "-auto-approve"],
+    ["terraform", "apply", "-no-color", "-auto-approve", *targets],
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
 )
 
-
 logs = []
 timestamp = int(time.time() * 1000)
+
+logs.append({"timestamp": timestamp, "message": f"Terraform Apply logs"})
+
 for line in result.stdout.splitlines():
     message = line.decode("utf-8")
     if not message:
@@ -29,12 +37,10 @@ logs.append(
 client = boto3.client("logs")
 client.create_log_stream(
     logGroupName=os.environ["CLOUDWATCH_LOG_GROUP"],
-    logStreamName=sys.argv[1],
+    logStreamName=os.environ["CLOUDWATCH_LOG_STREAM"],
 )
 response = client.put_log_events(
     logGroupName=os.environ["CLOUDWATCH_LOG_GROUP"],
-    logStreamName=sys.argv[1],
+    logStreamName=os.environ["CLOUDWATCH_LOG_STREAM"],
     logEvents=logs,
 )
-
-print("response", response)
