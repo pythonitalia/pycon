da0d3b9acabd5befa216e53d3f9194bde20c9403
Marco Acierno <marcoaciernoemail@gmail.com>
2021-11-28 05:01:05 +0000

    Move Pretix to ECS (#2510)

---
+++ infrastructure/applications/pretix/main.tf
@@ -0,0 +1,152 @@
+data "aws_db_instance" "database" {
+  db_instance_identifier = "pythonit-${terraform.workspace}"
+}
+
+resource "aws_ecs_cluster" "pretix" {
+  name = "${terraform.workspace}-pretix"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.sh")
+  vars = {
+    ecs_cluster = aws_ecs_cluster.pretix.name
+  }
+}
+
+data "aws_ami" "ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "pretix" {
+  ami               = data.aws_ami.ecs.id
+  instance_type     = "t3.small"
+  subnet_id         = data.aws_subnet.public.id
+  availability_zone = "eu-central-1a"
+  vpc_security_group_ids = [
+    aws_security_group.instance.id,
+    data.aws_security_group.rds.id
+  ]
+  source_dest_check    = false
+  user_data            = data.template_file.user_data.rendered
+  iam_instance_profile = aws_iam_instance_profile.instance.name
+  key_name             = "pretix"
+
+  tags = {
+    Name = "${terraform.workspace}-pretix-instance"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = aws_instance.pretix.id
+  vpc      = true
+  tags = {
+    Name = "${terraform.workspace}-pretix"
+  }
+}
+
+data "aws_ecr_repository" "repo" {
+  name = "pythonit/pretix"
+}
+
+resource "aws_ecs_task_definition" "pretix_service" {
+  family = "${terraform.workspace}-pretix"
+  container_definitions = jsonencode([
+    {
+      name      = "pretix"
+      image     = "${data.aws_ecr_repository.repo.repository_url}:latest"
+      cpu       = 2048
+      memory    = 1024
+      essential = true
+      environment = [
+        {
+          name  = "DATABASE_NAME"
+          value = "pretix"
+        },
+        {
+          name  = "DATABASE_USERNAME"
+          value = data.aws_db_instance.database.master_username
+        },
+        {
+          name  = "DATABASE_PASSWORD"
+          value = module.common_secrets.value.database_password
+        },
+        {
+          name  = "DATABASE_HOST"
+          value = data.aws_db_instance.database.address
+        },
+        {
+          name  = "MAIL_USER"
+          value = module.secrets.value.mail_user
+        },
+        {
+          name  = "MAIL_PASSWORD"
+          value = module.secrets.value.mail_password
+        },
+        {
+          name  = "SENTRY_DSN"
+          value = module.secrets.value.sentry_dsn
+        },
+        {
+          name  = "SECRET_KEY"
+          value = module.secrets.value.secret_key
+        },
+      ]
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "media"
+          containerPath = "/data/media"
+        },
+        {
+          sourceVolume  = "data"
+          containerPath = "/var/pretix-data"
+        }
+      ]
+      systemControls = [
+        {
+          "namespace" : "net.core.somaxconn",
+          "value" : "4096"
+        }
+      ]
+    },
+  ])
+
+  volume {
+    name      = "media"
+    host_path = "/var/pretix/data/media"
+  }
+
+  volume {
+    name      = "data"
+    host_path = "/var/pretix-data"
+  }
+
+  requires_compatibilities = []
+  tags                     = {}
+}
+
+resource "aws_ecs_service" "pretix" {
+  name                               = "${terraform.workspace}-pretix-service"
+  cluster                            = aws_ecs_cluster.pretix.id
+  task_definition                    = aws_ecs_task_definition.pretix_service.arn
+  desired_count                      = 1
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 100
+}
---
+++ infrastructure/applications/pretix/security.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "instance" {
+  name        = "${terraform.workspace}-pretix-instance"
+  description = "${terraform.workspace} pretix instance"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "out_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance.id
+}
+
+resource "aws_security_group_rule" "web_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance.id
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance.id
+}
