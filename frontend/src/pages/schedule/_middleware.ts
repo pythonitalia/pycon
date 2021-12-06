e369c5046dad06f72a2dced9a2888a539ea16e84
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-06 20:05:52 +0100

    fix

--- frontend/src/pages/schedule/_middleware.ts
+++ frontend/src/pages/schedule/_middleware.ts
@@ -18,8 +18,12 @@ export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
   // @ts-ignore
   process.__DEV__ = false;
 
+  console.log("a")
   const { pathname, locale } = req.nextUrl;
+  console.log("b")
+
   const client = getApolloClient();
+  console.log("c")
   const {
     data: {
       conference: { days },
@@ -27,15 +31,16 @@ export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
   } = await queryScheduleDays(client, {
     code: process.env.conferenceCode,
   });
+  console.log("d")
 
-  const firstDay = days[0].day;
-  const language = locale === "default" ? DEFAULT_LOCALE : locale;
+  // const firstDay = days[0].day;
+  // const language = locale === "default" ? DEFAULT_LOCALE : locale;
 
   // If we are landing on the /schedule page we want
   // to dynamically redirect the user to the first day of the schedule
-  if (pathname === "/schedule") {
-    return NextResponse.redirect(`/${language}/schedule/${firstDay}`);
-  }
+  // if (pathname === "/schedule") {
+  //   return NextResponse.redirect(`/${language}/schedule/${firstDay}`);
+  // }
 
   return undefined;
 }
