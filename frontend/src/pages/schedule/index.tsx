c58a895e9611d0c9f64f8b7b7a287f7bf922d725
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-07 00:38:51 +0100

    revert all schedule changes

---
+++ frontend/src/pages/schedule/index.tsx
@@ -0,0 +1,33 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+
+import { getApolloClient } from "~/apollo/client";
+import { queryScheduleDays } from "~/types";
+
+export const SchedulePage = () => (
+  <Head>
+    <meta name="robots" content="noindex, nofollow" />
+  </Head>
+);
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const lang = params.lang as string;
+  const client = getApolloClient();
+  const {
+    data: {
+      conference: { days },
+    },
+  } = await queryScheduleDays(client, {
+    code: process.env.conferenceCode,
+  });
+
+  const firstDay = days[0].day;
+  return {
+    redirect: {
+      destination: `/${lang}/schedule/${firstDay}/`,
+      permanent: false,
+    },
+  };
+};
+
+export default SchedulePage;
