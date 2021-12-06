7c554ca99b8b0de70bfdd1293ece74e8d9c9e7f0
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-06 19:27:04 +0100

    fix __dev__ maybe

--- frontend/src/pages/schedule/_middleware.ts
+++ frontend/src/pages/schedule/_middleware.ts
@@ -1,7 +1,4 @@
 /* eslint import/first: "off", @typescript-eslint/ban-ts-comment: "off" */
-// @ts-ignore
-this.__DEV__ = false;
-
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
@@ -10,6 +7,12 @@ import { DEFAULT_LOCALE } from "~/locale/languages";
 import { queryScheduleDays } from "~/types";
 
 export async function middleware(req: NextRequest, _ev: NextFetchEvent) {
+  this.__DEV__ = false;
+  // @ts-ignore
+  global.__DEV__ = false;
+  // @ts-ignore
+  process.__DEV__ = false;
+
   const { pathname, locale } = req.nextUrl;
   const client = getApolloClient();
   const {
