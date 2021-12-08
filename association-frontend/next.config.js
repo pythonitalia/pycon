6184d4cbe5c22ab0501de366bba20223e6cda0c7
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-09 00:22:13 +0100

    Fix safari login using first-party domain

--- association-frontend/next.config.js
+++ association-frontend/next.config.js
@@ -1,9 +1,18 @@
-const { API_URL, API_URL_SERVER, STRIPE_KEY } = process.env;
+const { API_URL, API_URL_SERVER, STRIPE_KEY, GRAPHQL_GATEWAY_URL } =
+  process.env;
 
 module.exports = {
   env: {
     API_URL: API_URL,
     API_URL_SERVER: API_URL_SERVER,
     STRIPE_KEY: STRIPE_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination: GRAPHQL_GATEWAY_URL,
+      },
+    ];
   },
 };
