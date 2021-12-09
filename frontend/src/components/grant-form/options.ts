08fde2a34d98e11515af7ec1640058a0ab66e1a6
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-09 02:45:20 +0100

    Disable placeholders in the grant form

--- frontend/src/components/grant-form/options.ts
+++ frontend/src/components/grant-form/options.ts
@@ -1,21 +1,26 @@
 export const GENDER_OPTIONS = [
   {
     value: "",
+    disabled: true,
     messageId: "profile.gender.selectGender",
   },
   {
+    disabled: false,
     value: "male",
     messageId: "profile.gender.male",
   },
   {
+    disabled: false,
     value: "female",
     messageId: "profile.gender.female",
   },
   {
+    disabled: false,
     value: "other",
     messageId: "profile.gender.other",
   },
   {
+    disabled: false,
     value: "not_say",
     messageId: "profile.gender.not_say",
   },
@@ -24,25 +29,31 @@ export const GENDER_OPTIONS = [
 export const OCCUPATION_OPTIONS = [
   {
     value: "",
+    disabled: true,
     messageId: "grants.form.fields.occupation.values.selectOption",
   },
   {
+    disabled: false,
     value: "developer",
     messageId: "grants.form.fields.occupation.values.developer",
   },
   {
+    disabled: false,
     value: "student",
     messageId: "grants.form.fields.occupation.values.student",
   },
   {
+    disabled: false,
     value: "researcher",
     messageId: "grants.form.fields.occupation.values.researcher",
   },
   {
+    disabled: false,
     value: "unemployed",
     messageId: "grants.form.fields.occupation.values.unemployed",
   },
   {
+    disabled: false,
     value: "other",
     messageId: "grants.form.fields.occupation.values.other",
   },
@@ -51,17 +62,21 @@ export const OCCUPATION_OPTIONS = [
 export const GRANT_TYPE_OPTIONS = [
   {
     value: "",
+    disabled: true,
     messageId: "grants.form.fields.grantType.values.selectOption",
   },
   {
+    disabled: false,
     value: "diversity",
     messageId: "grants.form.fields.grantType.values.diversity",
   },
   {
+    disabled: false,
     value: "unemployed",
     messageId: "grants.form.fields.grantType.values.unemployed",
   },
   {
+    disabled: false,
     value: "speaker",
     messageId: "grants.form.fields.grantType.values.speaker",
   },
@@ -70,19 +85,23 @@ export const GRANT_TYPE_OPTIONS = [
 export const INTERESTED_IN_VOLUNTEERING_OPTIONS = [
   {
     value: "",
+    disabled: true,
     messageId:
       "grants.form.fields.interestedInVolunteering.values.selectOption",
   },
   {
     value: "no",
+    disabled: false,
     messageId: "grants.form.fields.interestedInVolunteering.values.no",
   },
   {
     value: "yes",
+    disabled: false,
     messageId: "grants.form.fields.interestedInVolunteering.values.yes",
   },
   {
     value: "absolutely",
+    disabled: false,
     messageId: "grants.form.fields.interestedInVolunteering.values.absolutely",
   },
 ];
