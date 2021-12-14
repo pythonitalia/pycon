5a1ef48350ebea1f98a88aaba3092bcc6d8b07cf
Etty <beltrami.ester@gmail.com>
2021-12-14 10:57:51 +0000

    Add filters to Sponsor admin

--- backend/sponsors/admin.py
+++ backend/sponsors/admin.py
@@ -4,13 +4,17 @@ from ordered_model.admin import OrderedModelAdmin
 from .models import Sponsor, SponsorLevel
 
 
-@admin.register(SponsorLevel)
-class SponsorLevelAdmin(OrderedModelAdmin):
-    list_display = ("name", "conference", "move_up_down_links")
-    readonly_fields = ("order",)
-
-
 @admin.register(Sponsor)
 class SponsorAdmin(OrderedModelAdmin):
     list_display = ("name", "move_up_down_links")
+    list_filter = (
+        "levels__conference",
+        "levels__name",
+    )
+    readonly_fields = ("order",)
+
+
+@admin.register(SponsorLevel)
+class SponsorLevelAdmin(OrderedModelAdmin):
+    list_display = ("name", "conference", "move_up_down_links")
     readonly_fields = ("order",)
