8f645dab5f8dcd079fe80f7b630c4858b5a62b74
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-17 15:46:58 +0100

    wip settings refactor

--- association-backend/src/association/settings.py
+++ association-backend/src/association/settings.py
@@ -5,6 +5,9 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 
+# Unit-tests
+RUNNING_TESTS = config("RUNNING_TESTS", cast=bool, default=False)
+
 ENV = config("ENV", cast=str, default="local")
 DEBUG = config("DEBUG", cast=bool, default=False)
 DATABASE_URL = config("DATABASE_URL")
@@ -13,10 +16,11 @@ PRETIX_API_URL = config("PRETIX_API", default=None)
 PRETIX_API_TOKEN = config("PRETIX_API_TOKEN", cast=Secret, default=None)
 
 # Services URLs
-ASSOCIATION_FRONTEND_URL = config(
-    "ASSOCIATION_FRONTEND_URL",
-)
-USERS_SERVICE_URL = config("USERS_SERVICE")
+if not RUNNING_TESTS:
+    ASSOCIATION_FRONTEND_URL = config(
+        "ASSOCIATION_FRONTEND_URL",
+    )
+    USERS_SERVICE_URL = config("USERS_SERVICE")
 
 # Sentry
 SENTRY_DSN = config("SENTRY_DSN", cast=Secret, default="")
@@ -32,9 +36,6 @@ stripe.api_key = str(STRIPE_SECRET_API_KEY)
 # Secrets
 PASTAPORTO_SECRET = config("PASTAPORTO_SECRET", cast=str)
 SERVICE_TO_SERVICE_SECRET = config("SERVICE_TO_SERVICE_SECRET", cast=Secret)
-
-# Unit-tests
-RUNNING_TESTS = config("RUNNING_TESTS", cast=bool, default=False)
 
 if RUNNING_TESTS:
     original_url = make_url(DATABASE_URL)
