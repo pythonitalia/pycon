a66ec3720123dbad35ef0695a4b9e7c04698a37a
Marco Acierno <marcoaciernoemail@gmail.com>
2021-12-28 00:26:34 +0100

    tests for edge cases

--- association-backend/src/internal_api/tests/queries/test_user_id_is_member.py
+++ association-backend/src/internal_api/tests/queries/test_user_id_is_member.py
@@ -97,3 +97,42 @@ async def _(
     response = await internalapi_graphql_client.query(query, variables={"id": "1"})
     assert response.errors[0]["message"] == "Forbidden"
     assert not response.data
+
+
+@test("invalid id raises an error")
+async def _(
+    internalapi_graphql_client=internalapi_graphql_client,
+    db=db,
+):
+    internalapi_graphql_client.force_service_login(
+        issuer="pycon-backend", audience="association-backend"
+    )
+
+    query = """query($id: ID!) {
+        userIdIsMember(id: $id)
+    }"""
+
+    response = await internalapi_graphql_client.query(query, variables={"id": "abc-1"})
+    assert (
+        response.errors[0]["message"]
+        == "invalid literal for int() with base 10: 'abc-1'"
+    )
+    assert not response.data
+
+
+@test("empty id raises an error")
+async def _(
+    internalapi_graphql_client=internalapi_graphql_client,
+    db=db,
+):
+    internalapi_graphql_client.force_service_login(
+        issuer="pycon-backend", audience="association-backend"
+    )
+
+    query = """query($id: ID!) {
+        userIdIsMember(id: $id)
+    }"""
+
+    response = await internalapi_graphql_client.query(query, variables={"id": ""})
+    assert response.errors[0]["message"] == "Invalid ID"
+    assert not response.data
