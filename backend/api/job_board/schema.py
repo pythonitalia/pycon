eed1dfa3f55a56238f32186e208fd184f0823b40
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-12 01:07:03 +0100

    Avoid duplicating code on the strawberry layer

--- backend/api/job_board/schema.py
+++ backend/api/job_board/schema.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 import strawberry
+
 from job_board.models import JobListing
 
 from .types import JobListing as JobListingType
@@ -9,41 +10,17 @@ from .types import JobListing as JobListingType
 @strawberry.type
 class JobBoardQuery:
     @strawberry.field
-    def job_listings(self, info) -> List[JobListingType]:
+    def job_listings(self) -> List[JobListingType]:
         return [
-            JobListingType(
-                id=listing.id,
-                title=listing.title,
-                slug=listing.slug,
-                description=listing.description,
-                company=listing.company,
-                company_logo=(
-                    info.context.request.build_absolute_uri(listing.company_logo.url)
-                    if listing.company_logo
-                    else None
-                ),
-                apply_url=listing.apply_url,
-            )
+            JobListingType.from_django_model(listing)
             for listing in JobListing.objects.all()
         ]
 
     @strawberry.field
-    def job_listing(self, info, slug: str) -> Optional[JobListingType]:
+    def job_listing(self, slug: str) -> Optional[JobListingType]:
         listing = JobListing.objects.by_slug(slug).first()
 
         if not listing:
             return None
 
-        return JobListingType(
-            id=listing.id,
-            title=listing.title,
-            slug=listing.slug,
-            description=listing.description,
-            company=listing.company,
-            company_logo=(
-                info.context.request.build_absolute_uri(listing.company_logo.url)
-                if listing.company_logo
-                else None
-            ),
-            apply_url=listing.apply_url,
-        )
+        return JobListingType.from_django_model(listing)
--- backend/api/job_board/types.py
+++ backend/api/job_board/types.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import Optional
 
 import strawberry
@@ -13,8 +12,15 @@ class JobListing:
     slug: str = strawberry.field(resolver=make_localized_resolver("slug"))
     description: str = strawberry.field(resolver=make_localized_resolver("description"))
     company: str
-    company_logo: Optional[str]
-    apply_url: Optional[str]
+    company_logo_url: strawberry.Private[Optional[str]]
+    apply_url: str
+
+    @strawberry.field
+    def company_logo(self, info) -> Optional[str]:
+        if not self.company_logo_url:
+            return None
+
+        return info.context.request.build_absolute_uri(self.company_logo_url)
 
     def __init__(
         self,
@@ -23,13 +29,27 @@ class JobListing:
         slug: str,
         description: str,
         company: str,
-        company_logo: Optional[str],
-        apply_url: Optional[str],
+        company_logo_url: Optional[str],
+        apply_url: str,
     ) -> None:
         self.id = id
         self.title = title
         self.slug = slug
         self.description = description
         self.company = company
-        self.company_logo = company_logo
+        self.company_logo_url = company_logo_url
         self.apply_url = apply_url
+
+    @classmethod
+    def from_django_model(cls, instance):
+        return cls(
+            id=instance.id,
+            title=instance.title,
+            slug=instance.slug,
+            description=instance.description,
+            company=instance.company,
+            company_logo_url=instance.company_logo.url
+            if instance.company_logo
+            else None,
+            apply_url=instance.apply_url,
+        )
