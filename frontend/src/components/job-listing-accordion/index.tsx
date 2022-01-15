aa5da9b5e9cb44118064c850842b49544e318bac
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-15 01:18:21 +0000

    Jobs board UI (#2590)

---
+++ frontend/src/components/job-listing-accordion/index.tsx
@@ -0,0 +1,171 @@
+/** @jsxRuntime classic */
+
+/** @jsx jsx */
+import React, { useState } from "react";
+import { FormattedMessage } from "react-intl";
+import { Text, Box, Grid, Flex, jsx } from "theme-ui";
+
+import Image from "next/image";
+
+import { compile } from "~/helpers/markdown";
+import { JobListing } from "~/types";
+
+import { Link } from "../link";
+
+export const JobListingAccordion = ({
+  job,
+  backgroundColor,
+}: {
+  job: JobListing;
+  backgroundColor: string;
+}) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Box
+      as="li"
+      sx={{
+        borderTop: "primary",
+        bg: backgroundColor,
+        "&:last-child": {
+          borderBottom: "primary",
+        },
+      }}
+    >
+      <Grid
+        gap={[3, 5]}
+        sx={{
+          mx: "auto",
+          px: 3,
+          maxWidth: "container",
+          cursor: "pointer",
+          gridTemplateColumns: ["1fr 100px", null, "100px 1fr 1fr 200px"],
+        }}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            display: ["none", null, "block"],
+            borderRight: job.companyLogo ? "primary" : null,
+            borderLeft: job.companyLogo ? "primary" : null,
+          }}
+        >
+          {job.companyLogo && <Image layout="fill" src={job.companyLogo} />}
+        </Box>
+        <AccordionColumn
+          sx={{
+            display: ["flex", null, "none"],
+          }}
+        >
+          {job.company} - {job.title}
+        </AccordionColumn>
+        <AccordionColumn>{job.company}</AccordionColumn>
+        <AccordionColumn>{job.title}</AccordionColumn>
+        <Flex
+          sx={{
+            py: [2, 4],
+            px: [2, 4],
+            alignItems: "center",
+            borderLeft: "primary",
+          }}
+        >
+          <FormattedMessage
+            id={
+              expanded ? "global.accordion.close" : "global.accordion.readMore"
+            }
+          />
+        </Flex>
+      </Grid>
+      {expanded && (
+        <Box
+          sx={{
+            borderTop: "primary",
+          }}
+        >
+          <Grid
+            gap={[4, 2]}
+            sx={{
+              mx: "auto",
+              px: 3,
+              py: [4, 5],
+              maxWidth: "container",
+              gridTemplateColumns: ["1fr", "2fr 1fr"],
+            }}
+          >
+            <Box
+              sx={{
+                "p + p": {
+                  pt: 2,
+                },
+              }}
+            >
+              {compile(job.description).tree}
+            </Box>
+            <Grid
+              gap={3}
+              sx={{
+                justifyItems: "flex-start",
+                alignContent: "flex-start",
+              }}
+            >
+              {job.applyUrl && (
+                <Link
+                  target="_blank"
+                  external
+                  path={job.applyUrl}
+                  variant="arrow-button"
+                >
+                  <FormattedMessage id="jobboard.applyNow" />
+                </Link>
+              )}
+              <JobExtraInfo labelId="jobboard.company" value={job.company} />
+              <JobExtraInfo labelId="jobboard.role" value={job.title} />
+            </Grid>
+          </Grid>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+const AccordionColumn = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  sx?: any;
+}) => (
+  <Flex
+    sx={{
+      py: 4,
+      alignItems: "center",
+      display: ["none", null, "flex"],
+    }}
+    className={className}
+  >
+    {children}
+  </Flex>
+);
+
+const JobExtraInfo = ({
+  labelId,
+  value,
+}: {
+  labelId: string;
+  value: string;
+}) => (
+  <Box>
+    <Text
+      sx={{
+        textTransform: "uppercase",
+        fontWeight: "bold",
+        color: "black",
+        userSelect: "none",
+      }}
+    >
+      <FormattedMessage id={labelId} />
+    </Text>
+    <Text>{value}</Text>
+  </Box>
+);
---
+++ frontend/src/pages/jobs/index.tsx
@@ -0,0 +1,80 @@
+/** @jsxRuntime classic */
+
+/** @jsx jsx */
+import { Fragment } from "react";
+import { FormattedMessage } from "react-intl";
+import { Box, Heading, jsx } from "theme-ui";
+
+import { GetStaticProps } from "next";
+
+import { getApolloClient, addApolloState } from "~/apollo/client";
+import { JobListingAccordion } from "~/components/job-listing-accordion";
+import { MetaTags } from "~/components/meta-tags";
+import { prefetchSharedQueries } from "~/helpers/prefetch";
+import { useCurrentLanguage } from "~/locale/context";
+import { queryAllJobListings, useAllJobListingsQuery } from "~/types";
+
+const COLORS = ["blue", "orange", "violet", "keppel"];
+
+const JobsBoardPage = () => {
+  const language = useCurrentLanguage();
+  const {
+    data: { jobListings },
+  } = useAllJobListingsQuery({
+    variables: {
+      language,
+    },
+  });
+
+  return (
+    <Fragment>
+      <FormattedMessage id="jobboard.title">
+        {(text) => <MetaTags title={text} />}
+      </FormattedMessage>
+      <Box
+        sx={{
+          borderTop: "primary",
+        }}
+      />
+
+      <Box
+        sx={{
+          mx: "auto",
+          px: 3,
+          py: 5,
+          maxWidth: "container",
+        }}
+      >
+        <Heading as="h1">
+          <FormattedMessage id="jobboard.title" />
+        </Heading>
+      </Box>
+
+      <Box as="ul">
+        {jobListings.map((job, index) => (
+          <JobListingAccordion
+            backgroundColor={COLORS[index % COLORS.length]}
+            job={job}
+          />
+        ))}
+      </Box>
+    </Fragment>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const client = getApolloClient();
+
+  await Promise.all([
+    prefetchSharedQueries(client, locale),
+    queryAllJobListings(client, {
+      language: locale,
+    }),
+  ]);
+
+  return addApolloState(client, {
+    props: {},
+  });
+};
+
+export default JobsBoardPage;
