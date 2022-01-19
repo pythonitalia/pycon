c3b73309c408c53d09402ced4e04f62795eb5fec
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-19 23:46:45 +0000

    Add Olark on pycon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -26,6 +26,20 @@ export default class MyDocument extends Document<{
           <InitializeColorMode />
           <Main />
           <NextScript />
+          <!-- begin olark code -->
+          <script type="text/javascript">
+          ;(function(o,l,a,r,k,y){if(o.olark)return;
+          r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+          y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+          y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+          y.extend=function(i,j){y("extend",i,j)};
+          y.identify=function(i){y("identify",k.i=i)};
+          y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+          k=y._={s:[],t:[+new Date],c:{},l:a};
+          })(window,document,"static.olark.com/jsclient/loader.js");
+          /* Add configuration calls below this comment */
+          olark.identify('1751-12112149-10-1389');</script>
+          <!-- end olark code -->
         </body>
       </Html>
     );
