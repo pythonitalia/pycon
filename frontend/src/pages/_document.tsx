d66da24d14b9822ea8076092e8b12783169b908d
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-22 02:41:14 +0100

    Correctly add olark script

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { InitializeColorMode } from "theme-ui";
 
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class MyDocument extends Document<{
   ids: string[];
@@ -26,18 +27,23 @@ export default class MyDocument extends Document<{
           <InitializeColorMode />
           <Main />
           <NextScript />
-          <script type="text/javascript">{`
-          ;(function(o,l,a,r,k,y){if(o.olark)return;
-          r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
-          y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
-          y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
-          y.extend=function(i,j){y("extend",i,j)};
-          y.identify=function(i){y("identify",k.i=i)};
-          y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
-          k=y._={s:[],t:[+new Date],c:{},l:a};
-          })(window,document,"static.olark.com/jsclient/loader.js");
-          /* Add configuration calls below this comment */
-          olark.identify('1751-12112149-10-1389');`}</script>
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+;(function(o,l,a,r,k,y){if(o.olark)return;
+r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+y.extend=function(i,j){y("extend",i,j)};
+y.identify=function(i){y("identify",k.i=i)};
+y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+k=y._={s:[],t:[+new Date],c:{},l:a};
+})(window,document,"static.olark.com/jsclient/loader.js");
+/* Add configuration calls below this comment */
+olark.identify('1751-12112149-10-1389');`,
+            }}
+          />
         </body>
       </Html>
     );
