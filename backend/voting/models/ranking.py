7795410603a466df91095b7eb706202f728c7479
Etty <28976199+estyxx@users.noreply.github.com>
2022-02-07 10:30:04 +0000

    Update backend/voting/models/ranking.py
    Co-authored-by: Marco Acierno <marcoaciernoemail@gmail.com>

--- backend/voting/models/ranking.py
+++ backend/voting/models/ranking.py
@@ -27,10 +27,8 @@ class RankRequest(models.Model):
     def save(self, *args, **kwargs):
         super(RankRequest, self).save(*args, **kwargs)
 
-        exising_submissions = self.rank_submissions.all()
-
         # do not recreate ranking
-        if exising_submissions:
+        if self.rank_submissions.exists():
             return
         ranked_submissions = self.build_ranking(self.conference)
 
