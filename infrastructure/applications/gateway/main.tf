62c162a8a15fe8773840aaf4af24b2e87280f877
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-18 04:44:14 +0100

    fix error

--- infrastructure/applications/gateway/main.tf
+++ infrastructure/applications/gateway/main.tf
@@ -12,7 +12,6 @@ module "lambda" {
   source = "../../components/application_lambda"
 
   application = local.application
-  local_path  = local.local_path
   role_arn    = data.aws_iam_role.lambda.arn
   memory_size = 1024
 
