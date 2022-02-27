1afd3ba8eb3c252e9904015f10cc37a508a43738
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-27 18:39:44 +0100

    Temporarily change the is training check

--- frontend/src/components/schedule-view/is-training.ts
+++ frontend/src/components/schedule-view/is-training.ts
@@ -2,9 +2,10 @@ import { Item } from "./types";
 
 // TODO: let's be consistent with naming (training vs tutorial)
 export const isTraining = (item: Item) => {
-  if (item.submission) {
-    return item.submission.type!.name.toLowerCase() === "tutorial";
-  }
+  // TODO Remove for now and only use the item type to check
+  // if (item.submission) {
+  //   return item.submission.type!.name.toLowerCase() === "tutorial";
+  // }
 
   return item.type.toLowerCase() === "training";
 };
