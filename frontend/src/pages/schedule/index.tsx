3aad832c1664a42492aa0bd986e3db446ab8f78c
Marco Acierno <marcoaciernoemail@gmail.com>
2022-03-02 02:56:30 +0100

    use server side props for now

---
+++ frontend/src/pages/schedule/index.tsx
@@ -0,0 +1,26 @@
+import { GetServerSideProps } from "next";
+
+import { getApolloClient } from "~/apollo/client";
+import { queryScheduleDays } from "~/types";
+
+const ScheduleIndex = () => {};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // TODO Convert to _middleware
+  const client = getApolloClient();
+
+  const out = await queryScheduleDays(client, {
+    code: process.env.conferenceCode,
+  });
+  const days = out.data.conference.days;
+  const firstDay = days[0].day;
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: `/schedule/${firstDay}`,
+    },
+  };
+};
+
+export default ScheduleIndex;
