6dd00e39383426f0e19d2f5aec8e3d6f20bd85d1
Marco Acierno <marcoaciernoemail@gmail.com>
2022-03-31 01:38:48 +0100

    Remove old SpeakerVoucher model (#2744)

--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -305,37 +305,3 @@ class ScheduleItemInvitation(ScheduleItem):
         proxy = True
         verbose_name = _("Schedule invitation")
         verbose_name_plural = _("Schedule invitations")
-
-
-class SpeakerVoucher(TimeStampedModel):
-    conference = models.ForeignKey(
-        Conference,
-        on_delete=models.PROTECT,
-        verbose_name=_("conference"),
-        related_name="+",
-    )
-    user_id = models.IntegerField(verbose_name=_("user"))
-
-    voucher_code = models.TextField(
-        help_text=_(
-            "Voucher code generated for this speaker. "
-            "If the speaker has multiple events, only one code will be generated."
-        ),
-        blank=False,
-        null=False,
-    )
-    pretix_voucher_id = models.IntegerField(
-        help_text=_("ID of the voucher in the Pretix database"),
-        blank=False,
-        null=False,
-    )
-
-    voucher_email_sent_at = models.DateTimeField(
-        help_text=_("When the email was last sent"), blank=True, null=True
-    )
-
-    class Meta:
-        unique_together = (
-            "conference",
-            "user_id",
-        )
