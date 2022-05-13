9673c9731eaf177406ce4cb6b5e7af7acb7aed64
Etty <beltrami.ester@gmail.com>
2022-05-13 13:46:19 +0100

    Add test

---
+++ backend/pretix/tests/test_user_ticket.py
@@ -0,0 +1,21 @@
+from django.test import override_settings
+
+from pretix import get_user_ticket
+
+
+@override_settings(PRETIX_API="https://pretix/api/")
+def test_user_ticket(requests_mock, user, conference_factory, pretix_user_tickets):
+    conference = conference_factory(pretix_organizer_id="org", pretix_event_id="event")
+    pretix_user_tickets[0]["attendee_email"] = user.email.uppercase()
+
+    requests_mock.get(
+        f"https://pretix/api/organizers/org/events/event/tickets/attendee-tickets?attendee_email={user.email}",
+        json=pretix_user_tickets,
+    )
+
+    ticket = get_user_ticket(conference, user.email, pretix_user_tickets[0]["id"])
+
+    assert ticket
+    assert ticket.id == pretix_user_tickets[0]["id"]
+    assert ticket.name.lower() == pretix_user_tickets[0]["attendee_name"].lower()
+    assert ticket.email == pretix_user_tickets[0]["attendee_email"]
