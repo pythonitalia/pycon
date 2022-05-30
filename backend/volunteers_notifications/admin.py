c1735564813e4a409d13845a3810095369afeb81
Marco Acierno <marcoaciernoemail@gmail.com>
2022-05-30 20:59:40 +0200

    Dispatch notifications after saving

--- backend/volunteers_notifications/admin.py
+++ backend/volunteers_notifications/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.db import transaction
 
 from domain_events.publisher import send_volunteers_push_notification
 from volunteers_notifications.models import Notification, VolunteerDevice
+
+
+def _send_notifications(notification: Notification):
+    for device in VolunteerDevice.objects.all():
+        send_volunteers_push_notification(
+            notification_id=notification.id, volunteers_device_id=device.id
+        )
 
 
 @admin.register(Notification)
@@ -16,13 +24,8 @@ class NotificationAdmin(admin.ModelAdmin):
         if change:
             return super().save_model(request, obj, form, change)
 
-        ret = super().save_model(request, obj, form, change)
-
-        for device in VolunteerDevice.objects.all():
-            send_volunteers_push_notification(
-                notification_id=obj.id, volunteers_device_id=device.id
-            )
-        return ret
+        transaction.on_commit(lambda: _send_notifications(obj))
+        return super().save_model(request, obj, form, change)
 
     def has_change_permission(self, request, obj=None) -> bool:
         if obj:
