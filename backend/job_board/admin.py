0472b79d0adbf4d14734baa427ccb5a5bc5ebd25
Marco Acierno <marcoaciernoemail@gmail.com>
2022-05-31 20:36:28 +0200

    Allow sorting job listings

--- backend/job_board/admin.py
+++ backend/job_board/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
 
 from .models import JobListing
 
 
 @admin.register(JobListing)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(OrderedModelAdmin):
     model = JobListing
-    list_display = ("title", "company")
+    list_display = ("title", "company", "move_up_down_links")
