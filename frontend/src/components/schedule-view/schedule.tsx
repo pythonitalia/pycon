9e5c1aa7d73557542a053db1e2fb2fbacd7460a7
Marco Acierno <marcoaciernoemail@gmail.com>
2022-06-02 18:43:50 +0200

    Make the schedule appear again

--- frontend/src/components/schedule-view/schedule.tsx
+++ frontend/src/components/schedule-view/schedule.tsx
@@ -283,7 +283,7 @@ export const Schedule: React.SFC<{
       >
         {slots
           .filter(
-            (slot) => isInPhotoMode && slot?.items[0]?.title !== "Registration",
+            (slot) => !isInPhotoMode || (isInPhotoMode && slot?.items[0]?.title !== "Registration")
           )
           .map((slot) => {
             const rowStart = rowStartPos;
