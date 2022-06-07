0c5f19ce31916a36137c7c8a1ffcc51b59cd0d30
Marco Acierno <marcoaciernoemail@gmail.com>
2022-06-07 23:09:52 +0200

    Add database variable

--- infrastructure/applications/applications.tf
+++ infrastructure/applications/applications.tf
@@ -6,13 +6,13 @@ locals {
 # Applications
 
 module "pretix" {
-  source = "./pretix"
-  count  = local.deploy_pretix ? 1 : 0
+  source       = "./pretix"
+  count        = local.deploy_pretix ? 1 : 0
   enable_proxy = local.enable_proxy
 }
 
 module "pycon_backend" {
-  source = "./pycon_backend"
+  source       = "./pycon_backend"
   enable_proxy = local.enable_proxy
 
   providers = {
@@ -49,5 +49,6 @@ module "email_templates" {
 # Other resources
 
 module "database" {
-  source = "./database"
+  source       = "./database"
+  enable_proxy = local.enable_proxy
 }
---
+++ infrastructure/applications/database/variable.tf
@@ -0,0 +1,1 @@
+variable "enable_proxy" {}
