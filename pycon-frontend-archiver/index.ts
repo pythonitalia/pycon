5e77eb531f35f904efe4a97c12120f88621e5825
Marco Acierno <marcoaciernoemail@gmail.com>
2022-06-27 22:15:29 +0200

    Update scraper

--- pycon-frontend-archiver/index.ts
+++ pycon-frontend-archiver/index.ts
@@ -21,21 +21,55 @@ const scape = async (url: string, host: string) => {
   console.log(`Scraping: ${url}`);
   try {
     const response = await fetch(url);
+
     const path = url.replace(host, "");
-
     const body = await response.text();
-    const newUrls = await findUrls(body);
 
-    await storePage(path, body);
+    const parsableBody = cheerio.load(body);
+
+    const newUrls = await findUrls(parsableBody);
+
+    await storeImages(parsableBody);
+    await storePage(path, parsableBody.html());
+
+    // const promises = [];
 
     for (const newUrl of newUrls) {
+      // promises.push(scape(`${host}${newUrl}`, host));
       await scape(`${host}${newUrl}`, host);
     }
 
-    console.log("links", newUrls);
+    // await Promise.all(promises);
   } catch (e) {
     console.log("Error while scraping", e);
   }
+};
+
+const storeImages = async (body: cheerio.CheerioAPI) => {
+  const images = body("img");
+  for (const image of images) {
+    const src = image.attribs.src;
+
+    if (
+      !src.startsWith("https://cdn.pycon.it/") ||
+      src.startsWith("data:image")
+    ) {
+      continue;
+    }
+
+    const newSrc = await downloadImage(src);
+    image.attribs.src = newSrc;
+  }
+};
+
+const downloadImage = async (src: string): Promise<string> => {
+  const filename = src.replace("https://cdn.pycon.it/", "");
+  const response = await fetch(src);
+  const path = `${BASE_OUTPUT_PATH}/images/${filename}`;
+
+  await fs.mkdir(pathModule.dirname(path), { recursive: true });
+  await fs.writeFile(path, Buffer.from(await response.arrayBuffer()));
+  return `/images/${filename}`;
 };
 
 const storePage = async (path: string, body: string) => {
@@ -44,33 +78,34 @@ const storePage = async (path: string, body: string) => {
   await fs.writeFile(finalPath, body);
 };
 
-const findUrls = async (body: string) => {
-  const parsableBody = cheerio.load(body);
-  const links = parsableBody("a, script");
+const findUrls = async (body: cheerio.CheerioAPI) => {
+  const links = body("a, script");
   const urlsFound: Set<string> = new Set();
 
-  links.map((_, linkElement) => {
-    const link = linkElement.attribs.href ?? linkElement.attribs.src;
+  for (const linkElement of links) {
+    const attrLink = linkElement.attribs.href ?? linkElement.attribs.src;
 
-    if (!link) {
-      return;
+    if (!attrLink) {
+      continue;
     }
 
+    const link = attrLink.split("?")[0];
+
     if (link.startsWith("http") || link.startsWith("mailto")) {
-      return;
+      continue;
     }
 
     if (PAGES_TO_IGNORE.includes(link)) {
-      return;
+      continue;
     }
 
     if (VISITED_URLS.has(link)) {
-      return;
+      continue;
     }
 
     VISITED_URLS.add(link);
     urlsFound.add(link);
-  });
+  }
 
   return urlsFound;
 };
