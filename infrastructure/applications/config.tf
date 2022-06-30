b04fe4e867ddef38231185376fe3f56f16f20499
Marco Acierno <marcoaciernoemail@gmail.com>
2022-06-30 12:49:13 +0200

    Upgrade terraform & fix warnings

--- infrastructure/applications/config.tf
+++ infrastructure/applications/config.tf
@@ -3,6 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "3.61.0"
+      configuration_aliases = [aws.us]
     }
   }
 
--- infrastructure/applications/pycon_backend/providers.tf
+++ infrastructure/applications/pycon_backend/providers.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-  alias = "us"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.61.0"
+      configuration_aliases = [aws.us]
+    }
+  }
 }
