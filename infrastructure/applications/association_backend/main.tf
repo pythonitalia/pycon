aa4f3a6d931fdcf09f670d680385d2f6dcc2ff8b
Marco Acierno <marcoaciernoemail@gmail.com>
2022-07-09 17:56:26 +0200

    Update all docker files

--- infrastructure/applications/association_backend/main.tf
+++ infrastructure/applications/association_backend/main.tf
@@ -52,7 +52,6 @@ module "lambda" {
   role_arn           = data.aws_iam_role.lambda.arn
   subnet_ids         = [for subnet in data.aws_subnet_ids.private.ids : subnet]
   security_group_ids = [data.aws_security_group.rds.id, data.aws_security_group.lambda.id]
-  architecture       = "arm64"
   env_vars = {
     DEBUG        = "false"
     DATABASE_URL = local.db_connection
