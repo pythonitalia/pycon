ae1b3bd1ee837861348d648ecd891aad944d4459
Marco Acierno <marcoaciernoemail@gmail.com>
2022-07-09 11:35:59 +0200

    changes

--- infrastructure/applications/pycon_backend/main.tf
+++ infrastructure/applications/pycon_backend/main.tf
@@ -58,6 +58,7 @@ module "lambda" {
   role_arn           = data.aws_iam_role.lambda.arn
   subnet_ids         = [for subnet in data.aws_subnet_ids.private.ids : subnet]
   security_group_ids = [data.aws_security_group.rds.id, data.aws_security_group.lambda.id]
+  architecture       = "arm64"
   env_vars = {
     DATABASE_URL                                  = local.db_connection
     DEBUG                                         = "False"
