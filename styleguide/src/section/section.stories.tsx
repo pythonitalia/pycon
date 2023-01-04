8a6c8715de149d948fe7ee419fcc485da4832f81
Marco Acierno <marcoaciernoemail@gmail.com>
2023-01-04 17:34:48 +0000

    Section: add support for background color (#61)

--- styleguide/src/section/section.stories.tsx
+++ styleguide/src/section/section.stories.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { ContainerSize } from "../container/container";
 import { Heading } from "../heading";
 import { Text } from "../text";
+import { Color } from "../types";
 import { Section } from "./section";
 
 export const Standard = ({
   containerSize = "base",
+  background,
 }: {
   containerSize: ContainerSize;
+  background: Color | "none";
 }) => {
   return (
-    <Section containerSize={containerSize}>
+    <Section containerSize={containerSize} background={background}>
       <Heading size={1}>Section!</Heading>
       <Text size={1}>Body Body Body Body Body Body Body Body</Text>
     </Section>
@@ -27,6 +30,31 @@ export default {
         options: ["base", "medium"],
       },
       defaultValue: "base",
+    },
+    background: {
+      defaultValue: "none",
+      control: {
+        type: "select",
+        options: [
+          "none",
+          "coral",
+          "caramel",
+          "cream",
+          "yellow",
+          "green",
+          "purple",
+          "pink",
+          "blue",
+          "red",
+          "success",
+          "warning",
+          "neutral",
+          "black",
+          "grey",
+          "white",
+          "milk",
+        ],
+      },
     },
   },
 };
--- styleguide/src/section/section.tsx
+++ styleguide/src/section/section.tsx
@@ -1,16 +1,42 @@
+import clsx from "clsx";
 import React from "react";
 import { Container } from "../container";
 import { ContainerSize } from "../container/container";
 import { Spacer } from "../spacer";
+import { Color } from "../types";
 
 type Props = {
   children: React.ReactNode;
   containerSize?: ContainerSize;
+  background?: Color | "none";
 };
 
-export const Section = ({ children, containerSize = "base" }: Props) => {
+export const Section = ({
+  children,
+  containerSize = "base",
+  background = "none",
+}: Props) => {
   return (
-    <div>
+    <div
+      className={clsx({
+        "bg-coral": background === "coral",
+        "bg-caramel": background === "caramel",
+        "bg-cream": background === "cream",
+        "bg-yellow": background === "yellow",
+        "bg-green": background === "green",
+        "bg-purple": background === "purple",
+        "bg-pink": background === "pink",
+        "bg-blue": background === "blue",
+        "bg-red": background === "red",
+        "bg-success": background === "success",
+        "bg-warning": background === "warning",
+        "bg-neutral": background === "neutral",
+        "bg-black": background === "black",
+        "bg-grey": background === "grey",
+        "bg-white": background === "white",
+        "bg-milk": background === "milk",
+      })}
+    >
       <Container size={containerSize}>
         <Spacer size="xl" />
         {children}
