4cbce2f09c4b34c75dd10cea30aac110016bbd7f
Marco Acierno <marcoaciernoemail@gmail.com>
2023-01-30 13:10:32 +0000

    Show Keynoters in Homepage (#3265)

--- frontend/src/components/keynoters-section/index.tsx
+++ frontend/src/components/keynoters-section/index.tsx
@@ -1,7 +1,21 @@
+import {
+  Button,
+  Container,
+  Heading,
+  Link,
+  Section,
+  SliderGrid,
+  Spacer,
+  SpeakerCard,
+  VerticalStack,
+} from "@python-italia/pycon-styleguide";
 import React from "react";
+import { FormattedMessage } from "react-intl";
 
 import { useCurrentLanguage } from "~/locale/context";
 import { useKeynotesSectionQuery } from "~/types";
+
+import { createHref } from "../link";
 
 export const KeynotersSection = () => {
   const language = useCurrentLanguage();
@@ -24,5 +38,45 @@ export const KeynotersSection = () => {
     return null;
   }
 
-  return null;
+  return (
+    <Section noContainer spacingSize="3xl" background="caramel">
+      <Container>
+        <Heading size="display2" align="center">
+          Keynotes
+        </Heading>
+        <Spacer size="2xl" />
+      </Container>
+      <SliderGrid mdCols={2} cols={3} justifyContent="center" wrap="wrap">
+        {keynotes.map((keynote) => (
+          <Link
+            noHover
+            href={createHref({
+              path: `/keynotes/${keynote.slug}`,
+              locale: language,
+            })}
+          >
+            <SpeakerCard
+              talkTitle={keynote.title}
+              portraitUrl={keynote.speakers[0].participant.photo}
+              speakerName={keynote.speakers[0].fullName}
+            />
+          </Link>
+        ))}
+      </SliderGrid>
+
+      <Container>
+        <Spacer size="2xl" />
+        <VerticalStack alignItems="center">
+          <Button
+            href={createHref({
+              path: "/tickets",
+              locale: language,
+            })}
+          >
+            <FormattedMessage id="ticketsOverview.buyTicketsSection" />
+          </Button>
+        </VerticalStack>
+      </Container>
+    </Section>
+  );
 };
