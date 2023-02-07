18ba487f7553f62cfa058a7dc4a0d752ddf605af
Marco Acierno <marcoaciernoemail@gmail.com>
2023-02-07 13:23:01 +0100

    Add more information in the reviews system

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import List, Optional
 
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import F, OuterRef, Subquery
 from django.db.models.aggregates import Aggregate
 from django.shortcuts import redirect
@@ -10,8 +10,9 @@ from django.urls import path, reverse
 from django.utils.safestring import mark_safe
 
 from grants.models import Grant
+from participants.models import Participant
 from reviews.models import AvailableScoreOption, ReviewSession, UserReview
-from submissions.models import Submission
+from submissions.models import Submission, SubmissionTag
 from users.client import get_users_full_data
 from users.models import User
 
@@ -20,9 +21,16 @@ class AvailableScoreOptionInline(admin.TabularInline):
     model = AvailableScoreOption
 
 
+def get_all_tags():
+    # todo improve :)
+    return SubmissionTag.objects.values_list("id", "name")
+
+
 class SubmitVoteForm(forms.Form):
     score = forms.ModelChoiceField(queryset=AvailableScoreOption.objects.all())
     comment = forms.CharField(required=False)
+    exclude = forms.MultipleChoiceField(choices=get_all_tags)
+    seen = forms.CharField()
     _next = forms.CharField(required=False)
     _skip = forms.CharField(required=False)
 
@@ -212,6 +220,21 @@ class ReviewSessionAdmin(admin.ModelAdmin):
             grant_link = (
                 reverse("admin:grants_grant_change", args=(grant.id,)) if grant else ""
             )
+
+            tags_already_excluded = request.GET.get("exclude", "").split(",")
+
+            used_tags = (
+                Submission.objects.filter(
+                    conference_id=proposal.conference_id,
+                )
+                .values_list("tags__id", flat=True)
+                .distinct()
+            )
+
+            tags_to_filter = (
+                SubmissionTag.objects.filter(id__in=used_tags).order_by("name").all()
+            )
+
             context = dict(
                 self.admin_site.each_context(request),
                 proposal=proposal,
@@ -231,20 +254,48 @@ class ReviewSessionAdmin(admin.ModelAdmin):
                 speaker=speaker,
                 has_requested_grant=grant is not None,
                 grant_link=grant_link,
+                participant=Participant.objects.filter(
+                    user_id=proposal.speaker_id,
+                    conference=proposal.conference,
+                ).first(),
+                tags_to_filter=tags_to_filter,
+                tags_already_excluded=tags_already_excluded,
+                seen=request.GET.get("seen", "").split(","),
             )
             return TemplateResponse(request, "review-proposal.html", context)
         elif request.method == "POST":
             form = SubmitVoteForm(request.POST)
+            form.is_valid()
 
-            if not form.is_valid():
-                pass
+            seen = [
+                str(id_) for id_ in form.cleaned_data.get("seen", "").split(",") if id_
+            ]
+            seen.append(str(review_item_id))
+
+            exclude = form.cleaned_data.get("exclude", [])
 
             if form.cleaned_data.get("_skip"):
                 # Skipping to the next item without voting
                 next_to_review = get_next_to_review_item_id(
-                    review_session, request.user, skip_item=review_item_id
+                    review_session,
+                    request.user,
+                    skip_item=review_item_id,
+                    exclude=exclude,
+                    seen=seen,
                 )
             elif form.cleaned_data.get("_next"):
+                if not form.is_valid():
+                    messages.error(request, "Invalid vote")
+                    return redirect(
+                        reverse(
+                            "admin:reviews-vote-proposal",
+                            kwargs={
+                                "review_session_id": review_session_id,
+                                "review_item_id": review_item_id,
+                            },
+                        )
+                    )
+
                 # User is saving their vote
                 values = {
                     "user_id": request.user.id,
@@ -264,8 +315,30 @@ class ReviewSessionAdmin(admin.ModelAdmin):
                     },
                 )
                 next_to_review = get_next_to_review_item_id(
-                    review_session, request.user
+                    review_session, request.user, exclude=exclude, seen=seen
                 )
+
+            if not next_to_review:
+                messages.warning(
+                    request, "No new items to review, showing an already seen one."
+                )
+                next_to_review = get_next_to_review_item_id(
+                    review_session,
+                    request.user,
+                    skip_item=review_item_id,
+                    exclude=exclude,
+                )
+
+                if not next_to_review:
+                    messages.warning(request, "No new proposal to review.")
+                    return redirect(
+                        reverse(
+                            "admin:reviews-recap",
+                            kwargs={
+                                "review_session_id": review_session_id,
+                            },
+                        )
+                    )
 
             return redirect(
                 reverse(
@@ -275,12 +348,19 @@ class ReviewSessionAdmin(admin.ModelAdmin):
                         "review_item_id": next_to_review,
                     },
                 )
+                + f"?exclude={','.join(exclude)}&seen={','.join(seen)}"
             )
 
 
 def get_next_to_review_item_id(
-    review_session: ReviewSession, user: User, skip_item: Optional[int] = None
+    review_session: ReviewSession,
+    user: User,
+    skip_item: Optional[int] = None,
+    exclude: List[int] = None,
+    seen: List[int] = None,
 ) -> Optional[int]:
+    exclude = exclude or []
+    seen = seen or []
     already_reviewed = UserReview.objects.filter(
         user_id=user.id,
         review_session_id=review_session.id,
@@ -288,15 +368,18 @@ def get_next_to_review_item_id(
 
     if review_session.is_proposals_review:
         already_reviewed_ids = already_reviewed.values_list("proposal_id", flat=True)
+        allowed_tags = SubmissionTag.objects.exclude(id__in=exclude)
         unvoted_item = (
             review_session.conference.submissions.exclude(
-                id__in=list(already_reviewed_ids) + [skip_item]
+                id__in=list(already_reviewed_ids) + [skip_item] + seen,
             )
             .order_by("?")
+            .filter(tags__in=allowed_tags)
             .first()
         )
+
     elif review_session.is_grants_review:
         already_reviewed_ids = already_reviewed.values_list("grant_id", flat=True)
         raise ValueError("implement me")
 
-    return unvoted_item.id
+    return unvoted_item.id if unvoted_item else None
