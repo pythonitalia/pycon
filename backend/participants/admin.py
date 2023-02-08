78a5f5f33e3345e0cf97ddb1c6e2114560a16cd5
Marco Acierno <marcoaciernoemail@gmail.com>
2023-02-08 18:50:54 +0100

    Participant and reviews flow improvements

--- backend/participants/admin.py
+++ backend/participants/admin.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from participants.models import Participant
 from users.autocomplete import UsersBackendAutocomplete
@@ -32,3 +33,42 @@ class ParticipantForm(forms.ModelForm):
 class ParticipantAdmin(AdminUsersMixin, SearchUsersMixin):
     user_fk = "user_id"
     form = ParticipantForm
+    list_display = (
+        "user_display_name",
+        "conference",
+    )
+    list_filter = ("conference",)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "conference",
+                    "user_id",
+                    "photo",
+                    "photo_preview",
+                    "bio",
+                    "website",
+                    "twitter_handle",
+                    "instagram_handle",
+                    "linkedin_url",
+                    "facebook_url",
+                    "mastodon_handle",
+                    "speaker_level",
+                    "previous_talk_video",
+                ),
+            },
+        ),
+    )
+    readonly_fields = (
+        "photo_preview",
+        "user_display_name",
+    )
+
+    def user_display_name(self, obj):
+        if obj:
+            return self.get_user_display_name(obj.user_id)
+
+    def photo_preview(self, obj):
+        if obj:
+            return mark_safe(f'<img src="{obj.photo}" width="200" />')
