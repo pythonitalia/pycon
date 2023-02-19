228090b237e20fb7c96064ca2c180da555b88d97
Marco Acierno <marcoaciernoemail@gmail.com>
2023-02-19 15:31:14 +0000

    More sentry improvements in the Frontend (#3314)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,9 @@
 require("dotenv").config();
 const webpack = require("webpack");
 const path = require("path");
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const {
-  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-  SENTRY_ORG,
-  SENTRY_PROJECT,
-  SENTRY_AUTH_TOKEN,
-  NODE_ENV,
-  VERCEL_GITHUB_COMMIT_SHA,
   CONFERENCE_CODE,
   API_URL,
   API_TOKEN,
@@ -18,7 +12,7 @@ const {
   API_URL_SERVER,
 } = process.env;
 
-module.exports = {
+module.exports = withSentryConfig({
   i18n: {
     locales: ["default", "en", "it"],
     defaultLocale: "default",
@@ -66,7 +60,6 @@ module.exports = {
   env: {
     API_URL: API_URL,
     conferenceCode: CONFERENCE_CODE || "pycon-demo",
-    SENTRY_DSN: SENTRY_DSN || null,
     NEXT_PUBLIC_SOCIAL_CARD_SERVICE:
       NEXT_PUBLIC_SOCIAL_CARD_SERVICE ||
       "https://socialcards.python.it/api/card",
@@ -85,33 +78,12 @@ module.exports = {
   webpack: (config, options) => {
     config.resolve.alias["~"] = path.resolve(__dirname) + "/src";
 
-    if (!options.isServer) {
-      config.resolve.alias["@sentry/node"] = "@sentry/browser";
-    }
-
     config.plugins.push(
       new webpack.DefinePlugin({
         __DEV__: process.env.NODE_ENV !== "production",
       }),
     );
 
-    if (
-      SENTRY_ORG &&
-      SENTRY_PROJECT &&
-      SENTRY_AUTH_TOKEN &&
-      SENTRY_DSN &&
-      NODE_ENV === "production"
-    ) {
-      config.plugins.push(
-        new SentryWebpackPlugin({
-          include: ".next",
-          ignore: ["node_modules"],
-          urlPrefix: "~/_next",
-          release: VERCEL_GITHUB_COMMIT_SHA,
-        }),
-      );
-    }
-
     return config;
   },
-};
+});
