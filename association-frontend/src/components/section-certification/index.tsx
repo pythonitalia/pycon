d4e3445a0a881dd699d87a5a2920359cf8cc2662
Marco Acierno <marcoaciernoemail@gmail.com>
2023-03-07 17:40:49 +0100

    Add certification page

---
+++ association-frontend/src/components/section-certification/index.tsx
@@ -0,0 +1,54 @@
+import { SectionItem } from "~/components/section-item";
+
+export const SectionCertification = () => {
+  return (
+    <SectionItem
+      title={"Attestato Validazione Corso di Studi 🐍"}
+      withBackground={true}
+      overlay={false}
+      textTheme={"black"}
+      backgroundImageClass={"bg-white-background"}
+    >
+      <p className="mx-auto mb-4 text-xl text-black">
+        I corsi elencati in questa pagina sono offerti da Enti Scolastici,
+        pubblici o privati, che ci hanno volontariamente sottoposto il relativo
+        programma di studi.
+      </p>
+      <p className="mx-auto mb-4 text-xl text-black">
+        L'Associazione, a titolo assolutamente gratuito, li ha visionati e
+        ritenuti meritevoli di apparire in questa pagina, in quanto adeguati dal
+        punto di vista qualitativo dei contenuti e quantitativo delle ore di
+        insegnamento.
+      </p>
+      <ul className="mb-4">
+        <li>
+          <a
+            className="underline text-xl"
+            rel="noopener"
+            target="_blank"
+            href="https://www.centrocasnati.it/index.php/sections/liceo-game-development/"
+          >
+            Liceo Game Developement - Casnati - Como
+          </a>
+        </li>
+        <li>
+          <a
+            className="underline text-xl"
+            rel="noopener"
+            target="_blank"
+            href="https://www.itsrizzoli.it/"
+          >
+            ITS Angelo Rizzoli - Milano
+          </a>
+        </li>
+      </ul>
+      <p className="mx-auto mb-4 text-xl text-black">
+        L'attestato consente all'Ente di definire il proprio programma di studi
+        come “Approvato da Python Italia” e di mostrare il logo
+        dell'Associazione sui propri canali di comunicazione. L'attestato ha
+        validità di un anno e viene prorogato automaticamente nel caso il
+        programma didattico resti idoneo ai requisiti dell'Associazione.
+      </p>
+    </SectionItem>
+  );
+};
---
+++ association-frontend/src/pages/attestato.tsx
@@ -0,0 +1,11 @@
+import { SectionCertification } from "~/components/section-certification";
+
+const CertificationPage = () => {
+  return (
+    <>
+      <SectionCertification />
+    </>
+  );
+};
+
+export default CertificationPage;
