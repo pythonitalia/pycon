7b86c5b47c1de385c8a09c0870e20d2c8e491927
Marco Acierno <marcoaciernoemail@gmail.com>
2023-03-18 13:58:36 +0100

    rem

--- infrastructure/azure-applications/applications.tf
+++ infrastructure/azure-applications/applications.tf
@@ -25,13 +25,3 @@ module "pycon_backend" {
   resource_group_location = local.resource_group_location
   githash                 = var.githash_pycon_backend
 }
-
-module "users_backend" {
-  source = "./users_backend"
-
-  is_prod                 = local.is_prod
-  workspace               = local.workspace
-  resource_group_name     = local.resource_group_name
-  resource_group_location = local.resource_group_location
-  githash                 = var.githash_users_backend
-}
--- infrastructure/azure-applications/users_backend/resource_group.tf
+++
@@ -1,3 +0,0 @@
-data "azurerm_resource_group" "pythonitalia" {
-  name = var.resource_group_name
-}
