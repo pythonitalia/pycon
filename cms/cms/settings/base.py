da94f3d923d7348e19332c2372a126c152de684c
Marco Acierno <marcoaciernoemail@gmail.com>
2023-03-19 21:14:39 +0100

    whitenoise

--- cms/cms/settings/base.py
+++ cms/cms/settings/base.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
