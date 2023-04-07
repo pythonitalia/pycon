f1c96ca11e48c6e1d38d673bf16a6c045f5b8616
Marco Acierno <marcoaciernoemail@gmail.com>
2023-04-07 23:18:44 +0200

    Fix misalign with CardPart SideText

--- styleguide/src/multiple-parts-card/sidetext.tsx
+++ styleguide/src/multiple-parts-card/sidetext.tsx
@@ -8,7 +8,12 @@ type Props = {
 
 export const SideText = ({ children, className }: Props) => {
   return (
-    <div className={clsx("px-4 py-2 md:py-3.5 lg:px-6 text-left", className)}>
+    <div
+      className={clsx(
+        "px-4 py-2 md:py-3.5 lg:px-6 text-left flex items-center",
+        className
+      )}
+    >
       {children}
     </div>
   );
