b3fb2ae40d4be59e32d9fc542f38a3a7b41c8509
Marco Acierno <marcoaciernoemail@gmail.com>
2023-05-08 20:18:05 +0200

    Remove unused code

--- frontend/src/pages/api/badge.tsx
+++ frontend/src/pages/api/badge.tsx
@@ -1,59 +1,11 @@
-import satori from "satori";
-
 import type { NextRequest } from "next/server";
-
-import { Badge } from "~/components/badge";
-
-import { BADGE_WIDTH_PX, BADGE_HEIGHT_PX } from "../badge";
 
 export const config = {
   runtime: "edge",
 };
 
-const regularFont = fetch(
-  new URL("../../social-card-font/GeneralSans-Regular.otf", import.meta.url),
-).then((res) => res.arrayBuffer());
-const semiBoldFont = fetch(
-  new URL("../../social-card-font/GeneralSans-Semibold.otf", import.meta.url),
-).then((res) => res.arrayBuffer());
-
 const handler = async (req: NextRequest) => {
-  const regularFontData = await regularFont;
-  const semiBoldFontData = await semiBoldFont;
-
-  const svg = await satori(
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        fontFamily: '"GeneralSans"',
-      }}
-    >
-      <Badge />
-    </div>,
-    {
-      debug: false,
-      width: BADGE_WIDTH_PX,
-      height: BADGE_HEIGHT_PX,
-      fonts: [
-        {
-          name: "GeneralSans",
-          data: regularFontData,
-          style: "normal",
-          weight: 400,
-        },
-        {
-          name: "GeneralSans",
-          data: semiBoldFontData,
-          style: "normal",
-          weight: 600,
-        },
-      ],
-    },
-  );
-
-  return new Response(svg);
+  return new Response("hello");
 };
 
 export default handler;
