4b5b18d2f8a49dacfaf4ee0f5e3e0a417f911101
Patrick Arminio <patrick.arminio@gmail.com>
2023-05-20 15:51:00 +0100

    Add api for single scan

--- backend/api/badge_scanner/query.py
+++ backend/api/badge_scanner/query.py
@@ -6,12 +6,23 @@ from api.permissions import IsAuthenticated
 from badge_scanner import models
 
 
-from api.types import Paginated, PageInfo
+from api.types import Paginated
 from .types import BadgeScan
 
 
 @strawberry.type
 class BadgeScannerQuery:
+    @strawberry.field(permission_classes=[IsAuthenticated])
+    def badge_scan(self, info: Info, id: strawberry.ID) -> BadgeScan | None:
+        try:
+            scan = models.BadgeScan.objects.get(
+                id=id, scanned_by_id=info.context.request.user.id
+            )
+        except models.BadgeScan.DoesNotExist:
+            return None
+
+        return BadgeScan.from_db(scan)
+
     @strawberry.field(permission_classes=[IsAuthenticated])
     def badge_scans(
         self, info: Info, conference_code: str, page: int | None = 1
---
+++ backend/api/tests/schema/badge_scanner/get_single_scan.py
@@ -0,0 +1,76 @@
+import pytest
+from badge_scanner.models import BadgeScan
+
+pytestmark = pytest.mark.django_db
+
+
+def _get_scan_query(graphql_client, variables):
+    return graphql_client.query(
+        """
+        query GetScan($id: ID!) {
+            badgeScan(id: $id) {
+                id
+            }
+        }
+        """,
+        variables=variables,
+    )
+
+
+def test_raises_an_error_when_user_is_not_authenticated(graphql_client):
+    resp = _get_scan_query(
+        graphql_client,
+        variables={"id": "1"},
+    )
+
+    assert resp["errors"]
+    assert resp["errors"][0]["message"] == "User not logged in"
+
+
+def test_works(user, graphql_client, conference):
+    graphql_client.force_login(user)
+
+    scan = BadgeScan.objects.create(
+        scanned_by_id=user.id,
+        conference=conference,
+        scanned_user_id=1,
+        badge_url="https://foo.bar",
+        notes="",
+    )
+
+    resp = _get_scan_query(
+        graphql_client,
+        variables={"id": scan.id},
+    )
+
+    assert resp["data"]["badgeScan"]["id"] == str(scan.id)
+
+
+def test_returns_none_when_scan_does_not_exist(user, graphql_client):
+    graphql_client.force_login(user)
+
+    resp = _get_scan_query(
+        graphql_client,
+        variables={"id": "1"},
+    )
+
+    assert resp["data"]["badgeScan"] == None
+
+
+def test_returns_none_when_scan_by_other_user(user, graphql_client, conference):
+    graphql_client.force_login(user)
+
+    scan = BadgeScan.objects.create(
+        scanned_by_id=2,
+        conference=conference,
+        scanned_user_id=1,
+        badge_url="https://foo.bar",
+        notes="",
+    )
+
+    resp = _get_scan_query(
+        graphql_client,
+        variables={"id": scan.id},
+    )
+
+    assert resp["data"]["badgeScan"] == None
