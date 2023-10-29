b00100fc076c716524103f02f47b391d94eab03d
Marco Acierno <marcoaciernoemail@gmail.com>
2023-10-29 20:27:48 +0100

    imports

---
+++ backend/api/association_membership/mutations/__init__.py
@@ -0,0 +1,7 @@
+from .manage_user_subscription import manage_user_subscription
+from .subscribe_user_to_association import subscribe_user_to_association
+
+__all__ = [
+    "manage_user_subscription",
+    "subscribe_user_to_association",
+]
