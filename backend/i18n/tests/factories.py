04093cb887f768140934ab0274d5691a9889137d
Marco Acierno <marcoaciernoemail@gmail.com>
2023-11-05 17:21:28 +0100

    Deps update

--- backend/i18n/tests/factories.py
+++ backend/i18n/tests/factories.py
@@ -1,20 +1,33 @@
+import faker
 import factory
 from django.conf import settings
 from i18n.strings import LazyI18nString
 
 
-class LanguageFactory(factory.Faker):
-    def generate(self, extra_kwargs=None):
-        kwargs = {}
-        kwargs.update(extra_kwargs or {})
-        kwargs.pop("locale")
+class LanguageFactory(factory.fuzzy.BaseFuzzyAttribute):
+    _registry = {}
 
-        locale_to_faker = {"en": "en_US", "it": "it_IT"}
+    def __init__(self, provider, extra_kwargs=None, defaults=None):
+        self.provider = provider
+        self.extra_kwargs = extra_kwargs or {}
+        self._defaults = defaults or {}
 
+    def fuzz(self):
         data = {}
 
         for lang, _ in settings.LANGUAGES:
-            fake = self._get_faker(locale_to_faker[lang])
-            data[lang] = fake.format(self.provider, **kwargs)
+            fake = self.get_faker(lang)
+            data[lang] = fake.format(self.provider, **self.extra_kwargs)
 
         return LazyI18nString(data)
+
+    @classmethod
+    def get_faker(cls, lang):
+        locale_to_faker = {"en": "en_US", "it": "it_IT"}
+        locale = locale_to_faker[lang]
+
+        if locale in cls._registry:
+            return cls._registry[locale]
+
+        cls._registry[locale] = faker.Faker(locale=locale)
+        return cls._registry[locale]
