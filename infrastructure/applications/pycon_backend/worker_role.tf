cefdc0737b1223f2ded4b8c3a124a94fcf78c78c
Marco Acierno <marcoaciernoemail@gmail.com>
2023-12-10 19:10:41 +0100

    worker terraform

---
+++ infrastructure/applications/pycon_backend/worker_role.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_role" "worker" {
+  name = "pythonit-${terraform.workspace}-worker"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "worker_ecs_policy" {
+  role       = aws_iam_role.instance.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "worker" {
+  name = "pythonit-${terraform.workspace}-worker-instance-profile"
+  role = aws_iam_role.instance.name
+}
+
+resource "aws_iam_role_policy" "worker" {
+  name = "pythonit-${terraform.workspace}-worker-policy"
+  role = aws_iam_role.instance.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "cloudwatch:PutMetricData",
+        "ds:CreateComputer",
+        "ds:DescribeDirectories",
+        "ec2:DescribeInstanceStatus",
+        "ec2:CreateNetworkInterface",
+        "logs:*",
+        "ssm:*",
+        "ec2messages:*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage",
+        "ses:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
