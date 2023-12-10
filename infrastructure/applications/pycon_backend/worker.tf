ed4bf7fcdd5025da5079f15b234c7a9647d77f59
Marco Acierno <marcoaciernoemail@gmail.com>
2023-12-10 19:30:54 +0100

    syntax error

--- infrastructure/applications/pycon_backend/worker.tf
+++ infrastructure/applications/pycon_backend/worker.tf
@@ -1,7 +1,3 @@
-data "aws_db_instance" "database" {
-  db_instance_identifier = "pythonit-${terraform.workspace}"
-}
-
 resource "aws_ecs_cluster" "pretix" {
   name = "pythonit-${terraform.workspace}-worker"
 }
