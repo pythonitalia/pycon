e87ab1ee1903d574c213b5af344bf1661317ee12
Etty <beltrami.ester@gmail.com>
2023-12-30 12:10:07 +0100

    Limit filtering

--- backend/grants/admin.py
+++ backend/grants/admin.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional
 from countries.filters import CountryFilter
 from django.urls import path
 from django.template.response import TemplateResponse
-from django.db.models import Count
+from django.db.models import Count, Sum
 from helpers.constants import GENDERS
 from django import forms
 from django.contrib import admin, messages
@@ -568,52 +568,57 @@ class GrantAdmin(ExportMixin, admin.ModelAdmin):
         Custom view for summarizing Grant data in the Django admin.
         Aggregates data by country and status, and applies request filters.
         """
-        # Initialize statuses
         statuses = Grant.Status.choices
 
         # Apply filters to the Grant queryset
-        filter_params = request.GET.dict()
-        filtered_grants = Grant.objects.filter(**filter_params)
+        filtered_grants, formatted_filters = self._filter_and_format_grants(request)
 
         # Aggregate grant data by 'travelling_from' and 'status'
-        grants_data = filtered_grants.values("travelling_from", "status").annotate(
-            total=Count("id")
-        )
+        grants_by_country = filtered_grants.values(
+            "travelling_from", "status"
+        ).annotate(total=Count("id"))
 
         # Process and aggregate data for display
         (
-            summary,
-            totals_by_status,
+            country_stats,
+            status_totals,
             totals_per_continent,
-        ) = self._aggregate_data_by_country(grants_data, statuses)
-        gender_summary = self._aggregate_data_by_gender(filtered_grants, statuses)
-        human_readable_filters = self._format_filters_for_display(filter_params)
+        ) = self._aggregate_data_by_country(grants_by_country, statuses)
+        gender_stats = self._aggregate_data_by_gender(filtered_grants, statuses)
+        financial_summary, total_amount = self._aggregate_financial_data_by_status(
+            filtered_grants, statuses
+        )
+
         # Sort the summary data
-        sorted_summary = dict(sorted(summary.items(), key=lambda x: (x[0][0], x[0][2])))
+        sorted_country_stats = dict(
+            sorted(country_stats.items(), key=lambda x: (x[0][0], x[0][2]))
+        )
 
         context = {
-            "summary": sorted_summary,
+            "country_stats": sorted_country_stats,
             "statuses": statuses,
             "genders": {code: name for code, name in GENDERS},
-            "total": filtered_grants.count(),
-            "totals_by_status": totals_by_status,
+            "financial_summary": financial_summary,
+            "total_amount": total_amount,
+            "total_grants": filtered_grants.count(),
+            "status_totals": status_totals,
             "totals_per_continent": totals_per_continent,
-            "gender_summary": gender_summary,
-            "filters": human_readable_filters,
+            "gender_stats": gender_stats,
+            "filters": formatted_filters,
             **self.admin_site.each_context(request),
         }
         return TemplateResponse(request, "admin/grants/grant_summary.html", context)
 
-    def _aggregate_data_by_country(self, grants_data, statuses):
+    def _aggregate_data_by_country(self, grants_by_country, statuses):
         """
         Aggregates grant data by country and status.
         """
 
         summary = {}
-        totals_by_status = {status[0]: 0 for status in statuses}
+        status_totals = {status[0]: 0 for status in statuses}
         totals_per_continent = {}
 
-        for data in grants_data:
+        for data in grants_by_country:
             country = countries.get(code=data["travelling_from"])
             continent = country.continent.name if country else "Unknown"
             country_name = f"{country.name} {country.emoji}" if country else "Unknown"
@@ -625,14 +630,14 @@ class GrantAdmin(ExportMixin, admin.ModelAdmin):
                 summary[key] = {status[0]: 0 for status in statuses}
 
             summary[key][data["status"]] += data["total"]
-            totals_by_status[data["status"]] += data["total"]
+            status_totals[data["status"]] += data["total"]
 
             # Update continent totals
             if continent not in totals_per_continent:
                 totals_per_continent[continent] = {status[0]: 0 for status in statuses}
             totals_per_continent[continent][data["status"]] += data["total"]
 
-        return summary, totals_by_status, totals_per_continent
+        return summary, status_totals, totals_per_continent
 
     def _aggregate_data_by_gender(self, filtered_grants, statuses):
         """
@@ -656,10 +661,41 @@ class GrantAdmin(ExportMixin, admin.ModelAdmin):
 
         return gender_summary
 
-    def _format_filters_for_display(self, filter_params):
+    def _aggregate_financial_data_by_status(self, filtered_grants, statuses):
         """
-        Formats raw filter keys to user-friendly names.
+        Aggregates financial data (total amounts) by grant status.
         """
+        financial_data = filtered_grants.values("status").annotate(
+            total_amount_sum=Sum("total_amount")
+        )
+        financial_summary = {status[0]: 0 for status in statuses}
+        overall_total = 0
+
+        for data in financial_data:
+            status = data["status"]
+            total_amount = data["total_amount_sum"] or 0
+            financial_summary[status] += total_amount
+            overall_total += total_amount
+
+        return financial_summary, overall_total
+
+    def _filter_and_format_grants(self, request):
+        """
+        Filters the Grant queryset based on request parameters and
+        formats the filter keys for display.
+        """
+        field_lookups = [
+            "__exact",
+            "__in",
+            "__gt",
+            "__lt",
+            "__contains",
+            "__startswith",
+            "__endswith",
+            "__range",
+            "__isnull",
+        ]
+
         filter_mapping = {
             "conference__id": "Conference ID",
             "status": "Status",
@@ -669,20 +705,30 @@ class GrantAdmin(ExportMixin, admin.ModelAdmin):
             "travelling_from": "Country",
         }
 
-        def map_filter_key(key):
-            for suffix in [
-                "__exact",
-                "__in",
-                "__gt",
-                "__lt",
-                "__contains",
-                "__startswith",
-            ]:
-                if key.endswith(suffix):
-                    return filter_mapping.get(key[: -len(suffix)], key)
-            return filter_mapping.get(key, key)
+        allowed_filters = {
+            f + lookup for f in filter_mapping.keys() for lookup in field_lookups
+        }
 
-        return {map_filter_key(key): value for key, value in filter_params.items()}
+        def map_filter_key(key):
+            base_key = next(
+                (
+                    key[: -len(lookup)]
+                    for lookup in field_lookups
+                    if key.endswith(lookup)
+                ),
+                key,
+            )
+            return filter_mapping.get(base_key, base_key.capitalize())
+
+        # Apply filters to the Grant queryset and format filter keys for display
+        raw_filter_params = {
+            k: v for k, v in request.GET.items() if k in allowed_filters
+        }
+        filter_params = {map_filter_key(k): v for k, v in raw_filter_params.items()}
+
+        filtered_grants = Grant.objects.filter(**raw_filter_params)
+
+        return filtered_grants, filter_params
 
     class Media:
         js = ["admin/js/jquery.init.js"]
