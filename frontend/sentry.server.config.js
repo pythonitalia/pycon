aeee18ac458d42b1c6686f8be7a1255fce1f60d2
Marco Acierno <marcoaciernoemail@gmail.com>
2023-12-30 18:34:31 +0100

    config changes

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -1,11 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+import { sentryConfig } from "./sentry";
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  tracesSampleRate: 0.1,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [new Sentry.Replay()],
-});
+Sentry.init(sentryConfig);
