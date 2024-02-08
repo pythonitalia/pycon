089aaac83745d40c7c520808ab7b3957cc05a979
Etty <beltrami.ester@gmail.com>
2024-02-08 10:29:14 +0000

    Re-add imports in admin.__init__

---
+++ backend/conferences/admin/__init__.py
@@ -0,0 +1,8 @@
+from .conference import (
+    ConferenceAdmin,  #  noqa
+    KeynoteAdmin,  #  noqa
+    DeadlineAdmin,  #  noqa
+    TopicAdmin,  #  noqa
+    AudienceLevelAdmin,  #  noqa
+    BadgeScanAdmin,  #  noqa
+)  #  noqa
