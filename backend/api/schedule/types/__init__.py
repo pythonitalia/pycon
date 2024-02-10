d6aad69e91b9fc93e4a1d5370a7dd25a64c4a8e8
Marco Acierno <marcoaciernoemail@gmail.com>
2024-02-10 20:43:45 +0100

    fix

--- backend/api/schedule/types/__init__.py
+++ backend/api/schedule/types/__init__.py
@@ -2,5 +2,10 @@ from .day_room import DayRoom  # noqa
 from .day import Day  # noqa
 from .room import Room  # noqa
 from .schedule_item import ScheduleItem  # noqa
+from .schedule_invitation import (
+    ScheduleInvitation,  # noqa
+    ScheduleInvitationOption,  # noqa
+    ScheduleInvitationDate,  # noqa
+)
 from .schedule_item_user import ScheduleItemUser  # noqa
 from .slot import ScheduleSlot, ScheduleSlotType  # noqa
