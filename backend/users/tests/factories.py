99782008f2fb01efe23eac0dd9e776928a2ed5d9
Marco Acierno <marcoaciernoemail@gmail.com>
2024-02-19 03:44:44 +0100

    fix

--- backend/users/tests/factories.py
+++ backend/users/tests/factories.py
@@ -10,7 +10,7 @@ from users.models import User
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
-        skip_postgeneration_save = True
+        skip_postgeneration_save = False
 
     username = factory.Faker("word")
     email = factory.Faker("email")
