ba144f6fa1de473fbb48a01fcc32dd9a4b08898d
Marco Acierno <marcoaciernoemail@gmail.com>
2024-03-10 23:42:08 +0100

    me

--- backend/api/users/tests/test_me.py
+++ backend/api/users/tests/test_me.py
@@ -1,3 +1,5 @@
+from association_membership.enums import MembershipStatus
+from association_membership.models import Membership
 from conferences.tests.factories import ConferenceFactory
 import pytest
 
@@ -42,3 +44,54 @@ def test_user_schedule_favourites_calendar_url_fails_with_invalid_conference(
 
     me = response["data"]["me"]
     assert me["userScheduleFavouritesCalendarUrl"] is None
+
+
+def test_is_python_italia_member_user_without_membership(graphql_client, user):
+    graphql_client.force_login(user)
+    response = graphql_client.query(
+        """query {
+        me {
+            isPythonItaliaMember
+        }
+    }"""
+    )
+
+    me = response["data"]["me"]
+    assert me["isPythonItaliaMember"] is False
+
+
+def test_is_python_italia_member_with_membership_exists_and_active(
+    graphql_client, user
+):
+    Membership.objects.create(user=user, status=MembershipStatus.ACTIVE)
+
+    graphql_client.force_login(user)
+    response = graphql_client.query(
+        """query {
+        me {
+            isPythonItaliaMember
+        }
+    }"""
+    )
+
+    me = response["data"]["me"]
+    assert me["isPythonItaliaMember"] is True
+
+
+@pytest.mark.parametrize(
+    "status", [MembershipStatus.CANCELED, MembershipStatus.PENDING]
+)
+def test_is_python_italia_member_with_expired_membership(graphql_client, user, status):
+    Membership.objects.create(user=user, status=status)
+
+    graphql_client.force_login(user)
+    response = graphql_client.query(
+        """query {
+        me {
+            isPythonItaliaMember
+        }
+    }"""
+    )
+
+    me = response["data"]["me"]
+    assert me["isPythonItaliaMember"] is False
