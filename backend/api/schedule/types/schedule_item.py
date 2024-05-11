199f7b7fdbee850fc03148e42b09d3fb4d04fac6
Marco Acierno <marcoaciernoemail@gmail.com>
2024-05-11 23:28:40 +0200

    Add new recruiting type and ability to link to pages in schedule items

--- backend/api/schedule/types/schedule_item.py
+++ backend/api/schedule/types/schedule_item.py
@@ -33,6 +33,7 @@ class ScheduleItem:
         "AudienceLevel", strawberry.lazy("api.conferences.types")
     ] | None
     youtube_video_id: str | None
+    link: str
 
     abstract: str
     elevator_pitch: str
