4858afd05e5902c88dafd3a82f100d92c257a332
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-06 22:29:50 +0100

    Make all files public for now (#3864)

--- backend/pycon/tests/test_storage.py
+++ backend/pycon/tests/test_storage.py
@@ -1,35 +1,7 @@
 from unittest import mock
-from files_upload.constants import get_max_upload_size_bytes
 from files_upload.models import File
 from files_upload.tests.factories import FileFactory
 from pycon.storages import CustomFileSystemStorage, CustomS3Boto3Storage
-
-
-def test_s3_storage_generate_upload_url(mocker):
-    boto3_mock = mocker.patch("pycon.storages.boto3.client")
-    boto3_mock.return_value.generate_presigned_post.return_value = {
-        "url": "http://example.org/pycon-test",
-        "fields": {
-            "key": "test.txt",
-            "bucket": "pycon-test",
-        },
-    }
-    file = FileFactory(type=File.Type.PROPOSAL_MATERIAL)
-    storage = CustomS3Boto3Storage()
-    return_value = storage.generate_upload_url(file)
-    boto3_mock.return_value.generate_presigned_post.assert_called_once_with(
-        Bucket=mock.ANY,
-        Key=file.file.name,
-        ExpiresIn=3600,
-        Conditions=[["content-length-range", 1, get_max_upload_size_bytes(file.type)]],
-    )
-
-    assert return_value.url == "http://example.org/pycon-test"
-    assert return_value.fields == {
-        "key": "test.txt",
-        "bucket": "pycon-test",
-    }
-    assert return_value.fields_as_json == '{"key": "test.txt", "bucket": "pycon-test"}'
 
 
 def test_s3_storage_generate_public_upload_url(mocker):
