d5aeb5039f1566462d077416b273024ede4864f8
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-13 02:23:34 +0200

    idle worker kill process

--- backend/pycon/tasks.py
+++ backend/pycon/tasks.py
@@ -1,18 +1,25 @@
 from .celery import app
+from django.core.cache import cache
 
 
 @app.task
 def check_for_idle_heavy_processing_workers():
-    ...
-    # inspect = app.control.inspect()
-    # active_tasks = inspect.active()
+    inspect = app.control.inspect()
+    workers_stats = inspect.stats()
+    active_tasks = inspect.active()
 
-    # active_count = sum(
-    #     len(tasks) for tasks in active_tasks.values()
-    #     if 'heavy_processing' in [task['delivery_info']['routing_key'] for task in tasks]
-    # ) if active_tasks else 0
+    for worker_name in workers_stats.keys():
+        if not worker_name.startswith("heavyprocessing"):
+            continue
 
-    # for worker_name, tasks in active_tasks.items():
-    #     if len(tasks) > 0:
-    #         continue
-    # pass
+        current_tasks = active_tasks.get(worker_name, [])
+        if current_tasks:
+            continue
+
+        cache_key = f"celery-workers-idle:{worker_name}"
+
+        if cache.get(cache_key) == "idle":
+            app.control.broadcast("shutdown", destination=[worker_name])
+            continue
+
+        cache.set(cache_key, "idle", timeout=60 * 5)
