836293976d22a86be5cd4780e3d166531e2d1672
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-13 02:27:12 +0200

    change approach

--- backend/pycon/tasks.py
+++ backend/pycon/tasks.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from .celery import app
 from django.core.cache import cache
 
@@ -8,18 +9,21 @@ def check_for_idle_heavy_processing_workers():
     workers_stats = inspect.stats()
     active_tasks = inspect.active()
 
-    for worker_name in workers_stats.keys():
+    for worker_name, stats in workers_stats.items():
         if not worker_name.startswith("heavyprocessing"):
             continue
 
+        cache_key = f"celery-workers-idle:{worker_name}"
         current_tasks = active_tasks.get(worker_name, [])
+
         if current_tasks:
+            cache.delete(cache_key)
             continue
 
-        cache_key = f"celery-workers-idle:{worker_name}"
+        total_jobs_executed = Counter(stats.get("total", {})).total()
 
-        if cache.get(cache_key) == "idle":
+        if cache.get(cache_key) == total_jobs_executed:
             app.control.broadcast("shutdown", destination=[worker_name])
             continue
 
-        cache.set(cache_key, "idle", timeout=60 * 5)
+        cache.set(cache_key, total_jobs_executed)
