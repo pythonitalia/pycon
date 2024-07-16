4b50bac0a4866579457ea6689e5947cc42a9eb14
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-17 00:18:21 +0200

    remove test msgs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,3 @@
-import logfire
-import logging
 from typing import Any
 
 from django.http.request import HttpRequest
@@ -8,13 +6,8 @@ from strawberry.django.views import GraphQLView as BaseGraphQLVew
 from api.context import Context
 
 
-logger = logging.getLogger(__name__)
-
-
 class GraphQLView(BaseGraphQLVew):
     allow_queries_via_get = False
 
     def get_context(self, request: HttpRequest, response: Any) -> Context:
-        logger.info("test log")
-        logfire.info("test logfire")
         return Context(request=request, response=response)
