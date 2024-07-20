507cdcb14c1cae86fd89e075a2180d077a2fabf1
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-20 23:27:47 +0200

    Refactor logic:

--- backend/video_uploads/tasks.py
+++ backend/video_uploads/tasks.py
@@ -1,17 +1,8 @@
-from concurrent.futures import ThreadPoolExecutor
 import logging
-from functools import wraps
 
 from django.utils import timezone
-from django.core.files.storage import storages
-from io import BytesIO
-import zipfile
-import os
-import tempfile
-from urllib.parse import urlparse, unquote
 
-import requests
-from pycon.constants import GB, KB, MB
+from video_uploads.transfer import WetransferProcessing
 from video_uploads.models import WetransferToS3TransferRequest
 from pycon.celery import app
 
@@ -19,32 +10,7 @@ from pycon.celery import app
 logger = logging.getLogger(__name__)
 
 
-def wetransfer_error_handling(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            logger.exception(
-                "Error processing wetransfer to s3 transfer request: %s", e
-            )
-            request_id = args[0]
-            wetransfer_to_s3_transfer_request = (
-                WetransferToS3TransferRequest.objects.get(id=request_id)
-            )
-            wetransfer_to_s3_transfer_request.status = (
-                WetransferToS3TransferRequest.Status.FAILED
-            )
-            wetransfer_to_s3_transfer_request.failed_reason = str(e)
-            wetransfer_to_s3_transfer_request.save(
-                update_fields=["status", "failed_reason"]
-            )
-
-    return wrapper
-
-
 @app.task
-@wetransfer_error_handling
 def process_wetransfer_to_s3_transfer_request(request_id):
     wetransfer_to_s3_transfer_request = WetransferToS3TransferRequest.objects.get(
         id=request_id
@@ -66,222 +32,24 @@ def process_wetransfer_to_s3_transfer_request(request_id):
     wetransfer_to_s3_transfer_request.started_at = timezone.now()
     wetransfer_to_s3_transfer_request.save(update_fields=["status", "started_at"])
 
-    conference = wetransfer_to_s3_transfer_request.conference
-    wetransfer_url = wetransfer_to_s3_transfer_request.wetransfer_url
-    parsed_wetransfer_url = urlparse(wetransfer_url)
-
-    hostname = parsed_wetransfer_url.hostname
-    _, _, transfer_id, security_hash = parsed_wetransfer_url.path.split("/")
-
-    response = requests.post(
-        f"https://{hostname}/api/v4/transfers/{transfer_id}/download",
-        json={"security_hash": security_hash, "intent": "entire_transfer"},
-    )
-
-    if response.status_code == 403:
-        raise Exception("Wetransfer download link expired")
-
-    response.raise_for_status()
-
-    wetransfer_response = response.json()
-    direct_link = wetransfer_response["direct_link"]
-    parsed_direct_url = urlparse(direct_link)
-    direct_link_filename = unquote(parsed_direct_url.path.split("/")[-1])
-    _, ext = os.path.splitext(direct_link_filename)
-
-    imported_files = []
-    storage = storages["default"]
-
-    def save_file(filename, obj):
-        imported_files.append(filename)
-        storage.save(
-            f"conference-videos/{conference.code}/{filename}",
-            obj,
-        )
-
-    def read_and_save_file(file_obj, filename):
-        logger.info(
-            "Processing file %s of transfer request %s",
-            filename,
-            wetransfer_to_s3_transfer_request.id,
-        )
-
-        try:
-            file_data = BytesIO()
-            while True:
-                chunk = file_obj.read(500 * MB)
-
-                if not chunk:
-                    break
-
-                file_data.write(chunk)
-
-            save_file(filename, file_data)
-        finally:
-            file_obj.close()
-
-    def download_part(start, end, index, use_s3_as_temporary_storage):
-        logger.info(
-            "Downloading chunk %s-%s for wetransfer_to_s3_transfer_request %s",
-            start,
-            end,
-            wetransfer_to_s3_transfer_request.id,
-        )
-        part_file = tempfile.NamedTemporaryFile(
-            "wb",
-            prefix=f"wetransfer_{wetransfer_to_s3_transfer_request.id}.part{index}",
-            suffix=ext,
-            delete=False,
-        )
-
-        headers = {"Range": f"bytes={start}-{end}"}
-        with requests.get(direct_link, headers=headers, stream=True) as response:
-            for chunk in response.iter_content(chunk_size=512 * KB):
-                if chunk:  # pragma: no cover
-                    part_file.write(chunk)
-
-        part_file.flush()
-
-        if use_s3_as_temporary_storage:
-            logging.info(
-                "Moving part file to S3 for wetransfer_to_s3_transfer_request %s",
-                wetransfer_to_s3_transfer_request.id,
-            )
-            temporary_s3_path = f"wetransfer-to-s3-transfers/{wetransfer_to_s3_transfer_request.id}/{part_file.name[5:]}"
-            storage.save(
-                temporary_s3_path,
-                open(part_file.name, "rb"),
-            )
-
-            part_file.close()
-            os.unlink(part_file.name)
-            return temporary_s3_path
-
-        return open(part_file.name, "rb")
-
-    def _determinate_num_parts(file_size):
-        if file_size >= 50 * GB:
-            return 8
-
-        if file_size >= 10 * GB:
-            return 4
-
-        return 1
-
     try:
-        head_response = requests.head(direct_link)
-        file_size = int(head_response.headers["Content-Length"])
-        num_parts = _determinate_num_parts(file_size)
-        use_s3_as_temporary_storage = num_parts > 1
-        chunk_size = file_size // num_parts
-        files_parts = []
+        processing = WetransferProcessing(wetransfer_to_s3_transfer_request)
+        imported_files = processing.run()
 
-        with ThreadPoolExecutor(max_workers=num_parts) as executor:
-            futures = []
-            for i in range(num_parts):
-                start = i * chunk_size
-                end = start + chunk_size - 1 if i < num_parts - 1 else file_size
-                futures.append(
-                    executor.submit(
-                        download_part, start, end, i, use_s3_as_temporary_storage
-                    )
-                )
-
-            for future in futures:
-                files_parts.append(future.result())
-
-            if use_s3_as_temporary_storage:
-                full_file = tempfile.NamedTemporaryFile(
-                    "wb",
-                    prefix=f"wetransfer_{wetransfer_to_s3_transfer_request.id}",
-                    suffix=ext,
-                    delete=False,
-                )
-
-                for part_id, file_part_s3_path in enumerate(files_parts):
-                    logger.info(
-                        "Merging file parts of transfer %s (part #%s)",
-                        wetransfer_to_s3_transfer_request.id,
-                        part_id,
-                    )
-
-                    file_part = storage.open(file_part_s3_path)
-                    while True:
-                        chunk = file_part.read(500 * MB)
-                        if not chunk:
-                            break
-                        full_file.write(chunk)
-
-                    full_file.flush()
-                    file_part.close()
-            else:
-                full_file = files_parts[0]
-
-            full_file.flush()
-
-        with open(full_file.name, "rb") as temp_file_read:
-            match ext[1:]:
-                case "zip":
-                    logger.info(
-                        "Unzipping file for wetransfer_to_s3_transfer_request %s",
-                        wetransfer_to_s3_transfer_request.id,
-                    )
-                    futures = []
-
-                    with ThreadPoolExecutor(max_workers=num_parts) as executor:
-                        # read the zip upload all files to s3
-                        with zipfile.ZipFile(temp_file_read, "r") as zip_ref:
-                            for file_info in zip_ref.infolist():
-                                if not is_file_allowed(file_info):
-                                    continue
-
-                                filename = file_info.filename
-                                file_obj = zip_ref.open(filename)
-                                futures.append(
-                                    executor.submit(
-                                        read_and_save_file,
-                                        file_obj,
-                                        filename,
-                                    )
-                                )
-
-                            for future in futures:
-                                future.result()
-                case _:
-                    logger.info(
-                        "Uploading file for wetransfer_to_s3_transfer_request %s",
-                        wetransfer_to_s3_transfer_request.id,
-                    )
-                    save_file(direct_link_filename, temp_file_read)
-    finally:
-        full_file.close()
-        try:
-            os.unlink(full_file.name)
-        except Exception:
-            ...
-
-        if use_s3_as_temporary_storage:
-            for file_part_s3_path in files_parts:
-                storage.delete(file_part_s3_path)
-
-    wetransfer_to_s3_transfer_request.status = WetransferToS3TransferRequest.Status.DONE
-    wetransfer_to_s3_transfer_request.imported_files = imported_files
-    wetransfer_to_s3_transfer_request.finished_at = timezone.now()
-    wetransfer_to_s3_transfer_request.save(
-        update_fields=["status", "imported_files", "finished_at"]
-    )
-
-
-def is_file_allowed(file_info):
-    filename = file_info.filename
-
-    if file_info.is_dir():
-        return False
-
-    if "__MACOSX" in filename:
-        return False
-
-    if ".DS_Store" in filename:
-        return False
-
-    return True
+        wetransfer_to_s3_transfer_request.status = (
+            WetransferToS3TransferRequest.Status.DONE
+        )
+        wetransfer_to_s3_transfer_request.imported_files = imported_files
+        wetransfer_to_s3_transfer_request.finished_at = timezone.now()
+        wetransfer_to_s3_transfer_request.save(
+            update_fields=["status", "imported_files", "finished_at"]
+        )
+    except Exception as e:
+        logger.exception("Error processing wetransfer to s3 transfer request: %s", e)
+        wetransfer_to_s3_transfer_request.status = (
+            WetransferToS3TransferRequest.Status.FAILED
+        )
+        wetransfer_to_s3_transfer_request.failed_reason = str(e)
+        wetransfer_to_s3_transfer_request.save(
+            update_fields=["status", "failed_reason"]
+        )
---
+++ backend/video_uploads/transfer.py
@@ -0,0 +1,297 @@
+from io import BufferedReader, BytesIO
+import zipfile
+from django.core.files.storage import storages
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+import os
+import tempfile
+import requests
+from urllib.parse import unquote, urlparse
+from pycon.constants import GB, KB, MB
+from video_uploads.models import WetransferToS3TransferRequest
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class DownloadPart:
+    index: int
+    start: int
+    end: int
+
+    @property
+    def header(self):
+        return {"Range": f"bytes={self.start}-{self.end}"}
+
+
+class WetransferProcessing:
+    def __init__(self, wetransfer_to_s3_request: WetransferToS3TransferRequest) -> None:
+        self.wetransfer_to_s3_request = wetransfer_to_s3_request
+        self.imported_files = []
+        self.full_file_name = None
+        self.parts_refs = []
+
+    def run(self):
+        self.storage = storages["default"]
+        self.download_link = self.get_download_link()
+        self.filename, self.extension = self.get_filename_and_extension()
+
+        file_total_size = self.get_file_total_size()
+        download_parts = self.determine_download_parts(file_total_size)
+        self.use_s3_as_temporary_storage = len(download_parts) > 1
+
+        max_workers = os.cpu_count()
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            self.parts_refs = self.download_parts(download_parts, executor)
+            try:
+                self.full_file_name = self.merge_parts(self.parts_refs)
+
+                with open(self.full_file_name, "rb") as full_file:
+                    imported_files = self.process_downloaded_file(full_file, executor)
+            finally:
+                self.cleanup()
+
+        return imported_files
+
+    def process_downloaded_file(
+        self, full_file: BufferedReader, executor: ThreadPoolExecutor
+    ):
+        extension = self.extension[1:]
+        match extension:
+            case "zip":
+                imported_files = self.process_zip_file(full_file, executor)
+            case _:
+                imported_files = [self.save_file_to_s3(self.filename, full_file)]
+
+        return imported_files
+
+    def process_zip_file(
+        self, full_file: BufferedReader, executor: ThreadPoolExecutor
+    ) -> list[str]:
+        futures = []
+        filenames = []
+        with zipfile.ZipFile(full_file, "r") as zip_ref:
+            for file_info in zip_ref.infolist():
+                if not is_file_allowed(file_info):
+                    continue
+
+                filename = file_info.filename
+                file_obj = zip_ref.open(filename)
+                futures.append(
+                    executor.submit(self.process_zip_file_obj, file_obj, filename)
+                )
+
+            for future in futures:
+                filenames.append(future.result())
+
+        return filenames
+
+    def process_zip_file_obj(self, file_obj: BufferedReader, filename: str):
+        logger.info(
+            "Processing zip file %s for wetransfer_to_s3_transfer_request %s",
+            filename,
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+
+        try:
+            file_data = BytesIO()
+            while True:
+                chunk = file_obj.read(500 * MB)
+
+                if not chunk:
+                    break
+
+                file_data.write(chunk)
+
+            self.save_file_to_s3(filename, file_data)
+            return filename
+        finally:
+            file_obj.close()
+
+    def save_file_to_s3(self, filename: str, file_data: BytesIO):
+        logger.info(
+            "Uploading file %s to S3 for wetransfer_to_s3_transfer_request %s",
+            filename,
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+
+        conference = self.wetransfer_to_s3_transfer_request.conference
+        self.storage.save(
+            f"conference-videos/{conference.code}/{filename}",
+            file_data,
+        )
+        return filename
+
+    def cleanup(self):
+        if self.full_file_name:
+            os.unlink(self.full_file_name)
+
+        if self.use_s3_as_temporary_storage:
+            for part in self.parts_refs:
+                self.storage.delete(part)
+
+    def merge_parts(self, parts_refs: list[str]) -> str:
+        logger.info(
+            "Merging parts for wetransfer_to_s3_transfer_request %s",
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+
+        if not self.use_s3_as_temporary_storage:
+            return parts_refs[0]
+
+        merged_file = tempfile.NamedTemporaryFile(
+            "wb",
+            prefix=f"wetransfer_{self.wetransfer_to_s3_transfer_request.id}",
+            suffix=self.extension,
+            delete=False,
+        )
+
+        for s3_path in parts_refs:
+            logger.info(
+                "Downloading part %s from S3 for wetransfer_to_s3_transfer_request %s",
+                s3_path,
+                self.wetransfer_to_s3_transfer_request.id,
+            )
+
+            file_part = self.storage.open(s3_path)
+            while True:
+                chunk = file_part.read(500 * MB)
+
+                if not chunk:
+                    break
+
+                merged_file.write(chunk)
+            merged_file.flush()
+
+        return merged_file.name
+
+    def download_parts(
+        self, download_parts: list[DownloadPart], executor: ThreadPoolExecutor
+    ) -> list[str]:
+        parts_futures = []
+        for download_part in download_parts:
+            parts_futures.append(executor.submit(self.download_part, download_part))
+
+        for future in parts_futures:
+            future.result()
+
+        logger.info(
+            "Finished downloading all parts for wetransfer_to_s3_transfer_request %s",
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+        return parts_futures
+
+    def download_part(self, download_part: DownloadPart) -> str:
+        logger.info(
+            "Downloading chunk %s-%s for wetransfer_to_s3_transfer_request %s",
+            download_part.start,
+            download_part.end,
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+
+        part_file = tempfile.NamedTemporaryFile(
+            "wb",
+            prefix=f"wetransfer_{self.wetransfer_to_s3_transfer_request.id}.part{download_part.index}",
+            suffix=self.extension,
+            delete=False,
+        )
+
+        with requests.get(
+            self.download_link, headers=download_part.header, stream=True
+        ) as response:
+            for chunk in response.iter_content(chunk_size=512 * KB):
+                if chunk:  # pragma: no cover
+                    part_file.write(chunk)
+
+        part_file.flush()
+
+        if not self.use_s3_as_temporary_storage:
+            return part_file.name
+
+        logging.info(
+            "Moving part file %s to S3 for wetransfer_to_s3_transfer_request %s",
+            part_file.name,
+            self.wetransfer_to_s3_transfer_request.id,
+        )
+
+        s3_path = self.file_part_s3_path(download_part)
+        self.storage.save(
+            s3_path,
+            open(part_file.name, "rb"),
+        )
+
+        part_file.close()
+        os.unlink(part_file.name)
+        return s3_path
+
+    def determine_download_parts(self, file_size: int) -> list[DownloadPart]:
+        num_parts = self._determinate_total_num_of_parts(file_size)
+        chunk_size = file_size // num_parts
+        download_parts = []
+
+        for i in range(num_parts):
+            start = i * chunk_size
+            end = start + chunk_size - 1 if i < num_parts - 1 else file_size
+            download_parts.append(DownloadPart(index=i, start=start, end=end))
+
+        return download_parts
+
+    def _determinate_total_num_of_parts(self, file_size: int) -> int:
+        if file_size >= 50 * GB:
+            return 8
+
+        if file_size >= 10 * GB:
+            return 4
+
+        return 1
+
+    def get_file_total_size(self) -> int:
+        head_response = requests.head(self.download_link)
+        return int(head_response.headers["Content-Length"])
+
+    def get_download_link(self) -> str:
+        wetransfer_url = self.wetransfer_to_s3_request.wetransfer_url
+        parsed_wetransfer_url = urlparse(wetransfer_url)
+        hostname = parsed_wetransfer_url.hostname
+        _, _, transfer_id, security_hash = parsed_wetransfer_url.path.split("/")
+
+        response = requests.post(
+            f"https://{hostname}/api/v4/transfers/{transfer_id}/download",
+            json={"security_hash": security_hash, "intent": "entire_transfer"},
+        )
+
+        if response.status_code == 403:
+            raise Exception("Wetransfer download link expired")
+
+        response.raise_for_status()
+
+        wetransfer_response = response.json()
+        direct_link = wetransfer_response["direct_link"]
+        return direct_link
+
+    def get_filename_and_extension(self):
+        direct_link_filename = unquote(self.download_link.path.split("/")[-1])
+        _, ext = os.path.splitext(direct_link_filename)
+
+        return direct_link_filename, ext
+
+    def file_part_s3_path(self, download_part: DownloadPart) -> str:
+        return f"wetransfer-to-s3-transfers/{self.wetransfer_to_s3_transfer_request.id}/part{download_part.index}{self.extension}"
+
+
+def is_file_allowed(file_info: zipfile.ZipInfo) -> bool:
+    filename = file_info.filename
+
+    if file_info.is_dir():
+        return False
+
+    if "__MACOSX" in filename:
+        return False
+
+    if ".DS_Store" in filename:
+        return False
+
+    return True
