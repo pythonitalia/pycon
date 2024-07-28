ec0913d042d159dff13667c8347a6f379598e9f4
Marco Acierno <marcoaciernoemail@gmail.com>
2024-07-28 21:01:05 +0200

    create billing data

--- backend/api/billing/mutation.py
+++
@@ -1,8 +0,0 @@
-from strawberry.tools import create_type
-
-from api.billing.mutations.update_billing_address import update_billing_address
-
-BillingMutation = create_type(
-    "BillingMutation",
-    [update_billing_address],
-)
