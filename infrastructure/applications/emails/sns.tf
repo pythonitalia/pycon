568a4ceb71b1968651b25610cc0981c8b40b92bb
Marco Acierno <marcoaciernoemail@gmail.com>
2024-08-24 16:48:03 +0200

    change

--- infrastructure/applications/emails/sns.tf
+++ infrastructure/applications/emails/sns.tf
@@ -10,5 +10,5 @@ resource "aws_sns_topic" "emails_updates" {
 resource "aws_sns_topic_subscription" "backend" {
   topic_arn = aws_sns_topic.emails_updates.arn
   protocol  = "https"
-  endpoint  = "https://sns:${local.sns_webhook_secret}@${local.admin_domain}/notifications/sns-webhook"
+  endpoint  = "https://${local.admin_domain}/notifications/sns-webhook?api_key=${local.sns_webhook_secret}"
 }
