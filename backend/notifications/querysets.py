f40de3f552442ead58b1b059d82dca80cd14b725
Marco Acierno <marcoaciernoemail@gmail.com>
2024-08-26 20:32:49 +0200

    unused code

--- backend/notifications/querysets.py
+++ backend/notifications/querysets.py
@@ -13,19 +13,10 @@ class EmailTemplateQuerySet(ConferenceQuerySetMixin, models.QuerySet):
     ) -> "EmailTemplate":
         return self.get(identifier=identifier)
 
-    def system(self):
-        return self.filter(is_system_template=True)
-
 
 class SentEmailQuerySet(ConferenceQuerySetMixin, models.QuerySet):
     def pending(self):
         return self.filter(status=self.model.Status.pending)
-
-    def for_recipient(self, user):
-        return self.filter(recipient=user)
-
-    def for_template(self, identifier: "EmailTemplate.Identifier"):
-        return self.filter(email_template__identifier=identifier)
 
     def get_by_message_id(self, message_id):
         return self.filter(message_id=message_id).first()
