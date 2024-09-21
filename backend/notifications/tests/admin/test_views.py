555b24b5208ef5f81e84b8f67128a9e738902ed0
Marco Acierno <marcoaciernoemail@gmail.com>
2024-09-22 01:28:26 +0200

    :x gp

--- backend/notifications/tests/admin/test_views.py
+++ backend/notifications/tests/admin/test_views.py
@@ -2,15 +2,17 @@ from notifications.admin.views import view_email_template, view_sent_email
 from notifications.tests.factories import EmailTemplateFactory, SentEmailFactory
 
 
-def test_view_email_template():
+def test_view_email_template(rf):
+    request = rf.get("/")
     email_template = EmailTemplateFactory(body="Hello world!")
-    response = view_email_template(None, email_template.id)
+    response = view_email_template(request, email_template.id)
 
     assert b"Hello world!" in response.content
 
 
-def test_view_sent_email():
+def test_view_sent_email(rf):
+    request = rf.get("/")
     sent_email = SentEmailFactory(body="Hello world!")
-    response = view_sent_email(None, sent_email.id)
+    response = view_sent_email(request, sent_email.id)
 
     assert b"Hello world!" in response.content
