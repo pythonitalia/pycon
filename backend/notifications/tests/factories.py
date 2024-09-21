f0c008167689e4bbb38c0fe6cefe4af5b785cf9a
Marco Acierno <marcoaciernoemail@gmail.com>
2024-09-22 01:54:16 +0200

    reduce flaky tests

--- backend/notifications/tests/factories.py
+++ backend/notifications/tests/factories.py
@@ -14,6 +14,7 @@ class EmailTemplateFactory(DjangoModelFactory):
 
     class Meta:
         model = EmailTemplate
+        django_get_or_create = ("conference", "identifier")
 
 
 class SentEmailFactory(DjangoModelFactory):
