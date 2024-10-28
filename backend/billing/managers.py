d8a0f25babf364cdbd87f9a09c0b8a5ef8ff19f6
Marco Acierno <marcoaciernoemail@gmail.com>
2024-10-28 15:05:35 +0100

    fix

--- backend/billing/managers.py
+++ backend/billing/managers.py
@@ -1,6 +1,8 @@
+from django.db import models
+
 from conferences.querysets import ConferenceQuerySetMixin
 
 
-class BillingAddressQuerySet(ConferenceQuerySetMixin):
+class BillingAddressQuerySet(ConferenceQuerySetMixin, models.QuerySet):
     def of_user(self, user):
         return self.filter(user=user)
