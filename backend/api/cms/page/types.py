f3191a4fbb9fd3b06480c39b9120513309ab3a01
Marco Acierno <marcoaciernoemail@gmail.com>
2024-12-02 03:53:16 +0000

    Add support to show/hide a page based on user's ticket status (#4211)

--- backend/api/cms/page/types.py
+++ backend/api/cms/page/types.py
@@ -19,11 +19,28 @@ class GenericPage:
     body: list[get_block_union()]  # type: ignore
 
     @classmethod
-    def from_model(cls, obj: GenericPageModel) -> Self:
+    def from_model(
+        cls, obj: GenericPageModel, *, can_see_page: bool | None = None
+    ) -> Self:
+        match can_see_page:
+            case None:
+                # They can see it
+                # and there are no restrictions
+                # so show the whole page
+                body = obj.body
+            case True:
+                # They can see the whole page
+                # so skip the first block that is used to tell users to authenticate or similar
+                body = obj.body[1:]
+            case False:
+                # Only show the first block
+                # that is used to tell users to authenticate or similar
+                body = [obj.body[0]]
+
         return cls(
             id=obj.id,
             title=obj.seo_title or obj.title,
             search_description=obj.search_description,
             slug=obj.slug,
-            body=[get_block(block.block_type).from_block(block) for block in obj.body],
+            body=[get_block(block.block_type).from_block(block) for block in body],
         )
