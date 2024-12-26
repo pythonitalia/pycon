c0a51b18392ab872e197fcac0837fce75b6a4ebd
Marco Acierno <marcoaciernoemail@gmail.com>
2024-12-26 20:23:35 +0100

    tests

--- backend/custom_admin/tests/test_templatetags.py
+++ backend/custom_admin/tests/test_templatetags.py
@@ -9,4 +9,8 @@ def test_empty_string_if_none():
 
 
 def test_to_json_for_prop():
+    assert to_json_for_prop([1, 2, 3]) == "[1, 2, 3]"
+    assert to_json_for_prop(None) == "null"
+    assert to_json_for_prop(123) == "123"
+    assert to_json_for_prop("abc") == "\\u0022abc\\u0022"
     assert to_json_for_prop({"a": 123}) == "{\\u0022a\\u0022: 123}"
