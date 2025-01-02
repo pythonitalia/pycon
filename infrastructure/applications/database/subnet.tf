42000a5e3511ab8da57fea00f6fddfc0fb4ab48d
Marco Acierno <marcoaciernoemail@gmail.com>
2025-01-02 04:16:26 +0100

    change

--- infrastructure/applications/database/subnet.tf
+++ infrastructure/applications/database/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "rds" {
   name = "pythonit-${terraform.workspace}-rds-subnet"
-  description = "Pythonit rds subnet"
+  description = "pythonit ${terraform.workspace} rds subnet"
   subnet_ids = var.private_subnets_ids
 
   tags = {
