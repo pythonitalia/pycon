9d9caa83133a230febe5d1ee708a9b97658a2fc4
Marco Acierno <marcoaciernoemail@gmail.com>
2025-02-16 00:08:02 +0100

    tests

--- backend/api/participants/tests/test_queries.py
+++ backend/api/participants/tests/test_queries.py
@@ -21,6 +21,54 @@ def test_query_participant(graphql_client):
     assert response["data"]["participant"]["fullname"] == participant.user.fullname
 
 
+def test_query_private_fields_of_own_user(graphql_client, user):
+    graphql_client.force_login(user)
+    participant = ParticipantFactory(
+        user=user,
+        speaker_availabilities={"test": "test"},
+        previous_talk_video="test",
+        speaker_level="level",
+    )
+
+    response = graphql_client.query(
+        """query Participant($id: ID!, $conference: String!) {
+        participant(id: $id, conference: $conference) {
+            id
+            speakerAvailabilities
+            previousTalkVideo
+            speakerLevel
+        }
+    }
+    """,
+        variables={"id": participant.hashid, "conference": participant.conference.code},
+    )
+
+    assert response["data"]["participant"]["speakerAvailabilities"] == {"test": "test"}
+    assert response["data"]["participant"]["previousTalkVideo"] == "test"
+    assert response["data"]["participant"]["speakerLevel"] == "level"
+
+
+def test_cannot_query_private_fields_of_other_user(graphql_client):
+    participant = ParticipantFactory()
+
+    response = graphql_client.query(
+        """query Participant($id: ID!, $conference: String!) {
+        participant(id: $id, conference: $conference) {
+            id
+            speakerAvailabilities
+            previousTalkVideo
+            speakerLevel
+        }
+    }
+    """,
+        variables={"id": participant.hashid, "conference": participant.conference.code},
+    )
+
+    assert response["data"]["participant"]["speakerAvailabilities"] is None
+    assert response["data"]["participant"]["previousTalkVideo"] is None
+    assert response["data"]["participant"]["speakerLevel"] is None
+
+
 def test_query_participant_with_wrong_conference(graphql_client):
     participant = ParticipantFactory()
 
