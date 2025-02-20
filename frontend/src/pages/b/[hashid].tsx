38c342631724a5deccd1083cd2cc449dcc560b6f
Marco Acierno <marcoaciernoemail@gmail.com>
2025-02-20 11:34:53 +0100

    Cleanup ticketid to hash

--- frontend/src/pages/b/[hashid].tsx
+++ frontend/src/pages/b/[hashid].tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps } from "next";
 
 import { getApolloClient } from "~/apollo/client";
-import { queryTicketIdToUserHashid } from "~/types";
+import { queryTicketIdToHashid } from "~/types";
 
 export const getServerSideProps: GetServerSideProps = async ({
   req,
@@ -9,15 +9,15 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const hashid = params.hashid as string;
   const client = getApolloClient(null, req.cookies);
-  const result = await queryTicketIdToUserHashid(client, {
+  const result = await queryTicketIdToHashid(client, {
     ticketId: hashid,
     conference: process.env.conferenceCode,
   });
 
-  if (result.data?.ticketIdToUserHashid) {
+  if (result.data?.ticketIdToHashid) {
     return {
       redirect: {
-        destination: `/profile/${result.data.ticketIdToUserHashid}`,
+        destination: `/profile/${result.data.ticketIdToHashid}`,
         permanent: false,
       },
     };
