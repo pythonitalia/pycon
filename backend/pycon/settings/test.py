592de08efe6832014b5d351a3ee9363a203aabc0
Ester Beltrami <beltrami.ester@gmail.com>
2025-11-30 10:51:34 +0000

    Remove `django-debug-toolbar` from test config to fix local test failure
    Disable `debug_toolbar` from `INSTALLED_APPS` and its middleware in
    `test.py` settings to ensure tests pass locally regardless of
    developer setup.

--- backend/pycon/settings/test.py
+++ backend/pycon/settings/test.py
@@ -1,7 +1,14 @@
-from .base import env
 from .base import *  # noqa
+from .base import INSTALLED_APPS, MIDDLEWARE, env
 
 IS_RUNNING_TESTS = True
+
+# Disable Django Debug Toolbar in tests
+ENABLE_DJANGO_DEBUG_TOOLBAR = False
+if "debug_toolbar" in INSTALLED_APPS:
+    INSTALLED_APPS.remove("debug_toolbar")
+if "debug_toolbar.middleware.DebugToolbarMiddleware" in MIDDLEWARE:
+    MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
 
 SECRET_KEY = "this-key-should-only-be-used-for-tests"
 HASHID_DEFAULT_SECRET_SALT = "only-for-tests"
