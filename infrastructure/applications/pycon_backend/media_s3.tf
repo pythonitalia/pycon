00c5306a1c661cebb6c64fbc2b91f883988c2d2a
Marco Acierno <marcoaciernoemail@gmail.com>
2026-01-05 05:57:57 +0100

    Initial setup

--- infrastructure/applications/pycon_backend/media_s3.tf
+++ infrastructure/applications/pycon_backend/media_s3.tf
@@ -13,3 +13,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backend_media" {
     }
   }
 }
+
+resource "aws_s3_bucket_notification" "new_file_uploaded" {
+  bucket = aws_s3_bucket.backend_media.id
+
+  queue {
+    queue_arn = aws_sqs_queue.new_file_uploaded.arn
+    events = ["s3:ObjectCreated:*"]
+    filter_prefix = "files/"
+  }
+}
