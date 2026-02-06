4790c16129e571ac0cea4c27ae7fc9d3ad265413
Patrick Arminio <patrick.arminio@gmail.com>
2026-02-07 00:20:34 +0100

    Add MX record for Gmail activation on pydata.it

--- infrastructure/global/domains/pydata_it/records.tf
+++ infrastructure/global/domains/pydata_it/records.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "pydata_it_txt" {
   records = ["google-site-verification=Lwmb3AJYmMsxy-guo-bUbV3j-Be1a0nbp9c2f4lPmSM"]
   ttl     = "60"
 }
+
+resource "aws_route53_record" "pydata_it_mx" {
+  zone_id = aws_route53_zone.pydata_it.id
+  name    = "pydata.it"
+  type    = "MX"
+  records = ["1 smtp.google.com."]
+  ttl     = "60"
+}
