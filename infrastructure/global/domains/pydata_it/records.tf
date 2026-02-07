05dbc71403b2db8281a674fb7bd0ffc3867db756
Patrick Arminio <patrick.arminio@gmail.com>
2026-02-07 18:05:04 +0000

    Update DKIM value for pydata (#4565)

--- infrastructure/global/domains/pydata_it/records.tf
+++ infrastructure/global/domains/pydata_it/records.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "pydata_it_dkim" {
   zone_id = aws_route53_zone.pydata_it.id
   name    = "google._domainkey.pydata.it"
   type    = "TXT"
-  records = ["v=DKIM1;k=rsa;p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr/vX4g9YEgRabzeWScaBvX4idgMMoTqtlUpRYnbgvoKOY198qYuXR0xtB1JcuVO8Q++9pzVDI2IJS1sFm0uK9uFtWbRLuu2PpyI3sADrJAYtryoyawe2GQgC83yn2aKtAYTdQXp2ZVEEn3TsmcsXPHQ8F+BZP36/5VX2N9VPpaJ0aNVlL9Osk6TXretidOgjrzrcnd+gIp0KU+oEodArZuvimngfk5/5b9m5Nhpg4kRvbZzznjWkGv+UAXjnkgclIt35h5LWkZK/s47V7nIBlAewEEdk93diC5C6JJnxaA9qEGof6RNbj5Qob/r1tZwhVcWHT8O64SzUaH6dsjBsIQIDAQAB"]
+  records = ["v=DKIM1;k=rsa;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjEv46PX4U/7HSrVmAvoPWP+ShH3qIjqDeTWZoX132QulLy3YNLjWFIrOPItRs4k13JvRor5GFtfq+xwXil7MXVVQ/6r4mB4WeJX29kCitlXrxm7m25r4Kh4Pomlk0/8VWvB/E8b1aT1l9p8h2VS+boOec0zC9DrqEGpfWQff3awIDAQAB"]
   ttl     = "60"
 }
 
