572ac71f9fdca3e542664b1129595b8a565a5529
Marco Acierno <marcoaciernoemail@gmail.com>
2026-08-09 00:03:54 +0200

    run before

--- backend/conferences/admin/conference.py
+++ backend/conferences/admin/conference.py
@@ -557,6 +557,6 @@ def cleanup_string(string: str) -> str:
     new_string = normalize(
         "NFKD", "".join(char for char in string if char.isprintable())
     ).lower()
-    new_string = re.sub(r"\s+", " ", new_string)
     new_string = new_string.replace("-", " ")
+    new_string = re.sub(r"\s+", " ", new_string)
     return new_string.strip()
