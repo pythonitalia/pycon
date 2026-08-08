5a977f9f272c6645d846b9a6b147f3054c7b6a22
Marco Acierno <marcoaciernoemail@gmail.com>
2026-08-08 23:07:56 +0100

    Handle more symbols in the video matcher (#4720)

--- backend/conferences/tests/test_admin.py
+++ backend/conferences/tests/test_admin.py
@@ -539,7 +539,7 @@ def test_video_uploaded_path_matcher(
     mocker.patch(
         "conferences.admin.conference.walk_conference_videos_folder",
         return_value=[
-            "conf/video-1/1-Kim Kitsuragi.mp4",
+            "conf/video-1/1-Kim-Kitsuragi.mp4",
             "conf/video-2/2-Opening.mp4",
             "conf/video-2/5-Klaasje, Harrier Du Bois.mp4",
             "conf/video-2/2-Harrier Du Bois, Klaasje.mp4",
@@ -627,7 +627,7 @@ def test_video_uploaded_path_matcher(
     event_ord_speakers.refresh_from_db()
 
     assert event_1.video_uploaded_path == "conf/video-2/2-Opening.mp4"
-    assert event_2.video_uploaded_path == "conf/video-1/1-Kim Kitsuragi.mp4"
+    assert event_2.video_uploaded_path == "conf/video-1/1-Kim-Kitsuragi.mp4"
     assert event_3.video_uploaded_path == "conf/video-2/5-Klaasje, Harrier Du Bois.mp4"
     assert event_klaasje_alone.video_uploaded_path == "conf/video-2/12-Klaasje.mp4"
     assert keynote_schedule.video_uploaded_path == "conf/9-Anwesha Das.mp4"
