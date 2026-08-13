d1e01d70821deb116a102f677d88040e3e0cb689
Patrick Arminio <patrick.arminio@gmail.com>
2026-08-13 21:52:05 +0200

    Let Strawberry Django optimize conference queries

--- backend/api/conferences/schema.py
+++ backend/api/conferences/schema.py
@@ -1,21 +1,12 @@
-from api.context import Info
 from api.conferences.types import Conference
 import strawberry
 import strawberry_django
-from strawberry_django.optimizer import OptimizerConfig, optimize
 
 from conferences import models
 
 
 @strawberry.type
 class ConferenceQuery:
-    @strawberry_django.field
-    def conference(self, info: Info, code: str) -> Conference:
-        conferences = models.Conference.objects.filter(code=code).prefetch_related(
-            "durations"
-        )
-        return optimize(
-            conferences,
-            info,
-            config=OptimizerConfig(enable_only=False),
-        ).get()
+    @strawberry_django.field(prefetch_related=["durations"])
+    def conference(self, code: str) -> Conference:
+        return models.Conference.objects.filter(code=code)
