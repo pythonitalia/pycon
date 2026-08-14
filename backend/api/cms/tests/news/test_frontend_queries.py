ddb01efe37ecdfd55d1836d9b037dc968ec9bed0
Patrick Arminio <patrick.arminio@gmail.com>
2026-08-14 18:57:35 +0200

    Test news article frontend queries

---
+++ backend/api/cms/tests/news/test_frontend_queries.py
@@ -0,0 +1,119 @@
+import datetime
+
+import pytest
+from wagtail.rich_text import RichText
+
+from api.cms.tests.factories import GenericPageFactory, SiteFactory
+from cms.components.news.tests.factories import NewsArticleFactory
+from users.tests.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+ALL_NEWS_ARTICLES_QUERY = """\
+query AllNewsArticles($hostname: String!, $language: String!) {
+  newsArticles(hostname: $hostname, language: $language) {
+    id
+    slug
+  }
+}
+"""
+
+
+NEWS_ARTICLE_QUERY = """\
+query NewsArticle($hostname: String!, $slug: String!, $language: String!) {
+  newsArticle(hostname: $hostname, slug: $slug, language: $language) {
+    id
+    title
+    excerpt
+    body
+    publishedAt
+    authorFullname
+  }
+}
+"""
+
+
+def test_all_news_articles_frontend_query(
+    graphql_client,
+    django_assert_num_queries,
+):
+    parent = GenericPageFactory()
+    articles = []
+
+    for index in range(4):
+        article = NewsArticleFactory(
+            title=f"Article {index}",
+            slug=f"article-{index}",
+            parent=parent,
+            owner=UserFactory(full_name=f"Author {index}"),
+            first_published_at=datetime.datetime(
+                2026,
+                1,
+                index + 1,
+                10,
+                0,
+                tzinfo=datetime.UTC,
+            ),
+        )
+        article.save_revision().publish()
+        articles.append(article)
+
+    SiteFactory(hostname="pycon", port=80, root_page=parent)
+
+    with django_assert_num_queries(7):
+        response = graphql_client.query(
+            ALL_NEWS_ARTICLES_QUERY,
+            variables={"hostname": "pycon", "language": "en"},
+        )
+
+    assert response == {
+        "data": {
+            "newsArticles": [
+                {"id": str(article.id), "slug": article.slug}
+                for article in reversed(articles)
+            ]
+        }
+    }
+
+
+def test_news_article_frontend_query(
+    graphql_client,
+    django_assert_num_queries,
+):
+    published_at = datetime.datetime(2026, 1, 1, 10, 0, tzinfo=datetime.UTC)
+    parent = GenericPageFactory()
+    article = NewsArticleFactory(
+        title="Strawberry Fields",
+        slug="strawberry-fields",
+        excerpt="A GraphQL story",
+        body=RichText("<p>News article body</p>"),
+        parent=parent,
+        owner=UserFactory(full_name="Ada Strawberry"),
+        first_published_at=published_at,
+    )
+    article.save_revision().publish()
+    SiteFactory(hostname="pycon", port=80, root_page=parent)
+
+    with django_assert_num_queries(5):
+        response = graphql_client.query(
+            NEWS_ARTICLE_QUERY,
+            variables={
+                "hostname": "pycon",
+                "slug": article.slug,
+                "language": "en",
+            },
+        )
+
+    assert response == {
+        "data": {
+            "newsArticle": {
+                "id": str(article.id),
+                "title": "Strawberry Fields",
+                "excerpt": "A GraphQL story",
+                "body": "<p>News article body</p>",
+                "publishedAt": published_at.isoformat(),
+                "authorFullname": "Ada Strawberry",
+            }
+        }
+    }
