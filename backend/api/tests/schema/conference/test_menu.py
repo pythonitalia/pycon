b2b71e9653a3517c3725220de39adf5392d7c227
Patrick Arminio <patrick.arminio@gmail.com>
2026-08-14 13:31:26 +0100

    Migrate menu GraphQL types to Strawberry Django (#4736)

--- backend/api/tests/schema/conference/test_menu.py
+++ backend/api/tests/schema/conference/test_menu.py
@@ -1,6 +1,8 @@
+from pytest import mark
+
 from cms.tests.factories import MenuFactory, MenuLinkFactory
 from conferences.tests.factories import ConferenceFactory
-from pytest import mark
+from pages.tests.factories import PageFactory
 
 
 @mark.django_db
@@ -55,3 +57,68 @@ def test_get_menu(
 
     assert "errors" not in resp
     assert len(resp["data"]["conference"]["menu"]["links"]) == 3
+
+
+@mark.parametrize("link_count", [1, 4])
+@mark.django_db
+def test_frontend_header_menus_query_is_constant(
+    graphql_client, django_assert_num_queries, link_count
+):
+    conference = ConferenceFactory()
+    for identifier in ["conference-nav", "program-nav"]:
+        menu = MenuFactory(identifier=identifier, conference=conference)
+        for _ in range(link_count):
+            MenuLinkFactory(
+                menu=menu,
+                page=PageFactory(conference=conference),
+            )
+
+    with django_assert_num_queries(9):
+        resp = graphql_client.query(
+            """
+            query Header($code: String!) {
+                conference(code: $code) {
+                    id
+                    conferenceMenuEn: menu(identifier: "conference-nav") {
+                        links {
+                            text: title(language: "en")
+                            link: href(language: "en")
+                            page {
+                                slug(language: "en")
+                            }
+                        }
+                    }
+                    programMenuEn: menu(identifier: "program-nav") {
+                        links {
+                            text: title(language: "en")
+                            link: href(language: "en")
+                            page {
+                                slug(language: "en")
+                            }
+                        }
+                    }
+                    conferenceMenuIt: menu(identifier: "conference-nav") {
+                        links {
+                            text: title(language: "it")
+                            link: href(language: "it")
+                            page {
+                                slug(language: "it")
+                            }
+                        }
+                    }
+                    programMenuIt: menu(identifier: "program-nav") {
+                        links {
+                            text: title(language: "it")
+                            link: href(language: "it")
+                            page {
+                                slug(language: "it")
+                            }
+                        }
+                    }
+                }
+            }
+            """,
+            variables={"code": conference.code},
+        )
+
+    assert "errors" not in resp
